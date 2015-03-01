1cc6b449515916f43142aa07395382627c4875cd
david denton <david.denton@springer.com>
2015-03-01 22:31:51 +0000

    dave - tests for SegmentMatchers

---
+++ src/test/scala/io/github/daviddenton/fintrospect/SegmentMatcherTest.scala
@@ -0,0 +1,60 @@
+package io.github.daviddenton.fintrospect
+
+import io.github.daviddenton.fintrospect.SegmentMatchers._
+import org.joda.time.DateTime
+import org.scalatest.{ShouldMatchers, FunSpec}
+
+class SegmentMatcherTest extends FunSpec with ShouldMatchers {
+
+  describe("int") {
+    it("unapplies a valid value") {
+      int("name").unapply("123") should be === Some(123)
+    }
+
+    it("does not unapply an invalid value") {
+      int("name").unapply("notANumber") should be === None
+    }
+  }
+
+  describe("integer") {
+    it("unapplies a valid value") {
+      integer("name").unapply("123") should be === Some(123)
+    }
+
+    it("does not unapply an invalid value") {
+      integer("name").unapply("notANumber") should be === None
+    }
+  }
+
+  describe("long") {
+    it("unapplies a valid value") {
+      long("name").unapply("123") should be === Some(123L)
+    }
+
+    it("does not unapply an invalid value") {
+      long("name").unapply("notANumber") should be === None
+    }
+  }
+
+  describe("boolean") {
+    it("unapplies a valid value") {
+      boolean("name").unapply("true") should be === Some(true)
+    }
+
+    it("does not unapply an invalid value") {
+      boolean("name").unapply("notABoolean") should be === None
+    }
+  }
+
+  describe("string") {
+    it("unapplies a valid value") {
+      string("name").unapply("123") should be === Some("123")
+    }
+  }
+
+  describe("dateTime") {
+    it("unapplies a valid value") {
+      dateTime("name").unapply("1970-01-01T00:00:00+00:00") should be === Some(new DateTime(0))
+    }
+  }
+}
