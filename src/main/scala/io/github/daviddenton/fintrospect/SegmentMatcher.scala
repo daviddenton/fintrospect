cdc362d54c39088bf926560933efd3a975bb060e
david denton <david.denton@springer.com>
2015-03-02 09:13:55 +0000

    dave - added some testing of FintrospectModule

--- src/main/scala/io/github/daviddenton/fintrospect/SegmentMatcher.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/SegmentMatcher.scala
@@ -1,7 +1,5 @@
 package io.github.daviddenton.fintrospect
 
-import com.twitter.finagle.http.{path => fp}
-
 trait SegmentMatcher[T] {
   val argument: Option[(String, Class[_])] = None
 
--- src/main/scala/io/github/daviddenton/fintrospect/util/ArgoUtil.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/util/ArgoUtil.scala
@@ -1,25 +1,16 @@
 package io.github.daviddenton.fintrospect.util
 
-import argo.format.{CompactJsonFormatter, PrettyJsonFormatter}
+import argo.format.PrettyJsonFormatter
 import argo.jdom.JsonNodeFactories._
 import argo.jdom.{JdomParser, JsonNode, JsonNodeFactories, JsonRootNode}
 
 object ArgoUtil {
 
   private val pretty = new PrettyJsonFormatter()
-  private val compact = new CompactJsonFormatter()
 
   def parse(in: String): JsonRootNode = new JdomParser().parse(in)
-  
+
   def pretty(node: JsonRootNode): String = pretty.format(node)
-
-  def compact(node: JsonRootNode): String = compact.format(node)
-
-  def toJson(fields: Seq[(String, String)]): JsonRootNode = toJson(fields.toMap)
-
-  def toJson(fields: Map[String, String]): JsonRootNode = {
-    JsonNodeFactories.`object`(fields.map(f => field(f._1, string(f._2))).toSeq: _*)
-  }
 
   def obj(fields: Iterable[(String, JsonNode)]): JsonRootNode = {
     JsonNodeFactories.`object`(fields.map(f => field(f._1, f._2)).toSeq: _*)
