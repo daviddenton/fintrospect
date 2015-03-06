20903e5102a4e25011c01c8ab38190688b9ea92c
david denton <david.denton@springer.com>
2015-03-06 15:08:01 +0000

    dave - removing PathMethod as it's not useful

--- src/main/scala/io/github/daviddenton/fintrospect/PathMethod.scala
+++
@@ -1,5 +0,0 @@
-package io.github.daviddenton.fintrospect
-
-import org.jboss.netty.handler.codec.http.HttpMethod
-
-case class PathMethod(method: HttpMethod, summary: String, params: Seq[Parameter], responses: Seq[PathResponse], securities: Seq[Security])
