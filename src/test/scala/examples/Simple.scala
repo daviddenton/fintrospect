978ab44daed5a20f0bd96a072a67189f7e9a4c3e
david denton <david.denton@springer.com>
2015-03-06 16:21:38 +0000

    dave -  added examples of the various param types into Swagger

--- src/test/scala/examples/Simple.scala
+++ src/test/scala/examples/Simple.scala
@@ -2,26 +2,20 @@ package examples
 
 import java.net.InetSocketAddress
 
-import com.twitter.finagle.Service
 import com.twitter.finagle.builder.ServerBuilder
 import com.twitter.finagle.http.path.Root
-import com.twitter.finagle.http.{Http, Request, Response, RichHttp}
-import com.twitter.util.Future
+import com.twitter.finagle.http.{Http, Request, RichHttp}
 import io.github.daviddenton.fintrospect.SegmentMatchers._
 import io.github.daviddenton.fintrospect.renderers.SimpleJson
 import io.github.daviddenton.fintrospect.{Description, FintrospectModule}
 import org.jboss.netty.handler.codec.http.HttpMethod
+import util.Echo
 
 object Simple extends App {
-
-  case class AService() extends Service[Request, Response] {
-    def apply(request: Request): Future[Response] = ???
-  }
-
   val module = FintrospectModule(Root, SimpleJson())
-    .withRoute(Description("a get endpoint", HttpMethod.GET, _ / "echo"), string("message"), (s: String) => AService())
-    .withRoute(Description("a post endpoint", HttpMethod.POST, _ / "echo"), string("message"), (s: String) => AService())
-    .withRoute(Description("a friendly endpoint", HttpMethod.GET, _ / "welcome"), string("firstName"), fixed("bertrand"), string("secondName"), (x: String, y: String, z: String) => AService())
+    .withRoute(Description("a get endpoint", HttpMethod.GET, _ / "echo"), string("message"), (s: String) => Echo(s))
+    .withRoute(Description("a post endpoint", HttpMethod.POST, _ / "echo"), string("message"), (s: String) => Echo(s))
+    .withRoute(Description("a friendly endpoint", HttpMethod.GET, _ / "welcome"), string("firstName"), fixed("bertrand"), string("secondName"), (x: String, y: String, z: String) => Echo(x))
 
   ServerBuilder()
     .codec(RichHttp[Request](Http()))
--- src/test/scala/io/github/daviddenton/fintrospect/renderers/JsonRendererTest.scala
+++ src/test/scala/io/github/daviddenton/fintrospect/renderers/JsonRendererTest.scala
@@ -9,6 +9,7 @@ import io.github.daviddenton.fintrospect.util.ArgoUtil._
 import io.github.daviddenton.fintrospect.{Description, FintrospectModule}
 import org.jboss.netty.handler.codec.http.HttpMethod
 import org.scalatest.{FunSpec, ShouldMatchers}
+import util.Echo
 
 import scala.io.Source
 
@@ -16,12 +17,13 @@ abstract class JsonRendererTest(name: String, renderer: FintrospectModule.Render
   describe(name) {
     it("renders as expected") {
       val module = FintrospectModule(Root, renderer)
-        .withRoute(Description("a get endpoint", HttpMethod.GET, _ / "echo").withHeader("aHeader", classOf[String]), string("message"), (s: String) => null)
-        .withRoute(Description("a post endpoint", HttpMethod.POST, _ / "echo"), string("message"), (s: String) => null)
-        .withRoute(Description("a friendly endpoint", HttpMethod.GET, _ / "welcome"), string("firstName"), fixed("bertrand"), string("secondName"), (x: String, y: String, z: String) => null)
+        .withRoute(Description("a get endpoint", HttpMethod.GET, _ / "echo").withHeader("header", classOf[String]), string("message"), (s: String) => Echo(s))
+        .withRoute(Description("a post endpoint", HttpMethod.POST, _ / "echo").withBodyParam("bodyParam", classOf[Int]), string("message"), (s: String) => Echo(s))
+        .withRoute(Description("a friendly endpoint", HttpMethod.GET, _ / "welcome").withQueryParam("query", classOf[Boolean]), string("firstName"), fixed("bertrand"), string("secondName"), (x: String, y: String, z: String) => Echo(x, y, z))
 
       val expected = parse(Source.fromInputStream(renderer.getClass.getResourceAsStream(s"$name.json")).mkString)
-      parse(Await.result(module.toService.apply(Request("/"))).content.toString(Utf8)) should be === expected
+      val actual = Await.result(module.toService(Request("/"))).content.toString(Utf8)
+      parse(actual) should be === expected
     }
   }
 
