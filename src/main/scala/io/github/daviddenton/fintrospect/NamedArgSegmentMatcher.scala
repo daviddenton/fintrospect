23001ac28805b2e24b2d2c0ec9b8762d13ca7bb4
david denton <david.denton@springer.com>
2015-03-06 15:28:40 +0000

    dave -  types of parameter

--- src/main/scala/io/github/daviddenton/fintrospect/NamedArgSegmentMatcher.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/NamedArgSegmentMatcher.scala
@@ -1,7 +1,6 @@
 package io.github.daviddenton.fintrospect
 
 abstract class NamedArgSegmentMatcher[T](val name: String)(implicit m: Manifest[T]) extends SegmentMatcher[T] {
-  override val argument = Some((name, m.runtimeClass))
-
+  override val argument = Some(Parameter.path(name, m.runtimeClass))
   override def toString = s"{$name}"
 }
--- src/main/scala/io/github/daviddenton/fintrospect/SegmentMatcher.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/SegmentMatcher.scala
@@ -1,7 +1,7 @@
 package io.github.daviddenton.fintrospect
 
 trait SegmentMatcher[T] {
-  val argument: Option[(String, Class[_])] = None
+  val argument: Option[Parameter] = None
 
   def unapply(str: String): Option[T]
 }
