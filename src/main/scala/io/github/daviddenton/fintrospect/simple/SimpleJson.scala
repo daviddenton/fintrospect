f6e870a7d89db7c8be31e8c1a00eee313abfad70
david denton <david.denton@springer.com>
2015-03-06 14:16:28 +0000

    dave - reimplemented SimpleJson

--- src/main/scala/io/github/daviddenton/fintrospect/simple/SimpleJson.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/simple/SimpleJson.scala
@@ -1,9 +1,14 @@
 package io.github.daviddenton.fintrospect.simple
 
-import argo.jdom.JsonRootNode
+import argo.jdom.JsonNodeFactories.string
+import argo.jdom.{JsonNode, JsonRootNode}
 import io.github.daviddenton.fintrospect.ModuleRoute
+import io.github.daviddenton.fintrospect.util.ArgoUtil._
 
 object SimpleJson extends (Seq[ModuleRoute] => JsonRootNode) {
-//  def apply(moduleRoutes: Seq[ModuleRoute]): JsonRootNode = obj("resources" -> obj(moduleRoutes.map(_.describe)))
-  def apply(moduleRoutes: Seq[ModuleRoute]): JsonRootNode = ???
+  private def render(mr: ModuleRoute): (String, JsonNode) = {
+    mr.description.method + ":" + (mr.description.complete(mr.rootPath).toString :: mr.segmentMatchers.map(_.toString).toList).mkString("/") -> string(mr.description.value)
+  }
+
+  def apply(moduleRoutes: Seq[ModuleRoute]): JsonRootNode = obj("resources" -> obj(moduleRoutes.map(render)))
 }
--- src/test/scala/io/github/daviddenton/fintrospect/simple/SimpleJsonTest.scala
+++ src/test/scala/io/github/daviddenton/fintrospect/simple/SimpleJsonTest.scala
@@ -1,18 +1,29 @@
 package io.github.daviddenton.fintrospect.simple
 
+import com.twitter.finagle.http.Request
+import com.twitter.finagle.http.path.Root
+import com.twitter.io.Charsets._
+import com.twitter.util.Await
+import io.github.daviddenton.fintrospect.FintrospectModule
+import io.github.daviddenton.fintrospect.SegmentMatchers._
+import io.github.daviddenton.fintrospect.swagger.SwDescription
+import io.github.daviddenton.fintrospect.util.ArgoUtil.parse
+import org.jboss.netty.handler.codec.http.HttpMethod
 import org.scalatest.{FunSpec, ShouldMatchers}
+
+import scala.io.Source
 
 class SimpleJsonTest extends FunSpec with ShouldMatchers {
 
-//  describe("simple") {
-//    it("renders as expected") {
-//      val module = FintrospectModule(Root, SimpleJson)
-//        .withRoute(SimpleDescription("a get endpoint", HttpMethod.GET, _ / "echo"), string("message"), (s: String) => null)
-//        .withRoute(SimpleDescription("a post endpoint", HttpMethod.POST, _ / "echo"), string("message"), (s: String) => null)
-//        .withRoute(SimpleDescription("a friendly endpoint", HttpMethod.GET, _ / "welcome"), string("firstName"), fixed("bertrand"), string("secondName"), (x: String, y: String, z: String) => null)
-//
-//      val expected = parse(Source.fromInputStream(this.getClass.getResourceAsStream("expected.json")).mkString)
-//      parse(Await.result(module.toService.apply(Request("/"))).content.toString(UTF_8)) should be === expected
-//    }
-//  }
+  describe("simple") {
+    it("renders as expected") {
+      val module = FintrospectModule(Root, SimpleJson)
+        .withRoute(SwDescription("a get endpoint", HttpMethod.GET, _ / "echo"), string("message"), (s: String) => null)
+        .withRoute(SwDescription("a post endpoint", HttpMethod.POST, _ / "echo"), string("message"), (s: String) => null)
+        .withRoute(SwDescription("a friendly endpoint", HttpMethod.GET, _ / "welcome"), string("firstName"), fixed("bertrand"), string("secondName"), (x: String, y: String, z: String) => null)
+
+      val expected = parse(Source.fromInputStream(this.getClass.getResourceAsStream("expected.json")).mkString)
+      parse(Await.result(module.toService.apply(Request("/"))).content.toString(Utf8)) should be === expected
+    }
+  }
 }
