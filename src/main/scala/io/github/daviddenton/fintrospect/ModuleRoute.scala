32862a42da55fd35bd802a85f7225fd109ad37f1
david denton <david.denton@springer.com>
2015-03-06 13:51:13 +0000

    dave - swagger 2 test

--- src/main/scala/io/github/daviddenton/fintrospect/ModuleRoute.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/ModuleRoute.scala
@@ -3,6 +3,6 @@ package io.github.daviddenton.fintrospect
 import com.twitter.finagle.http.path.Path
 import io.github.daviddenton.fintrospect.swagger.SwDescription
 
-private[fintrospect] class ModuleRoute protected[fintrospect](description: SwDescription, rootPath: Path, segmentMatchers: Seq[SegmentMatcher[_]]) {
+case class ModuleRoute protected[fintrospect](description: SwDescription, rootPath: Path, segmentMatchers: Seq[SegmentMatcher[_]]) {
   override def toString: String = (description.complete(rootPath).toString :: segmentMatchers.map(_.toString).toList).mkString("/")
 }
