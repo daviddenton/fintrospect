5b941a80b3dcdb9ab6ca84801d4b6e7b6f9b1793
David Denton <denton.david@gmail.com>
2015-03-06 15:00:58 +0000

    dave - rename

--- src/main/scala/io/github/daviddenton/fintrospect/PathMethod.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/PathMethod.scala
@@ -2,4 +2,4 @@ package io.github.daviddenton.fintrospect
 
 import org.jboss.netty.handler.codec.http.HttpMethod
 
-case class PathMethod(method: HttpMethod, summary: String, params: Seq[Parameter], responses: Seq[PathResponse], securities: Seq[SwSecurity])
+case class PathMethod(method: HttpMethod, summary: String, params: Seq[Parameter], responses: Seq[PathResponse], securities: Seq[Security])
