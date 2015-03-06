304c9cfaaa7f36f086d483208cbb49507b37d584
david denton <david.denton@springer.com>
2015-03-06 15:00:58 +0000

    dave - rename

--- src/main/scala/io/github/daviddenton/fintrospect/Security.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/Security.scala
@@ -4,15 +4,15 @@ import argo.jdom.JsonNode
 import argo.jdom.JsonNodeFactories._
 import io.github.daviddenton.fintrospect.util.ArgoUtil._
 
-case class SwSecurity(private val name: String, private val definition: JsonNode) {
+case class Security(private val name: String, private val definition: JsonNode) {
   def toPathSecurity: (String, JsonNode) = name -> obj()
 
  def toJsonDefinition: (String, JsonNode) = name -> definition
 }
 
-object SwSecurity {
-  def apiKey(name: String): SwSecurity = {
-    SwSecurity(name, obj(
+object Security {
+  def apiKey(name: String): Security = {
+    Security(name, obj(
       "type" -> string("api_key"),
       "name" -> string(name),
       "in" -> string("header")))
