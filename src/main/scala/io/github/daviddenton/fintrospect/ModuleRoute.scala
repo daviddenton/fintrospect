1649057d101441baaaaa1eeed13d206996014db8
david denton <david.denton@springer.com>
2015-03-06 16:28:22 +0000

    dave - hidden route

--- src/main/scala/io/github/daviddenton/fintrospect/ModuleRoute.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/ModuleRoute.scala
@@ -2,7 +2,7 @@ package io.github.daviddenton.fintrospect
 
 import com.twitter.finagle.http.path.Path
 
-case class ModuleRoute(description: Description, rootPath: Path, segmentMatchers: Seq[SegmentMatcher[_]]) {
+class ModuleRoute(val description: Description, rootPath: Path, segmentMatchers: Seq[SegmentMatcher[_]]) {
   val params = segmentMatchers.flatMap(_.toParameter) ++ description.params
 
   override def toString: String = (description.complete(rootPath).toString :: segmentMatchers.map(_.toString).toList).mkString("/")
