95ef584355e78c17785965b30caf76840859759d
David Denton <denton.david@gmail.com>
2015-03-07 20:24:14 +0000

    dave - inlining locations

--- src/main/scala/io/github/daviddenton/fintrospect/parameters/Query.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/parameters/Query.scala
@@ -1,9 +1,15 @@
 package io.github.daviddenton.fintrospect.parameters
 
-import io.github.daviddenton.fintrospect.parameters.Locations.QueryLocation
+import com.twitter.finagle.http.Request
 
 import scala.reflect.ClassTag
 
 object Query extends Parameters[RequestParameter]() {
-  protected def create[T](name: String, required: Boolean, parse: (String => Option[T]))(implicit ct: ClassTag[T]) = new RequestParameter[T](name, QueryLocation, required, parse)
+  private val location = new Location {
+    override def toString = "query"
+
+    override def from(name: String, request: Request): Option[String] = request.params.get(name)
+  }
+
+  protected def create[T](name: String, required: Boolean, parse: (String => Option[T]))(implicit ct: ClassTag[T]) = new RequestParameter[T](name, location, required, parse)
 }
