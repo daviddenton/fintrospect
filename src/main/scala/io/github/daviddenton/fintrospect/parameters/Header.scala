f5aca5b64845a08f68fdb9f4b19d5d89b51d2264
david denton <david.denton@springer.com>
2015-03-07 20:24:14 +0000

    dave - inlining locations

--- src/main/scala/io/github/daviddenton/fintrospect/parameters/Header.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/parameters/Header.scala
@@ -1,9 +1,16 @@
 package io.github.daviddenton.fintrospect.parameters
 
-import io.github.daviddenton.fintrospect.parameters.Locations.HeaderLocation
+import com.twitter.finagle.http.Request
 
 import scala.reflect.ClassTag
 
 object Header extends Parameters[RequestParameter]() {
-  protected def create[T](name: String, required: Boolean, parse: (String => Option[T]))(implicit ct: ClassTag[T]) = new RequestParameter[T](name, HeaderLocation, required, parse)
+
+  private val location = new Location {
+    override def toString = "header"
+
+    override def from(name: String, request: Request): Option[String] = Option(request.headers().get(name))
+  }
+
+  protected def create[T](name: String, required: Boolean, parse: (String => Option[T]))(implicit ct: ClassTag[T]) = new RequestParameter[T](name, location, required, parse)
 }
--- src/main/scala/io/github/daviddenton/fintrospect/parameters/Location.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/parameters/Location.scala
@@ -2,28 +2,6 @@ package io.github.daviddenton.fintrospect.parameters
 
 import com.twitter.finagle.http.Request
 
-sealed trait Location {
+trait Location {
   def from(name: String, request: Request): Option[String]
-}
-
-object Locations {
-
-  object PathLocation extends Location {
-    override def toString = "path"
-
-    override def from(name: String, request: Request): Option[String] = None
-  }
-
-  object HeaderLocation extends Location {
-    override def toString = "header"
-
-    override def from(name: String, request: Request): Option[String] = Option(request.headers().get(name))
-  }
-
-  object QueryLocation extends Location {
-    override def toString = "query"
-
-    override def from(name: String, request: Request): Option[String] = request.params.get(name)
-  }
-
 }
