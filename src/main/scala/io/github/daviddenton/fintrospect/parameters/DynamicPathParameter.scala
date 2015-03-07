3c94a0aa6675a15d1a38b44e2781ff4112df5927
David Denton <denton.david@gmail.com>
2015-03-07 21:15:19 +0000

    dave - removed DynamicPathParameter as unused

--- src/main/scala/io/github/daviddenton/fintrospect/parameters/DynamicPathParameter.scala
+++
@@ -1,9 +0,0 @@
-package io.github.daviddenton.fintrospect.parameters
-
-import scala.reflect.ClassTag
-
-class DynamicPathParameter[T](name: String, parse: (String => Option[T]))(implicit ct: ClassTag[T]) extends PathParameter[T](name)(ct) {
-  def unapply(str: String): Option[T] = parse(str)
-  override def toString = s"{$name}"
-  override val toParameter: Option[PathParameter[_]] = Some(this)
-}
