0e25d22ecf96470837c1571459a66faf1547b113
david denton <david.denton@springer.com>
2015-03-07 21:20:16 +0000

    dave - tidy

--- src/main/scala/io/github/daviddenton/fintrospect/ModuleRoute.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/ModuleRoute.scala
@@ -6,5 +6,5 @@ import io.github.daviddenton.fintrospect.parameters.PathParameter
 class ModuleRoute(val description: Description, rootPath: Path, pathParams: Seq[PathParameter[_]]) {
   val params = pathParams.flatten ++ description.params
 
-  override def toString: String = (description.complete(rootPath).toString :: pathParams.map(_.toString).toList).mkString("/")
+  override def toString: String = (description.complete(rootPath).toString :: pathParams.map(_.toString()).toList).mkString("/")
 }
