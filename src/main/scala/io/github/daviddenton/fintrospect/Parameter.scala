b621673d56f8f614b15e52ee0f729712a77e8d1f
david denton <david.denton@springer.com>
2015-03-07 11:47:37 +0000

    dave - moving params around refactor

--- src/main/scala/io/github/daviddenton/fintrospect/Parameter.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/Parameter.scala
@@ -7,3 +7,7 @@ import scala.reflect.ClassTag
 class Parameter[T] protected[fintrospect](val name: String, val location: String, val required: Boolean)(implicit ct: ClassTag[T]) {
   val paramType = decapitalize(ct.runtimeClass.getSimpleName)
 }
+
+abstract class RequestParameter[T](name: String, location: String, required: Boolean)(implicit ct: ClassTag[T]) extends Parameter[T](name, location, required)(ct) {
+  
+}
