da90f591f384e5474bf2dd87185f21f884a4af5f
David Denton <denton.david@gmail.com>
2015-03-07 14:58:44 +0000

    dave - maybe start to combine segment matchers and parameters

--- src/main/scala/io/github/daviddenton/fintrospect/Parameter.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/Parameter.scala
@@ -10,7 +10,7 @@ abstract class Parameter[T] protected[fintrospect](val name: String, val where: 
   val paramType = decapitalize(ct.runtimeClass.getSimpleName)
 }
 
-class PathParameter[T](name: String, parse: (String => Option[T]))(implicit ct: ClassTag[T]) extends Parameter[T](name, "path", true)(ct) {
+class PathParameter[T](name: String, parse: (String => Option[T]))(implicit ct: ClassTag[T]) extends Parameter[T](name, "path", true)(ct) with SegmentMatcher[T] {
   def unapply(str: String): Option[T] = parse(str)
   override def toString = s"{$name}"
 }
