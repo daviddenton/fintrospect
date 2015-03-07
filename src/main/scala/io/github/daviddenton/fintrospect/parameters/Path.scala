4099a5079f23d7d94bc81bfd63f3366f36801e8c
david denton <david.denton@springer.com>
2015-03-07 20:39:04 +0000

    dave - removed SegmentMatcher as now PathParameter

--- src/main/scala/io/github/daviddenton/fintrospect/parameters/Path.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/parameters/Path.scala
@@ -3,14 +3,18 @@ package io.github.daviddenton.fintrospect.parameters
 import scala.reflect.ClassTag
 
 object Path extends Parameters[PathParameter]() {
-  protected def create[T](name: String, required: Boolean, parse: (String => Option[T]))(implicit ct: ClassTag[T]) = new PathParameter[T](name, parse)
+  protected def create[T](name: String, required: Boolean, parse: (String => Option[T]))(implicit ct: ClassTag[T]) = new PathParameter[T](name)(ct) {
+    def unapply(str: String): Option[T] = parse(str)
 
-  def fixed(value: String): SegmentMatcher[String] = new SegmentMatcher[String] {
-    val toParameter: Option[Parameter[_]] = None
+    override def toString = s"{$name}"
 
-    override def unapply(str: String): Option[String] = if (str == value) Some(str) else None
-
-    override def toString: String = value
+    override val toParameter: Option[PathParameter[_]] = Some(this)
   }
 
+
+  def fixed(value: String): PathParameter[String] = new PathParameter[String](value) {
+    val toParameter: Option[PathParameter[_]] = None
+
+    override def unapply(str: String): Option[String] = if (str == value) Some(str) else None
+  }
 }
--- src/main/scala/io/github/daviddenton/fintrospect/parameters/PathParameter.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/parameters/PathParameter.scala
@@ -2,10 +2,7 @@ package io.github.daviddenton.fintrospect.parameters
 
 import scala.reflect.ClassTag
 
-class PathParameter[T](name: String, parse: (String => Option[T]))(implicit ct: ClassTag[T]) extends Parameter[T](name, "path", true)(ct) with SegmentMatcher[T] {
-  override val toParameter: Option[Parameter[_]] = Some(this)
-
-  def unapply(str: String): Option[T] = parse(str)
-
-  override def toString = s"{$name}"
+abstract class PathParameter[T](name: String)(implicit ct: ClassTag[T]) extends Parameter[T](name, "path", true)(ct) {
+  val toParameter: Option[PathParameter[_]]
+  def unapply(str: String): Option[T]
 }
