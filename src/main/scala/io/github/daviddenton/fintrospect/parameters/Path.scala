cef845183e47485b6b3f58718fb5f634cf5993ef
David Denton <denton.david@gmail.com>
2015-03-07 21:24:27 +0000

    dave - tidy

--- src/main/scala/io/github/daviddenton/fintrospect/parameters/Path.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/parameters/Path.scala
@@ -6,14 +6,14 @@ object Path extends Parameters[PathParameter]() {
   protected def create[T](name: String, required: Boolean, parse: (String => Option[T]))(implicit ct: ClassTag[T]) = new PathParameter[T](name)(ct) {
     def unapply(str: String): Option[T] = parse(str)
 
-    override def toString = s"{$name}"
+    override def toString() = s"{$name}"
 
     override def iterator: Iterator[PathParameter[_]] = Some(this).iterator
   }
 
   def fixed(value: String): PathParameter[String] = new PathParameter[String](value) {
 
-    override def toString = name
+    override def toString() = name
 
     override def unapply(str: String): Option[String] = if (str == value) Some(str) else None
 
