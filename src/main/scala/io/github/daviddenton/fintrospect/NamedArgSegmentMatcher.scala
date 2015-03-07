f818568801f9fac4c255fd94585450a34ebddedb
david denton <david.denton@springer.com>
2015-03-07 12:53:03 +0000

    dave - moving params around refactor

--- src/main/scala/io/github/daviddenton/fintrospect/NamedArgSegmentMatcher.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/NamedArgSegmentMatcher.scala
@@ -2,8 +2,8 @@ package io.github.daviddenton.fintrospect
 
 import scala.reflect.ClassTag
 
-class NamedArgSegmentMatcher[T](val parameter: RequestParameter[T])(implicit m: ClassTag[T]) extends SegmentMatcher[T] {
-  def unapply(str: String): Option[T] = parameter.mapper(str)
+class NamedArgSegmentMatcher[T](parameter: RequestParameter[T])(implicit m: ClassTag[T]) extends SegmentMatcher[T] {
+  def unapply(str: String): Option[T] = parameter.unapply(str)
   override def toString = s"{${parameter.name}}"
 }
 
