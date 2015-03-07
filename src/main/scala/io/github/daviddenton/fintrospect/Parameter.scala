971cf0527a316647ce31d82d454a6da3a4ed9ffa
David Denton <denton.david@gmail.com>
2015-03-07 14:22:09 +0000

    dave - moving away from Path parameters being requestparameters

--- src/main/scala/io/github/daviddenton/fintrospect/Parameter.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/Parameter.scala
@@ -10,7 +10,7 @@ class Parameter[T] protected[fintrospect](val name: String, val where: String, v
   val paramType = decapitalize(ct.runtimeClass.getSimpleName)
 }
 
-abstract class RequestParameter[T](name: String, location: Location, required: Boolean)(implicit ct: ClassTag[T]) extends Parameter[T](name, location.toString, required)(ct) {
-  def from(request: Request): Option[T] = location.from(name, request).flatMap(unapply)
-  def unapply(str: String): Option[T]
+class RequestParameter[T](name: String, location: Location, required: Boolean, parse: (String => Option[T]))(implicit ct: ClassTag[T]) extends Parameter[T](name, location.toString, required)(ct) {
+  def from(request: Request): Option[T] = location.from(name, request).flatMap(parse)
+  def unapply(str: String): Option[T] = parse(str)
 }
