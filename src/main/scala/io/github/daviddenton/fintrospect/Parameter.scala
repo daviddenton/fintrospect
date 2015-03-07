b789440f0ecf0fd6c98c800e0b4a5a6d943fce07
david denton <david.denton@springer.com>
2015-03-07 11:44:01 +0000

    dave - moving params around refactor

--- src/main/scala/io/github/daviddenton/fintrospect/Parameter.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/Parameter.scala
@@ -4,13 +4,6 @@ import java.beans.Introspector._
 
 import scala.reflect.ClassTag
 
-class Parameter[T] private(val name: String, val location: String, val required: Boolean)(implicit ct: ClassTag[T]) {
+class Parameter[T] protected[fintrospect](val name: String, val location: String, val required: Boolean)(implicit ct: ClassTag[T]) {
   val paramType = decapitalize(ct.runtimeClass.getSimpleName)
-}
-
-object Parameter {
-  def path[T](name: String)(implicit ct: ClassTag[T]) = new Parameter[T](name, "path", true)
-  def body[T](name: String)(implicit ct: ClassTag[T]) = new Parameter[T](name, "body", true)
-  def query[T](name: String)(implicit ct: ClassTag[T]) = new Parameter[T](name, "query", true)
-  def header[T](name: String)(implicit ct: ClassTag[T]) = new Parameter[T](name, "header", true)
 }
---
+++ src/main/scala/io/github/daviddenton/fintrospect/Parameters.scala
@@ -0,0 +1,13 @@
+package io.github.daviddenton.fintrospect
+
+import scala.reflect.ClassTag
+
+object Parameters {
+  def path[T](name: String)(implicit ct: ClassTag[T]) = new Parameter[T](name, "path", true)
+
+  def body[T](name: String)(implicit ct: ClassTag[T]) = new Parameter[T](name, "body", true)
+
+  def query[T](name: String)(implicit ct: ClassTag[T]) = new Parameter[T](name, "query", true)
+
+  def header[T](name: String)(implicit ct: ClassTag[T]) = new Parameter[T](name, "header", true)
+}
