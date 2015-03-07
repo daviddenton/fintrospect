e8144a6fd1f0d63877f3ffc2331403359d3d5212
david denton <david.denton@springer.com>
2015-03-07 14:22:09 +0000

    dave - moving away from Path parameters being requestparameters

--- src/main/scala/io/github/daviddenton/fintrospect/Parameters.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/Parameters.scala
@@ -8,29 +8,17 @@ import org.joda.time.format.ISODateTimeFormat._
 import scala.util.Try
 
 class Parameters private(location: Location) {
-  def dateTime(name: String): RequestParameter[DateTime] = new RequestParameter[DateTime](name, location, true) {
-    override def unapply(str: String): Option[DateTime] = Try(dateTimeNoMillis().parseDateTime(str)).toOption
-  }
+  def dateTime(name: String): RequestParameter[DateTime] = new RequestParameter[DateTime](name, location, true, str => Try(dateTimeNoMillis().parseDateTime(str)).toOption)
 
-  def boolean(name: String): RequestParameter[Boolean] = new RequestParameter[Boolean](name, location, true) {
-    override def unapply(str: String): Option[Boolean] = Try(str.toBoolean).toOption
-  }
+  def boolean(name: String): RequestParameter[Boolean] = new RequestParameter[Boolean](name, location, true, str => Try(str.toBoolean).toOption)
 
-  def string(name: String): RequestParameter[String] = new RequestParameter[String](name, location, true) {
-    override def unapply(str: String): Option[String] = Option(str)
-  }
+  def string(name: String): RequestParameter[String] = new RequestParameter[String](name, location, true, Option(_))
 
-  def long(name: String): RequestParameter[Long] = new RequestParameter[Long](name, location, true) {
-    override def unapply(str: String): Option[Long] = path.Long.unapply(str)
-  }
+  def long(name: String): RequestParameter[Long] = new RequestParameter[Long](name, location, true, str => path.Long.unapply(str))
 
-  def int(name: String): RequestParameter[Int] = new RequestParameter[Int](name, location, true) {
-    override def unapply(str: String): Option[Int] = path.Integer.unapply(str)
-  }
+  def int(name: String): RequestParameter[Int] = new RequestParameter[Int](name, location, true, str => path.Integer.unapply(str))
 
-  def integer(name: String): RequestParameter[Integer] = new RequestParameter[Integer](name, location, true) {
-    override def unapply(str: String): Option[Integer] = path.Integer.unapply(str).map(new Integer(_))
-  }
+  def integer(name: String): RequestParameter[Integer] = new RequestParameter[Integer](name, location, true, str => path.Integer.unapply(str).map(new Integer(_)))
 }
 
 object Parameters {
