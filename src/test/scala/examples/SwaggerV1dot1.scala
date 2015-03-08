acc06335cad8605b306fda3177f0b5b4770a230e
david denton <david.denton@springer.com>
2015-03-08 20:03:18 +0000

    dave - more tidy

--- src/test/scala/examples/SwaggerV1dot1.scala
+++ src/test/scala/examples/SwaggerV1dot1.scala
@@ -8,15 +8,25 @@ import com.twitter.finagle.http.filter.Cors._
 import com.twitter.finagle.http.path.Root
 import com.twitter.finagle.http.{Http, Request, RichHttp}
 import io.github.daviddenton.fintrospect._
+import io.github.daviddenton.fintrospect.parameters.{Query, Header}
 import io.github.daviddenton.fintrospect.parameters.Path._
 import io.github.daviddenton.fintrospect.renderers.Swagger1dot1Json
 import org.jboss.netty.handler.codec.http.HttpMethod._
+import org.jboss.netty.handler.codec.http.HttpResponseStatus._
 
 object SwaggerV1dot1 extends App {
   val module = FintrospectModule(Root, Swagger1dot1Json())
-    .withRoute(Description("a get endpoint"), On(GET, _ / "echo"), string("message"), (s: String) => Echo(s))
+    .withRoute(
+      Description("a get endpoint")
+        .requiring(Header.string("header"))
+        .returning(OK -> "peachy")
+        .returning(FORBIDDEN -> "no way jose"),
+      On(GET, _ / "echo"), string("message"), (s: String) => Echo(s))
     .withRoute(Description("a post endpoint"), On(POST, _ / "echo"), string("message"), (s: String) => Echo(s))
-    .withRoute(Description("a friendly endpoint"), On(GET, _ / "welcome"), string("firstName"), fixed("bertrand"), string("secondName"), (x: String, y: String, z: String) => Echo(x))
+    .withRoute(
+      Description("a friendly endpoint")
+        .requiring(Query.boolean("query")),
+      On(GET, _ / "welcome"), string("firstName"), fixed("bertrand"), string("secondName"), (x: String, y: String, z: String) => Echo(x, y, z))
 
   ServerBuilder()
     .codec(RichHttp[Request](Http()))
