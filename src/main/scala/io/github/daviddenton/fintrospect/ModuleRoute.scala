d96bbb1d4e1edcd753422255d09894d8cda54d67
David Denton <denton.david@gmail.com>
2015-03-08 21:44:28 +0000

    dave - rename

--- src/main/scala/io/github/daviddenton/fintrospect/ModuleRoute.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/ModuleRoute.scala
@@ -3,9 +3,9 @@ package io.github.daviddenton.fintrospect
 import com.twitter.finagle.http.path.Path
 import io.github.daviddenton.fintrospect.parameters.PathParameter
 
-class ModuleRoute(val description: Description, val on: On, rootPath: Path, pathParams: Seq[PathParameter[_]]) {
+class ModuleRoute(val description: Description, val on: On, basePath: Path, pathParams: Seq[PathParameter[_]]) {
   val allParams = pathParams.flatten ++ description.params
   val allResponses = description.responses
 
-  override def toString: String = (on.completeRoutePath(rootPath).toString :: pathParams.map(_.toString()).toList).mkString("/")
+  override def toString: String = (on.completeRoutePath(basePath).toString :: pathParams.map(_.toString()).toList).mkString("/")
 }
