cdb91bffefe151d0ecfce7bd657e2837553f5562
david denton <david.denton@springer.com>
2015-03-08 21:33:57 +0000

    dave - rename

--- src/main/scala/io/github/daviddenton/fintrospect/On.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/On.scala
@@ -4,5 +4,5 @@ import com.twitter.finagle.http.path.Path
 import org.jboss.netty.handler.codec.http.HttpMethod
 
 case class On(method: HttpMethod, complete: (Path => Path)) {
-  def matches(method: HttpMethod, rootPath: Path, path: Path) = method == this.method && path == complete(rootPath)
+  def matches(method: HttpMethod, basePath: Path, path: Path) = method == this.method && path == complete(basePath)
 }
