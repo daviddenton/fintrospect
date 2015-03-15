5b05f2c25cdc1f3e7cd888e031a09958dd564836
David Denton <denton.david@gmail.com>
2015-03-15 18:12:54 +0000

    dave - added support for date format in parameters

--- src/test/scala/io/github/daviddenton/fintrospect/parameters/ParametersTest.scala
+++ src/test/scala/io/github/daviddenton/fintrospect/parameters/ParametersTest.scala
@@ -1,7 +1,7 @@
 
 package io.github.daviddenton.fintrospect.parameters
 
-import org.joda.time.DateTime
+import org.joda.time.{LocalDate, DateTime}
 import org.scalatest.{FunSpec, ShouldMatchers}
 
 abstract class ParametersTest[T[_] <: Parameter[_]](parameters: Parameters[T]) extends FunSpec with ShouldMatchers {
@@ -55,6 +55,20 @@ abstract class ParametersTest[T[_] <: Parameter[_]](parameters: Parameters[T]) e
   describe("dateTime") {
     it("unapplies a valid value") {
       parameters.dateTime("name").unapply("1970-01-01T00:00:00+00:00") should be === Some(new DateTime(0))
+    }
+
+    it("does not unapply an invalid value") {
+      parameters.dateTime("name").unapply("notADateTime") should be === None
+    }
+  }
+
+  describe("date") {
+    it("unapplies a valid value") {
+      parameters.localDate("name").unapply("1970-01-01") should be === Some(new LocalDate(1970, 1, 1))
+    }
+
+    it("does not unapply an invalid value") {
+      parameters.localDate("name").unapply("notADate") should be === None
     }
   }
 }
