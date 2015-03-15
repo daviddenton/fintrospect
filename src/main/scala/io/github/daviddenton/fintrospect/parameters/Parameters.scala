6ba4e58f3bec36b42a7eb5d7030026dc3489e2a8
david denton <david.denton@springer.com>
2015-03-15 18:12:54 +0000

    dave - added support for date format in parameters

--- src/main/scala/io/github/daviddenton/fintrospect/parameters/Parameters.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/parameters/Parameters.scala
@@ -2,13 +2,15 @@ package io.github.daviddenton.fintrospect.parameters
 
 import com.twitter.finagle.http.path
 import io.github.daviddenton.fintrospect.parameters.Requirement._
-import org.joda.time.DateTime
+import org.joda.time.{LocalDate, DateTime}
 import org.joda.time.format.ISODateTimeFormat._
 
 import scala.reflect.ClassTag
 import scala.util.Try
 
 abstract class Parameters[P[_] <: Parameter[_]] protected[parameters]() {
+  def localDate(name: String, description: String = null, required: Requirement = Mandatory): P[LocalDate] = create(name, Option(description), required, str => Try(date().parseLocalDate(str)).toOption)
+
   def dateTime(name: String, description: String = null, required: Requirement = Mandatory): P[DateTime] = create(name, Option(description), required, str => Try(dateTimeNoMillis().parseDateTime(str)).toOption)
 
   def boolean(name: String, description: String = null, required: Requirement = Mandatory): P[Boolean] = create(name, Option(description), required, str => Try(str.toBoolean).toOption)
