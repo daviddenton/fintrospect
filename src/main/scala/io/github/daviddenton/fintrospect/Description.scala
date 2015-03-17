f75c6a4d62ff131d02c6aa9527b39ed0b848e312
david denton <david.denton@springer.com>
2015-03-17 09:43:56 +0000

    dave - removed request parameters

--- src/main/scala/io/github/daviddenton/fintrospect/Description.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/Description.scala
@@ -1,24 +1,26 @@
 package io.github.daviddenton.fintrospect
 
-import io.github.daviddenton.fintrospect.parameters.{OptionalRequestParameter, RequiredRequestParameter, RequestParameters}
+import io.github.daviddenton.fintrospect.parameters.{RequestParameter, Requirement, OptionalRequestParameter, RequiredRequestParameter}
 import org.jboss.netty.handler.codec.http.HttpResponseStatus
 
 case class Description private(name: String,
                                summary: Option[String],
                                produces: List[MimeType],
                                consumes: List[MimeType],
-                               requestParams: RequestParameters,
+                               optional: List[OptionalRequestParameter[_]],
+                               required: List[RequiredRequestParameter[_]],
                                responses: Map[HttpResponseStatus, String]) {
   def consuming(mimeType: MimeType*) = copy(consumes = (mimeType ++ produces).toList)
 
   def producing(mimeType: MimeType*) = copy(produces = (mimeType ++ produces).toList)
-
-  def taking(rp: RequiredRequestParameter[_]) = copy(requestParams = requestParams.requiring(rp))
-  def taking(rp: OptionalRequestParameter[_]) = copy(requestParams = requestParams.optionally(rp))
+  def taking(rp: RequiredRequestParameter[_]) = copy(required = rp :: required)
+  def taking(rp: OptionalRequestParameter[_]) = copy(optional = rp :: optional)
 
   def returning(codes: (HttpResponseStatus, String)*): Description = copy(responses = responses ++ codes)
+
+  def allParams: Iterable[(Requirement, RequestParameter[_])] = List[(Requirement, RequestParameter[_])]() ++ optional.map(Requirement.Optional -> _) ++ required.map(Requirement.Mandatory -> _)
 }
 
 object Description {
-  def apply(name: String, summary: String = null): Description = Description(name, Option(summary), Nil, Nil, RequestParameters(), Map.empty)
+  def apply(name: String, summary: String = null): Description = Description(name, Option(summary), Nil, Nil, Nil, Nil, Map.empty)
 }
--- src/main/scala/io/github/daviddenton/fintrospect/ModuleRoute.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/ModuleRoute.scala
@@ -1,11 +1,11 @@
 package io.github.daviddenton.fintrospect
 
 import com.twitter.finagle.http.path.Path
+import io.github.daviddenton.fintrospect.parameters.PathParameter
 import io.github.daviddenton.fintrospect.parameters.Requirement._
-import io.github.daviddenton.fintrospect.parameters.{Parameter, PathParameter, Requirement}
 
 class ModuleRoute(val description: Description, val on: On, basePath: Path, pathParams: Seq[PathParameter[_]]) {
-  val allParams: Iterable[(Requirement, Parameter[_, _])] = description.requestParams.allParams ++ pathParams.map(Mandatory -> _)
+  val allParams = description.allParams ++ pathParams.map(Mandatory -> _)
 
   val allResponses = description.responses
 
