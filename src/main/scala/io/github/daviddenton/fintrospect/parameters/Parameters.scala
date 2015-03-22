9bb368fea570090185ffa00303b2507e3efc4f52
david denton <david.denton@springer.com>
2015-03-22 17:11:44 +0000

    dave - refactor of parameters to remove hierarchy

--- src/main/scala/io/github/daviddenton/fintrospect/parameters/Parameters.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/parameters/Parameters.scala
@@ -7,22 +7,24 @@ import org.joda.time.{DateTime, LocalDate}
 import scala.reflect.ClassTag
 import scala.util.Try
 
-abstract class Parameters[P[_] <: Parameter[_]] protected[parameters]() {
-  def localDate(name: String, description: String = null): P[LocalDate] = create(name, Option(description), str => Try(date().parseLocalDate(str)).toOption)
+class Parameters[P[_] <: Parameter[_]] protected[parameters](builder: ParameterBuilder[P]) {
+  def localDate(name: String, description: String = null): P[LocalDate] = builder.apply(name, Option(description), str => Try(date().parseLocalDate(str)).toOption)
 
-  def dateTime(name: String, description: String = null): P[DateTime] = create(name, Option(description), str => Try(dateTimeNoMillis().parseDateTime(str)).toOption)
+  def dateTime(name: String, description: String = null): P[DateTime] = builder.apply(name, Option(description), str => Try(dateTimeNoMillis().parseDateTime(str)).toOption)
 
-  def boolean(name: String, description: String = null): P[Boolean] = create(name, Option(description), str => Try(str.toBoolean).toOption)
+  def boolean(name: String, description: String = null): P[Boolean] = builder.apply(name, Option(description), str => Try(str.toBoolean).toOption)
 
-  def string(name: String, description: String = null): P[String] = create(name, Option(description), Option(_))
+  def string(name: String, description: String = null): P[String] = builder.apply(name, Option(description), Option(_))
 
-  def long(name: String, description: String = null): P[Long] = create(name, Option(description), str => path.Long.unapply(str))
+  def long(name: String, description: String = null): P[Long] = builder.apply(name, Option(description), str => path.Long.unapply(str))
 
-  def int(name: String, description: String = null): P[Int] = create(name, Option(description), str => path.Integer.unapply(str))
+  def int(name: String, description: String = null): P[Int] = builder.apply(name, Option(description), str => path.Integer.unapply(str))
 
-  def integer(name: String, description: String = null): P[Integer] = create(name, Option(description), str => path.Integer.unapply(str).map(new Integer(_)))
+  def integer(name: String, description: String = null): P[Integer] = builder.apply(name, Option(description), str => path.Integer.unapply(str).map(new Integer(_)))
+}
 
-  protected def create[T](name: String, description: Option[String], parse: (String => Option[T]))(implicit ct: ClassTag[T]): P[T]
+trait ParameterBuilder[P[_] <: Parameter[_]] {
+  def apply[T](name: String, description: Option[String], parse: (String => Option[T]))(implicit ct: ClassTag[T]): P[T]
 }
 
 
--- src/main/scala/io/github/daviddenton/fintrospect/parameters/RequiredRequestParameter.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/parameters/RequiredRequestParameter.scala
@@ -7,5 +7,12 @@ import scala.reflect.ClassTag
 class RequiredRequestParameter[T](name: String, description: Option[String], location: Location, parse: (String => Option[T]))(implicit ct: ClassTag[T])
   extends RequestParameter[T](name, description, location, parse)(ct) {
   def from(request: Request): T = unapply(request).get
+
   def unapply(request: Request): Option[T] = location.from(name, request).flatMap(parse)
+}
+
+object RequiredRequestParameter {
+  def builderForLocation(location: Location) = new ParameterBuilder[RequiredRequestParameter]() {
+    def apply[T](name: String, description: Option[String], parse: (String => Option[T]))(implicit ct: ClassTag[T]) = new RequiredRequestParameter[T](name, description, location, parse)
+  }
 }
