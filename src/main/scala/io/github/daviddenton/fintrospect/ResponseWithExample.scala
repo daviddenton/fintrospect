15e169242e1f5a9f59497f517ba2a1e65662cd35
david denton <david.denton@springer.com>
2015-03-28 18:34:59 +0000

    dave - added error example JSON

--- src/main/scala/io/github/daviddenton/fintrospect/ResponseWithExample.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/ResponseWithExample.scala
@@ -1,6 +1,12 @@
 package io.github.daviddenton.fintrospect
 
 import argo.jdom.JsonNode
+import io.github.daviddenton.fintrospect.util.ArgoUtil
+import io.github.daviddenton.fintrospect.util.ArgoUtil._
 import org.jboss.netty.handler.codec.http.HttpResponseStatus
 
 case class ResponseWithExample(status: HttpResponseStatus, description: String, example: JsonNode = null)
+
+object ResponseWithExample {
+  val ERROR_EXAMPLE = obj("message" -> ArgoUtil.string("an error message goes here"))
+}
