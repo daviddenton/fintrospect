ef6bbd98430325174a7215593a694f6d1901a3c3
david denton <david.denton@springer.com>
2015-03-28 12:04:01 +0000

    dave - definition models

--- src/main/scala/io/github/daviddenton/fintrospect/renderers/JsonToJsonSchema.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/renderers/JsonToJsonSchema.scala
@@ -1,8 +1,9 @@
 package io.github.daviddenton.fintrospect.renderers
 
-import argo.jdom.{JsonRootNode, JsonNode}
 import argo.jdom.JsonNodeFactories.string
 import argo.jdom.JsonNodeType._
+import argo.jdom.{JsonField, JsonNode, JsonNodeFactories, JsonRootNode}
+import io.github.daviddenton.fintrospect.renderers.JsonToJsonSchema.IllegalSchemaException
 import io.github.daviddenton.fintrospect.util.ArgoUtil._
 
 import scala.collection.JavaConversions._
@@ -65,5 +66,34 @@ object JsonToJsonSchema {
       case ARRAY => obj("type" -> string("array"), "items" -> input.getElements.to[Seq].headOption.map(toSchema).getOrElse(throw new IllegalSchemaException("Cannot use an empty list for a schema!")))
       case OBJECT => obj("type" -> string("object"), "properties" -> obj(input.getFieldList.to[Seq].map(f => f.getName.getText -> toSchema(f.getValue)): _*))
     }
+  }
+}
+
+class JsonToJsonSchema(input: JsonNode) {
+
+  private var id = 0
+  private var definitions: List[JsonField] = Nil
+
+  def toSchema(input: JsonNode): JsonRootNode = {
+    input.getType match {
+      case NULL => throw new IllegalSchemaException("Cannot use a null value in a schema!")
+      case STRING => obj("type" -> string("string"))
+      case TRUE => obj("type" -> string("boolean"))
+      case FALSE => obj("type" -> string("boolean"))
+      case NUMBER => obj("type" -> string("number"))
+      case ARRAY => obj("type" -> string("array"), "items" -> input.getElements.to[Seq].headOption.map(toSchema).getOrElse(throw new IllegalSchemaException("Cannot use an empty list for a schema!")))
+      case OBJECT => objectToSchema(input)
+    }
+  }
+
+  private def objectToSchema(input: JsonNode): JsonRootNode = {
+    val name = "definition" + id
+    id += 1
+    definitions = JsonNodeFactories.field(name, obj("type" -> string("object"), "properties" -> obj(input.getFieldList.to[Seq].map(f => f.getName.getText -> toSchema(f.getValue)): _*))) :: definitions
+    obj("$ref" -> string(s"#/definitions/$name"))
+  }
+
+  def toSchemaAndModels(): (JsonRootNode, List[JsonField]) = {
+    (toSchema(input), definitions)
   }
 }
