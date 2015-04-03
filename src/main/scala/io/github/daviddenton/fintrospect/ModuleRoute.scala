4c4cb470a6a9f423db336279859a86ecbdccd8be
david denton <david.denton@springer.com>
2015-04-03 16:46:19 +0100

    dave - added body parameters to allparams

--- src/main/scala/io/github/daviddenton/fintrospect/ModuleRoute.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/ModuleRoute.scala
@@ -6,7 +6,7 @@ import io.github.daviddenton.fintrospect.parameters.{Parameter, PathParameter, R
 
 class ModuleRoute(val description: Description, val on: On, val basePath: Path, pathParams: Seq[PathParameter[_]]) {
   val allParams: List[(Requirement, Parameter[_])] = description.optional.map(Optional -> _) ++
-    (description.required ++ pathParams).map(Mandatory -> _)
+    (description.required ++ pathParams ++ description.body).map(Mandatory -> _)
 
   override def toString: String = (on.completeRoutePath(basePath).toString :: pathParams.map(_.toString()).toList).mkString("/")
 }
