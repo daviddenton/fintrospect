304ea1b32d0fac3e0bffdc7f2e3f442cfd375f07
david denton <david.denton@springer.com>
2015-04-06 19:21:01 +0100

    dave - docs

--- src/main/scala/io/github/daviddenton/fintrospect/FintrospectModule.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/FintrospectModule.scala
@@ -23,6 +23,9 @@ object FintrospectModule {
 
   def toService(binding: Binding): Svc = RoutingService.byMethodAndPathObject(binding)
 
+  /**
+   * Create a module using the given base-path and description renderer.
+   */
   def apply(basePath: Path, renderer: Renderer): FintrospectModule = new FintrospectModule(basePath, renderer, Nil, PartialFunction.empty[(HttpMethod, Path), Svc])
 
   private case class ValidateParams(moduleRoute: ModuleRoute) extends SimpleFilter[Request, Response]() {
@@ -45,6 +48,9 @@ object FintrospectModule {
   }
 }
 
+/**
+ * Self-describing module builder (uses the immutable builder pattern).
+ */
 class FintrospectModule private(basePath: Path, renderer: Renderer, moduleRoutes: List[ModuleRoute], private val userRoutes: Binding) {
 
   private def withDefault() = {
@@ -57,51 +63,82 @@ class FintrospectModule private(basePath: Path, renderer: Renderer, moduleRoutes
       userRoutes.orElse(bindFn(ValidateParams(moduleRoute).andThen(Identify(moduleRoute)))))
   }
 
+  /**
+   * Calls back to the Route to attach itself to the Module.
+   */
   def withRoute(route: Route): FintrospectModule = route.attachTo(this)
 
+  /**
+   * Attach described Route to the module.
+   */
   def withRoute(description: Description, on: On, fn: () => Svc) = withDescribedRoute(description, on) {
     filtered => {
       case method -> path if on.matches(method, basePath, path) => filtered.andThen(fn())
     }
   }
 
+  /**
+   * Attach described Route to the module.
+   */
   def withRoute[A](description: Description, on: On, PP0: PP[A], fn: A => Svc) = withDescribedRoute(description, on, PP0) {
     filtered => {
       case method -> path / PP0(s0) if on.matches(method, basePath, path) => filtered.andThen(fn(s0))
     }
   }
 
+  /**
+   * Attach described Route to the module.
+   */
   def withRoute[A, B](description: Description, on: On, PP0: PP[A], PP1: PP[B], fn: (A, B) => Svc) = withDescribedRoute(description, on, PP0, PP1) {
     filtered => {
       case method -> path / PP0(s0) / PP1(s1) if on.matches(method, basePath, path) => filtered.andThen(fn(s0, s1))
     }
   }
 
+  /**
+   * Attach described Route to the module.
+   */
   def withRoute[A, B, C](description: Description, on: On, PP0: PP[A], PP1: PP[B], PP2: PP[C], fn: (A, B, C) => Svc) = withDescribedRoute(description, on, PP0, PP1, PP2) {
     filtered => {
       case method -> path / PP0(s0) / PP1(s1) / PP2(s2) if on.matches(method, basePath, path) => filtered.andThen(fn(s0, s1, s2))
     }
   }
 
+  /**
+   * Attach described Route to the module.
+   */
   def withRoute[A, B, C, D](description: Description, on: On, PP0: PP[A], PP1: PP[B], PP2: PP[C], PP3: PP[D], fn: (A, B, C, D) => Svc) = withDescribedRoute(description, on, PP0, PP1, PP2, PP3) {
     filtered => {
       case method -> path / PP0(s0) / PP1(s1) / PP2(s2) / PP3(s3) if on.matches(method, basePath, path) => filtered.andThen(fn(s0, s1, s2, s3))
     }
   }
 
+  /**
+   * Attach described Route to the module.
+   */
   def withRoute[A, B, C, D, E](description: Description, on: On, PP0: PP[A], PP1: PP[B], PP2: PP[C], PP3: PP[D], PP4: PP[E], fn: (A, B, C, D, E) => Svc) = withDescribedRoute(description, on, PP0, PP1, PP2, PP3, PP4) {
     filtered => {
       case method -> path / PP0(s0) / PP1(s1) / PP2(s2) / PP3(s3) / PP4(s4) if on.matches(method, basePath, path) => filtered.andThen(fn(s0, s1, s2, s3, s4))
     }
   }
 
+  /**
+   * Attach described Route to the module.
+   */
   def withRoute[A, B, C, D, E, F](description: Description, on: On, PP0: PP[A], PP1: PP[B], PP2: PP[C], PP3: PP[D], PP4: PP[E], PP5: PP[F], fn: (A, B, C, D, E, F) => Svc) = withDescribedRoute(description, on, PP0, PP1, PP2, PP3, PP4, PP5) {
     filtered => {
       case method -> path / PP0(s0) / PP1(s1) / PP2(s2) / PP3(s3) / PP4(s4) / PP5(s5) if on.matches(method, basePath, path) => filtered.andThen(fn(s0, s1, s2, s3, s4, s5))
     }
   }
 
+  /**
+   * Finaliser for the module builder to convert itself to a Partial Function which matches incoming requests.
+   * Use this function when combining many modules together in an app.
+   */
   def routes = withDefault().userRoutes
 
+  /**
+   * Finaliser for the module builder to convert itself to a Finagle Service. Use this function when there is only one module.
+   */
   def toService = FintrospectModule.toService(routes)
 }
