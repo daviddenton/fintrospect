4581013af6e81b3c975e91a72ee149e53ae00c24
david denton <david.denton@springer.com>
2015-04-06 19:26:09 +0100

    dave - slight tidy of doc

--- src/main/scala/io/github/daviddenton/fintrospect/renderers/SimpleJson.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/renderers/SimpleJson.scala
@@ -5,9 +5,6 @@ import argo.jdom.JsonRootNode
 import io.github.daviddenton.fintrospect.util.ArgoUtil._
 import io.github.daviddenton.fintrospect.{ModuleRoute, Renderer}
 
-/**
- * Ultra-basic renderer implementation that only supports the route paths and the main descriptions of each.
- */
 class SimpleJson private() extends Renderer {
   private def render(mr: ModuleRoute): Field = {
     mr.on.method + ":" + mr.toString -> string(mr.description.name)
@@ -16,6 +13,9 @@ class SimpleJson private() extends Renderer {
   def apply(mr: Seq[ModuleRoute]): JsonRootNode = obj("resources" -> obj(mr.map(render)))
 }
 
+/**
+ * Ultra-basic Renderer implementation that only supports the route paths and the main descriptions of each.
+ */
 object SimpleJson {
   def apply(): Renderer = new SimpleJson()
 }
