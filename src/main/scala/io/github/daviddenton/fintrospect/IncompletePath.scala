2c50ffd87ae65a06c9622f5ad400a7119dc469ee
david denton <david.denton@springer.com>
2015-04-11 18:38:18 +0100

    dave - new route replaces old

--- src/main/scala/io/github/daviddenton/fintrospect/IncompletePath.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/IncompletePath.scala
@@ -2,24 +2,11 @@ package io.github.daviddenton.fintrospect
 
 import com.twitter.finagle.http.path.{->, /, Path}
 import io.github.daviddenton.fintrospect.FintrospectModule.{FF, Svc}
-import io.github.daviddenton.fintrospect.parameters.{Path => Fp, Parameter, Requirement, PathParameter}
+import io.github.daviddenton.fintrospect.parameters.{Path => Fp, PathParameter}
 import org.jboss.netty.handler.codec.http.HttpMethod
 
 object IncompletePath {
   def apply(description: Description, method: HttpMethod): IncompletePath0 = IncompletePath0(description, method, identity)
-}
-
-abstract class CompletePath(val description: Description, val method: HttpMethod, pathFn: Path => Path, val pathParams: PathParameter[_]*) {
-
-  val allParams: List[(Requirement, Parameter[_])] = {
-    (description.params ++ pathParams.flatMap(identity)).map(p => p.requirement -> p)
-  }
-
-  def matches(actualMethod: HttpMethod, basePath: Path, actualPath: Path) = actualMethod == method && actualPath == pathFn(basePath)
-
-  def toPf(basePath: Path): (FF) => PartialFunction[(HttpMethod, Path), Svc]
-
-  def describeFor(basePath: Path): String = (pathFn(basePath).toString :: pathParams.map(_.toString()).toList).mkString("/")
 }
 
 trait IncompletePath {
@@ -33,7 +20,7 @@ case class IncompletePath0(description: Description, method: HttpMethod, pathFn:
 
   def /[T](pp0: PathParameter[T]) = IncompletePath1(description, method, pathFn, pp0)
 
-  def then(fn: () => Svc): CompletePath = new CompletePath(description, method, pathFn) {
+  def then(fn: () => Svc): Route = new Route(description, method, pathFn) {
     override def toPf(basePath: Path): (FF) => PartialFunction[(HttpMethod, Path), Svc] = {
       filtered: FF => {
         case actualMethod -> path if matches(actualMethod, basePath, path) => filtered.andThen(fn())
@@ -48,7 +35,7 @@ case class IncompletePath1[A](description: Description, method: HttpMethod, path
 
   def /[B](pp2: PathParameter[B]): IncompletePath2[A, B] = IncompletePath2(description, method, pathFn, pp1, pp2)
 
-  def then(fn: (A) => Svc): CompletePath = new CompletePath(description, method, pathFn, pp1) {
+  def then(fn: (A) => Svc): Route = new Route(description, method, pathFn, pp1) {
     override def toPf(basePath: Path): (FF) => PartialFunction[(HttpMethod, Path), Svc] = {
       filtered: FF => {
         case actualMethod -> path / pp1(s1) if matches(actualMethod, basePath, path) => filtered.andThen(fn(s1))
@@ -64,7 +51,7 @@ case class IncompletePath2[A, B](description: Description, method: HttpMethod, p
 
   def /[C](pp3: PathParameter[C]): IncompletePath3[A, B, C] = IncompletePath3(description, method, pathFn, pp1, pp2, pp3)
 
-  def then(fn: (A, B) => Svc): CompletePath = new CompletePath(description, method, pathFn, pp1, pp2) {
+  def then(fn: (A, B) => Svc): Route = new Route(description, method, pathFn, pp1, pp2) {
     override def toPf(basePath: Path): (FF) => PartialFunction[(HttpMethod, Path), Svc] = {
       filtered: FF => {
         case actualMethod -> path / pp1(s1) / pp2(s2) if matches(actualMethod, basePath, path) => filtered.andThen(fn(s1, s2))
@@ -81,7 +68,7 @@ case class IncompletePath3[A, B, C](description: Description, method: HttpMethod
 
   def /[D](pp4: PathParameter[D]): IncompletePath4[A, B, C, D] = IncompletePath4(description, method, pathFn, pp1, pp2, pp3, pp4)
 
-  def then(fn: (A, B, C) => Svc): CompletePath = new CompletePath(description, method, pathFn, pp1, pp2, pp3) {
+  def then(fn: (A, B, C) => Svc): Route = new Route(description, method, pathFn, pp1, pp2, pp3) {
     override def toPf(basePath: Path): (FF) => PartialFunction[(HttpMethod, Path), Svc] = {
       filtered: FF => {
         case actualMethod -> path / pp1(s1) / pp2(s2) / pp3(s3) if matches(actualMethod, basePath, path) => filtered.andThen(fn(s1, s2, s3))
@@ -100,7 +87,7 @@ case class IncompletePath4[A, B, C, D](description: Description, method: HttpMet
 
   def /[E](pp5: PathParameter[E]): IncompletePath5[A, B, C, D, E] = IncompletePath5(description, method, pathFn, pp1, pp2, pp3, pp4, pp5)
 
-  def then(fn: (A, B, C, D) => Svc): CompletePath = new CompletePath(description, method, pathFn, pp1, pp2, pp3, pp4) {
+  def then(fn: (A, B, C, D) => Svc): Route = new Route(description, method, pathFn, pp1, pp2, pp3, pp4) {
     override def toPf(basePath: Path): (FF) => PartialFunction[(HttpMethod, Path), Svc] = {
       filtered: FF => {
         case actualMethod -> path / pp1(s1) / pp2(s2) / pp3(s3) / pp4(s4) if matches(actualMethod, basePath, path) => filtered.andThen(fn(s1, s2, s3, s4))
@@ -118,7 +105,7 @@ case class IncompletePath5[A, B, C, D, E](description: Description, method: Http
                                         ) extends IncompletePath {
   def /[F](pp5: PathParameter[F]) = throw new UnsupportedOperationException("Limit on number of elements!")
 
-  def then(fn: (A, B, C, D, E) => Svc): CompletePath = new CompletePath(description, method, pathFn, pp1, pp2, pp3, pp4, pp5) {
+  def then(fn: (A, B, C, D, E) => Svc): Route = new Route(description, method, pathFn, pp1, pp2, pp3, pp4, pp5) {
     override def toPf(basePath: Path): (FF) => PartialFunction[(HttpMethod, Path), Svc] = {
       filtered: FF => {
         case actualMethod -> path / pp1(s1) / pp2(s2) / pp3(s3) / pp4(s4) / pp5(s5) if matches(actualMethod, basePath, path) => filtered.andThen(fn(s1, s2, s3, s4, s5))
--- src/main/scala/io/github/daviddenton/fintrospect/ModuleRoute.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/ModuleRoute.scala
@@ -3,8 +3,8 @@ package io.github.daviddenton.fintrospect
 import com.twitter.finagle.http.path.Path
 import io.github.daviddenton.fintrospect.parameters.{Parameter, Requirement}
 
-class ModuleRoute(val completedPath: CompletePath, val basePath: Path) {
-  val description: Description = completedPath.description
-  val allParams: List[(Requirement, Parameter[_])] = completedPath.allParams
-  override def toString: String = completedPath.describeFor(basePath)
+class ModuleRoute(val route: Route, val basePath: Path) {
+  val description: Description = route.description
+  val allParams: List[(Requirement, Parameter[_])] = route.allParams
+  override def toString: String = route.describeFor(basePath)
 }
