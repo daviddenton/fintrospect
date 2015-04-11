615d8fe1f99f5878c8332ae7df79ca356b30e213
David Denton <denton.david@gmail.com>
2015-04-11 19:57:46 +0100

    dave - removed ModuleRoute as was legacy fluff

--- src/main/scala/io/github/daviddenton/fintrospect/ModuleRoute.scala
+++
@@ -1,7 +0,0 @@
-package io.github.daviddenton.fintrospect
-
-import com.twitter.finagle.http.path.Path
-
-class ModuleRoute(val route: Route, val basePath: Path) {
-  override def toString: String = route.describeFor(basePath)
-}
--- src/main/scala/io/github/daviddenton/fintrospect/Renderer.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/Renderer.scala
@@ -1,10 +1,11 @@
 package io.github.daviddenton.fintrospect
 
 import argo.jdom.JsonRootNode
+import com.twitter.finagle.http.path.Path
 
 /**
  * Contract trait for the pluggable Renderers (Swagger etc..)
  */
 trait Renderer {
-  def apply(routes: Seq[ModuleRoute]): JsonRootNode
+  def apply(basePath: Path, routes: Seq[Route]): JsonRootNode
 }
