d75a3328a77dfeda2ff16bcd4e3193f1c56ae253
David Denton <denton.david@gmail.com>
2015-05-17 15:51:07 +0100

    dave - refactor of form params

--- src/main/scala/io/github/daviddenton/fintrospect/parameters/Form.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/parameters/Form.scala
@@ -1,17 +1,23 @@
 package io.github.daviddenton.fintrospect.parameters
 
+import java.util.{List => JList}
+
+import com.twitter.io.Charsets
 import io.github.daviddenton.fintrospect.FinagleTypeAliases.FTRequest
-import io.github.daviddenton.fintrospect.RequestParams
+import org.jboss.netty.handler.codec.http.QueryStringDecoder
+
+import scala.util.Try
 
 /**
  * Builder for parameters that are encoded in the HTTP form.
  */
 object Form {
   private val location = new Location {
-    override def toString = "query"
+    override def toString = "form"
 
     override def from(name: String, request: FTRequest): Option[String] = {
-      new RequestParams(request).get(name)
+      val params = Try(new QueryStringDecoder("?" + request.getContent.toString(Charsets.Utf8)).getParameters).toOption.getOrElse(new java.util.HashMap[String, JList[String]])
+      Option(params.get(name)).map(_.get(0))
     }
   }
 
--- src/test/scala/io/github/daviddenton/fintrospect/parameters/RequiredFormTest.scala
+++ src/test/scala/io/github/daviddenton/fintrospect/parameters/RequiredFormTest.scala
@@ -1,9 +1,17 @@
 package io.github.daviddenton.fintrospect.parameters
 
-import com.twitter.finagle.http.Request
+import com.twitter.finagle.http.{MediaType, Request}
+import org.jboss.netty.handler.codec.http.QueryStringEncoder
 
 class RequiredFormTest extends JsonSupportingParametersTest[RequiredRequestParameter](Form.required) {
   override def from[X](param: RequiredRequestParameter[X], value: String): Option[X] = {
-    scala.util.Try(param.from(Request(paramName -> value))).toOption
+    val request = Request()
+    request.setContentType(MediaType.WwwForm)
+    request.setContentString(new QueryStringEncoder("") {
+      {
+        addParam(paramName, value)
+      }
+    }.toUri.getRawQuery)
+    scala.util.Try(param.from(request)).toOption
   }
 }
