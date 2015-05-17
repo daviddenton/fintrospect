174f03da9954115099fb974300a81bab42a1f436
David Denton <denton.david@gmail.com>
2015-05-17 15:51:07 +0100

    dave - refactor of form params

--- src/main/scala/io/github/daviddenton/fintrospect/parameters/Query.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/parameters/Query.scala
@@ -1,7 +1,11 @@
 package io.github.daviddenton.fintrospect.parameters
 
+import java.util.{List => JList}
+
 import io.github.daviddenton.fintrospect.FinagleTypeAliases.FTRequest
-import io.github.daviddenton.fintrospect.RequestParams
+import org.jboss.netty.handler.codec.http.QueryStringDecoder
+
+import scala.util.Try
 
 /**
  * Builder for parameters that are encoded in the HTTP query.
@@ -11,7 +15,11 @@ object Query {
     override def toString = "query"
 
     override def from(name: String, request: FTRequest): Option[String] = {
-      new RequestParams(request).get(name)
+      Option(parseParams(request.getUri).get(name)).map(_.get(0))
+    }
+
+    private def parseParams(s: String) = {
+      Try(new QueryStringDecoder(s).getParameters).toOption.getOrElse(new java.util.HashMap[String, JList[String]])
     }
   }
 
