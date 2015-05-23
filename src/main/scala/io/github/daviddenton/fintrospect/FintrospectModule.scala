0f4ef8b3b358a87c4790121a3c2157a74c74ac70
David Denton <denton.david@gmail.com>
2015-05-24 00:19:41 +0100

    dave - moved bad request onto TypedRequestBuilder

--- src/main/scala/io/github/daviddenton/fintrospect/FintrospectModule.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/FintrospectModule.scala
@@ -1,11 +1,14 @@
 package io.github.daviddenton.fintrospect
 
+import argo.jdom.JsonRootNode
 import com.twitter.finagle.http.path.Path
 import com.twitter.finagle.{Service, SimpleFilter}
 import com.twitter.util.Future
 import io.github.daviddenton.fintrospect.FintrospectModule._
 import io.github.daviddenton.fintrospect.Routing.fromBinding
 import io.github.daviddenton.fintrospect.parameters.Requirement._
+import io.github.daviddenton.fintrospect.util.ResponseBuilder._
+import io.github.daviddenton.fintrospect.util.{ResponseBuilder, TypedRespBuilder}
 import org.jboss.netty.handler.codec.http.HttpMethod.GET
 import org.jboss.netty.handler.codec.http.{HttpMethod, HttpRequest, HttpResponse}
 
@@ -30,16 +33,16 @@ object FintrospectModule {
   /**
    * Create a module using the given base-path and description descRenderer.
    */
-  def apply(basePath: Path, descRenderer: Renderer, responseRenderer: ModuleResponseRenderer = ModuleResponseRenderer.Json): FintrospectModule = {
+  def apply(basePath: Path, descRenderer: Renderer, responseRenderer: TypedRespBuilder[JsonRootNode] = ResponseBuilder.Json): FintrospectModule = {
     new FintrospectModule(basePath, descRenderer, responseRenderer, Nil, empty[(HttpMethod, Path), Service[HttpRequest, HttpResponse]])
   }
 
-  private case class ValidateParams(route: Route, responseRenderer: ModuleResponseRenderer) extends SimpleFilter[HttpRequest, HttpResponse]() {
+  private case class ValidateParams(route: Route, responseRenderer: TypedRespBuilder[JsonRootNode]) extends SimpleFilter[HttpRequest, HttpResponse]() {
     override def apply(request: HttpRequest, service: Service[HttpRequest, HttpResponse]): Future[HttpResponse] = {
       val paramsAndParseResults = route.describedRoute.params.map(p => (p, p.parseFrom(request)))
       val withoutMissingOptionalParams = paramsAndParseResults.filterNot(pr => pr._1.requirement == Optional && pr._2.isEmpty)
       val missingOrFailed = withoutMissingOptionalParams.filterNot(pr => pr._2.isDefined && pr._2.get.isSuccess).map(_._1)
-      if (missingOrFailed.isEmpty) service(request) else Future.value(responseRenderer.badRequest(missingOrFailed))
+      if (missingOrFailed.isEmpty) service(request) else responseRenderer.BadRequest(missingOrFailed)
     }
   }
 
@@ -55,9 +58,9 @@ object FintrospectModule {
 /**
  * Self-describing module builder (uses the immutable builder pattern).
  */
-class FintrospectModule private(basePath: Path, descRenderer: Renderer, responseRenderer: ModuleResponseRenderer, theRoutes: List[Route], private val currentBinding: Binding) {
+class FintrospectModule private(basePath: Path, descRenderer: Renderer, responseRenderer: TypedRespBuilder[JsonRootNode], theRoutes: List[Route], private val currentBinding: Binding) {
   private def withDefault() = withRoute(DescribedRoute("Description route").at(GET).bindTo(() => {
-    Service.mk((req) => Future.value(Renderer.toResponse(descRenderer(basePath, theRoutes))))
+    Service.mk((req) => Renderer.toResponse(descRenderer(basePath, theRoutes)))
   }))
 
   private def totalBinding = withDefault().currentBinding
