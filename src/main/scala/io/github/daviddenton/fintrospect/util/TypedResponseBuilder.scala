cca3c106e754c067f7ba1334c1454fd7dbbbc3b7
David Denton <denton.david@gmail.com>
2015-05-24 17:47:38 +0100

    dave - extracting module builder

--- src/main/scala/io/github/daviddenton/fintrospect/util/TypedResponseBuilder.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/util/TypedResponseBuilder.scala
@@ -1,30 +1,29 @@
 package io.github.daviddenton.fintrospect.util
 
-import io.github.daviddenton.fintrospect.ContentType
 import io.github.daviddenton.fintrospect.parameters.RequestParameter
+import org.jboss.netty.handler.codec.http.HttpResponseStatus._
 import org.jboss.netty.handler.codec.http.{HttpResponse, HttpResponseStatus}
 
 import scala.language.implicitConversions
 
-class TypedResponseBuilder[T](toFormat: T => String,
-                              contentType: ContentType,
+class TypedResponseBuilder[T](newBuilder: () => ResponseBuilder[T],
                               msgToError: String => T,
                               exToError: Throwable => T,
                               bpToError: List[RequestParameter[_]] => T) {
 
-  def apply(): ResponseBuilder[T] = new ResponseBuilder[T](toFormat, contentType)
+  def apply(): ResponseBuilder[T] = newBuilder()
 
-  def Ok: HttpResponse = apply().withCode(HttpResponseStatus.OK).build
+  def Ok: HttpResponse = apply().withCode(OK).build
 
-  def Ok(content: T): HttpResponse = Ok(toFormat(content))
+  def Ok(content: T): HttpResponse = apply().withCode(OK).withContent(content).build
 
-  def Ok(content: String): HttpResponse = apply().withCode(HttpResponseStatus.OK).withContent(content).build
+  def Ok(content: String): HttpResponse = apply().withCode(OK).withContent(content).build
 
-  def Error(status: HttpResponseStatus, message: String): HttpResponse = apply().withCode(status).withContent(toFormat(msgToError(message))).build
+  def Error(status: HttpResponseStatus, message: String): HttpResponse = apply().withCode(status).withContent(msgToError(message)).build
 
-  def Error(status: HttpResponseStatus, content: T): HttpResponse = apply().withCode(status).withContent(toFormat(content)).build
+  def Error(status: HttpResponseStatus, content: T): HttpResponse = apply().withCode(status).withContent(content).build
 
   def Error(status: HttpResponseStatus, error: Throwable): HttpResponse = Error(status, exToError(error))
 
-  def BadRequest(badParameters: List[RequestParameter[_]]): HttpResponse = Error(HttpResponseStatus.BAD_REQUEST, bpToError(badParameters))
+  def BadRequest(badParameters: List[RequestParameter[_]]): HttpResponse = Error(BAD_REQUEST, bpToError(badParameters))
 }
