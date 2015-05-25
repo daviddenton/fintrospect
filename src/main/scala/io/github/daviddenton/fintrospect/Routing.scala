a7b6fc576982c425c53f735c507b82befad9004a
David Denton <denton.david@gmail.com>
2015-05-25 19:50:52 +0100

    daveD - updated message to be friendly

--- src/main/scala/io/github/daviddenton/fintrospect/Routing.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/Routing.scala
@@ -1,17 +1,17 @@
 package io.github.daviddenton.fintrospect
 
 import com.twitter.finagle.Service
-import com.twitter.finagle.http.Response
 import com.twitter.finagle.http.path.Path
 import com.twitter.util.Future
+import io.github.daviddenton.fintrospect.util.JsonResponseBuilder._
 import io.github.daviddenton.fintrospect.util.ResponseBuilder._
-import org.jboss.netty.handler.codec.http.HttpResponseStatus._
+import org.jboss.netty.handler.codec.http.HttpResponseStatus.NOT_FOUND
 import org.jboss.netty.handler.codec.http.{HttpMethod, HttpRequest, HttpResponse}
 
 class Routing private(routes: PartialFunction[HttpRequest, Service[HttpRequest, HttpResponse]]) extends Service[HttpRequest, HttpResponse] {
   private val notFoundPf: PartialFunction[HttpRequest, Service[HttpRequest, HttpResponse]] = {
     case _ => new Service[HttpRequest, HttpResponse] {
-      def apply(request: HttpRequest): Future[HttpResponse] = Response(NOT_FOUND)
+      def apply(request: HttpRequest): Future[HttpResponse] = Error(NOT_FOUND, "No route found on this path. Have you used the correct HTTP verb?")
     }
   }
   private val requestToService = routes orElse notFoundPf
