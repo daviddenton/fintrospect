de6f019d295f8b4f7cc5ba53df4a840a8235247a
David Denton <denton.david@gmail.com>
2015-05-25 09:33:00 +0100

    dave -  removed error example

--- src/main/scala/io/github/daviddenton/fintrospect/ResponseWithExample.scala
+++ src/main/scala/io/github/daviddenton/fintrospect/ResponseWithExample.scala
@@ -1,11 +1,7 @@
 package io.github.daviddenton.fintrospect
 
 import argo.jdom.JsonNode
-import io.github.daviddenton.fintrospect.util.ArgoUtil._
 import org.jboss.netty.handler.codec.http.HttpResponseStatus
 
 case class ResponseWithExample(status: HttpResponseStatus, description: String, example: JsonNode = null)
 
-object ResponseWithExample {
-  val ERROR_EXAMPLE = obj("message" -> string("an error message goes here"))
-}
