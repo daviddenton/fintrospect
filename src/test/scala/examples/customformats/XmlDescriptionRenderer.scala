7c6bb4be7b0eefc3a4aaded7e0cc253a367b232f
David Denton <denton.david@gmail.com>
2015-05-30 06:46:04 +0100

    daveD - ModuleRenderer.scala now takes a DescriptionRenderer that can build a total response, so you can vary the content type between the routes and the description route

--- src/test/scala/examples/customformats/XmlDescriptionRenderer.scala
+++ src/test/scala/examples/customformats/XmlDescriptionRenderer.scala
@@ -3,11 +3,12 @@ package examples.customformats
 import com.twitter.finagle.http.path.Path
 import io.github.daviddenton.fintrospect.Route
 import io.github.daviddenton.fintrospect.renderers.DescriptionRenderer
+import org.jboss.netty.handler.codec.http.HttpResponse
 
-class XmlDescriptionRenderer () extends DescriptionRenderer[String] {
+class XmlDescriptionRenderer extends DescriptionRenderer[HttpResponse] {
   private def renderRoute(basePath: Path, route: Route): XmlFormat = XmlFormat( s"<entry>${route.method}:${route.describeFor(basePath)}</entry>")
 
   private def renderRoutes(basePath: Path, routes: Seq[Route]): String = XmlFormat(routes.map(renderRoute(basePath, _)): _*).toString()
 
-  def apply(basePath: Path, routes: Seq[Route]): String = XmlFormat( s"<paths>${renderRoutes(basePath, routes)}</paths>").value
+  def apply(basePath: Path, routes: Seq[Route]): HttpResponse = XmlResponseBuilder.Ok(XmlFormat( s"<paths>${renderRoutes(basePath, routes)}</paths>").value)
 }
