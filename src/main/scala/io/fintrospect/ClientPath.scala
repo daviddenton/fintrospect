0a6493e544355be6440e90babe6c375a9f188ad5
David Denton <denton.david@gmail.com>
2015-06-03 20:07:33 +0100

    daveD - started to add routing

--- src/main/scala/io/fintrospect/ClientPath.scala
+++ src/main/scala/io/fintrospect/ClientPath.scala
@@ -1,56 +1,64 @@
 package io.fintrospect
 
+import com.twitter.finagle.Service
+import com.twitter.finagle.http.Request
 import com.twitter.finagle.http.path.{Path => FPath}
+import com.twitter.util.Future
+import io.fintrospect.ClientPath.Bob
 import io.fintrospect.parameters.{Path, PathParameter}
+import org.jboss.netty.handler.codec.http.{HttpRequest, HttpResponse}
 
-case class ClientPath[R](route: R, pathFn: FPath => FPath) {
-  def /(part: String): ClientPath[R] = ClientPath(route, pathFn.andThen(p => p / part))
-
-  def /[A](pp0: PathParameter[A]) = ClientPath1(route, pathFn, pp0)
-
-  def toService = route
+object ClientPath {
+  type Bob = (Service[HttpRequest, HttpResponse]) => Future[HttpResponse]
 }
 
-case class ClientPath1[R, A](route: R, pathFn: FPath => FPath, pp0: PathParameter[A]) {
-  def /(part: String) = /(Path.fixed(part))
+case class ClientPath[R](route: R, path: FPath) {
+  def /(part: String): ClientPath[R] = ClientPath(route, path / part)
 
-  def /[B](pp1: PathParameter[B]) = ClientPath2(route, pathFn, pp0, pp1)
+  def /[A](pp0: PathParameter[A]): ClientPath1[R, A] = ClientPath1(route, path, pp0)
 
-  def toService = {
-
-    route
-  }
+  def apply(): Bob = _(Request(path.toString))
 }
 
-case class ClientPath2[R, A, B](route: R, pathFn: FPath => FPath,
+case class ClientPath1[R, A](route: R, path: FPath, pp0: PathParameter[A]) {
+  def /(part: String): ClientPath2[R, A, String] = /(Path.fixed(part))
+
+  def /[B](pp1: PathParameter[B]): ClientPath2[R, A, B] = ClientPath2(route, path, pp0, pp1)
+
+  private def pathFor(a: A) = path / pp0(a)
+
+  def apply(a: A): Bob = _(Request((path / pp0(a)).toString()))
+}
+
+case class ClientPath2[R, A, B](route: R, path: FPath,
                                 pp0: PathParameter[A],
                                 pp1: PathParameter[B]
                                  ) {
-  def /(part: String) = /(Path.fixed(part))
+  def /(part: String): ClientPath3[R, A, B, String] = /(Path.fixed(part))
 
-  def /[C](pp2: PathParameter[C]) = ClientPath3(route, pathFn, pp0, pp1, pp2)
+  def /[C](pp2: PathParameter[C]): ClientPath3[R, A, B, C] = ClientPath3(route, path, pp0, pp1, pp2)
 
-  def toService = ???
+  def apply(a: A, b: B): Bob = _(Request((path / pp0(a) / pp1(b)).toString()))
 }
 
-case class ClientPath3[R, A, B, C](route: R, pathFn: FPath => FPath,
+case class ClientPath3[R, A, B, C](route: R, path: FPath,
                                    pp0: PathParameter[A],
                                    pp1: PathParameter[B],
                                    pp2: PathParameter[C]
                                     ) {
-  def /(part: String) = /(Path.fixed(part))
+  def /(part: String): ClientPath4[R, A, B, C, String] = /(Path.fixed(part))
 
-  def /[D](pp3: PathParameter[D]) = ClientPath4(route, pathFn, pp0, pp1, pp2, pp3)
+  def /[D](pp3: PathParameter[D]): ClientPath4[R, A, B, C, D] = ClientPath4(route, path, pp0, pp1, pp2, pp3)
 
-  def toService = ???
+  def apply(a: A, b: B, c: C): Bob = _(Request((path / pp0(a) / pp1(b) / pp2(c)).toString()))
 }
 
 case class ClientPath4[R, A, B, C, D](route: R,
-                                      pathFn: FPath => FPath,
+                                      path: FPath,
                                       pp0: PathParameter[A],
                                       pp1: PathParameter[B],
                                       pp2: PathParameter[C],
                                       pp3: PathParameter[D]
                                        ) {
-  def toService = ???
+  def apply(a: A, b: B, c: C, d: D): Bob = _(Request((path / pp0(a) / pp1(b) / pp2(c) / pp3(d)).toString()))
 }
