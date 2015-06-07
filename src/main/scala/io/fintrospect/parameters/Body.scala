5bfd59a3509d34a84393d22d6043b4cb0554b327
David Denton <denton.david@gmail.com>
2015-06-07 20:59:53 +0100

    dave - removed duplication

--- src/main/scala/io/fintrospect/parameters/Body.scala
+++ src/main/scala/io/fintrospect/parameters/Body.scala
@@ -7,14 +7,11 @@ import org.jboss.netty.handler.codec.http.HttpRequest
 
 import scala.util.Try
 
-class Body private(val description: Option[String], val paramType: ParamType, val example: JsonNode, val parse: (String => Option[JsonNode])) extends Parameter[JsonNode] {
+class Body private(val description: Option[String], val paramType: ParamType, val example: JsonNode, val parse: (String => Option[JsonNode]))
+  extends Parameter[JsonNode] with Mandatory[JsonNode] {
   override val name = "body"
   override val where = "body"
-  override val requirement = Requirement.Mandatory
-
-  def from(request: HttpRequest): JsonNode = parse(request.getContent.toString(Charsets.Utf8)).get
-
-  override def parseFrom(request: HttpRequest): Option[Try[JsonNode]] = Some(Try(from(request)))
+  override def parseFrom(request: HttpRequest): Option[Try[JsonNode]] = Some(Try(parse(request.getContent.toString(Charsets.Utf8)).get))
 }
 
 object Body {
