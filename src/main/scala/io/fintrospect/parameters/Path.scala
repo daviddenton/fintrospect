403bcc3b9d6122523e6e2278af282ff29c13b059
David Denton <denton.david@gmail.com>
2015-06-07 20:41:10 +0100

    dave - refactoring parameters

--- src/main/scala/io/fintrospect/parameters/Path.scala
+++ src/main/scala/io/fintrospect/parameters/Path.scala
@@ -13,11 +13,7 @@ object Path extends Parameters[PathParameter, Mandatory](PathParameter.builder) 
    * A special path segment that is defined, but has no intrinsic value other than for route matching. Useful when embedded
    * between 2 other path parameters. eg. /myRoute/{id}/aFixedPart/{subId}
    */
-  def fixed(aName: String): PathParameter[String] = new PathParameter[String] with Mandatory[String] {
-    override val name = aName
-    override val description = None
-    override val paramType = StringParamType
-
+  def fixed(aName: String): PathParameter[String] = new PathParameter[String](aName, None, StringParamType) with Mandatory[String] {
     override def toString() = name
 
     override def unapply(str: String): Option[String] = if (str == name) Some(str) else None
--- src/main/scala/io/fintrospect/parameters/RequiredRequestParameter.scala
+++ src/main/scala/io/fintrospect/parameters/RequiredRequestParameter.scala
@@ -4,11 +4,7 @@ import scala.util.Try
 
 object RequiredRequestParameter {
   def builder(location: Location) = () => new ParameterBuilder[RequestParameter, Mandatory]() {
-    def apply[T](aName: String, aDescription: Option[String], aParamType: ParamType, parse: (String => Try[T])) =
-      new RequestParameter[T](location, parse) with Mandatory[T] {
-        override val name = aName
-        override val description = aDescription
-        override val paramType = aParamType
-      }
+    def apply[T](name: String, description: Option[String], paramType: ParamType, parse: (String => Try[T])) =
+      new RequestParameter[T](name, description, paramType, location, parse) with Mandatory[T]
   }
 }
