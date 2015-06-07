208b5104fd85b530ffc051569adec3bd9c1f665f
David Denton <denton.david@gmail.com>
2015-06-07 20:49:07 +0100

    dave -refactor towards simplicity - through duplication (for the mo :()

--- src/main/scala/io/fintrospect/parameters/Header.scala
+++ src/main/scala/io/fintrospect/parameters/Header.scala
@@ -2,6 +2,8 @@ package io.fintrospect.parameters
 
 import org.jboss.netty.handler.codec.http.HttpRequest
 
+import scala.util.Try
+
 /**
  * Builder for parameters that are encoded as HTTP headers.
  */
@@ -12,6 +14,18 @@ object Header {
     override def from(name: String, request: HttpRequest): Option[String] = Option(request.headers().get(name))
   }
 
-  val required = new Parameters(RequiredRequestParameter.builder(location))
-  val optional = new Parameters(OptionalRequestParameter.builder(location))
-}
+  val required = new Parameters[RequestParameter, Mandatory] {
+    override protected def builder(): ParameterBuilder[RequestParameter, Mandatory] =
+      new ParameterBuilder[RequestParameter, Mandatory]() {
+        def apply[T](name: String, description: Option[String], paramType: ParamType, parse: (String => Try[T])) =
+          new RequestParameter[T](name, description, paramType, location, parse) with Mandatory[T]
+      }
+  }
+
+  val optional =  new Parameters[RequestParameter, Optional] {
+    override protected def builder(): ParameterBuilder[RequestParameter, Optional] =
+      new ParameterBuilder[RequestParameter, Optional]() {
+        def apply[T](name: String, description: Option[String], paramType: ParamType, parse: (String => Try[T])) =
+          new RequestParameter[T](name, description, paramType, location, parse) with Optional[T]
+      }
+  }}
