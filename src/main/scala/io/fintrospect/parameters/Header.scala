ce4d78355f3c7b165b9b050e2f42af4ffb45bf33
David Denton <denton.david@gmail.com>
2015-06-08 13:45:40 +0100

    dave - reresimplified

--- src/main/scala/io/fintrospect/parameters/Header.scala
+++ src/main/scala/io/fintrospect/parameters/Header.scala
@@ -9,30 +9,19 @@ import scala.util.Try
  */
 object Header {
 
-  private val aLocation = new Location {
+  private val location = new Location {
     override def toString = "header"
 
     override def from(name: String, request: HttpRequest): Option[String] = Option(request.headers().get(name))
   }
 
   val required = new Parameters[MandatoryRequestParameter] {
-    override protected def parameter[T](aName: String, aDescription: Option[String], aParamType: ParamType, parse: (String => Try[T])) =
-      new MandatoryRequestParameter[T](parse) {
-        val name = aName
-        val location = aLocation
-        val description = aDescription
-        val paramType = aParamType
-      }
+    override protected def parameter[T](name: String, description: Option[String], paramType: ParamType, parse: (String => Try[T])) =
+      new MandatoryRequestParameter[T](name, location, description, paramType, parse)
   }
 
   val optional = new Parameters[OptionalRequestParameter] {
-    override protected def parameter[T](aName: String, aDescription: Option[String], aParamType: ParamType, parse: (String => Try[T])) =
-      new OptionalRequestParameter[T](parse) {
-        val name = aName
-        val location = aLocation
-        val description = aDescription
-        val paramType = aParamType
-
-      }
+    override protected def parameter[T](name: String, description: Option[String], paramType: ParamType, parse: (String => Try[T])) =
+      new OptionalRequestParameter[T](name, location, description, paramType, parse)
   }
 }
