3cc5babe3422a17142cde630977eb92ed582f5d9
David Denton <denton.david@gmail.com>
2015-06-08 17:41:15 +0100

    dave - added client service identification functionality

--- src/main/scala/io/fintrospect/clients/Client.scala
+++ src/main/scala/io/fintrospect/clients/Client.scala
@@ -1,11 +1,26 @@
 package io.fintrospect.clients
 
-import com.twitter.finagle.Service
+import com.twitter.finagle.{Service, SimpleFilter}
 import com.twitter.util.Future
+import io.fintrospect.Headers._
+import io.fintrospect.clients.Client.Identify
 import io.fintrospect.parameters.{Parameter, PathParameter, RequestParameter}
 import io.fintrospect.util.PlainTextResponseBuilder._
 import org.jboss.netty.handler.codec.http.HttpResponseStatus._
 import org.jboss.netty.handler.codec.http._
+
+object Client {
+  private case class Identify(method: HttpMethod, pathParams: List[PathParameter[_]]) extends SimpleFilter[HttpRequest, HttpResponse]() {
+    private val description = method + "." + pathParams.map(_.toString()).mkString("/")
+
+    override def apply(request: HttpRequest, service: Service[HttpRequest, HttpResponse]): Future[HttpResponse] = {
+      service(request).map { response =>
+        response.headers().set(IDENTIFY_SVC_HEADER, description)
+        response
+      }
+    }
+  }
+}
 
 class Client(method: HttpMethod,
              requestParams: List[RequestParameter[_]],
@@ -16,6 +31,7 @@ class Client(method: HttpMethod,
   private val requiredParams = allPossibleParams.filter(_.required)
   private val queryParams = requestParams.filter(_.where == "query")
   private val headerParams = requestParams.filter(_.where == "header")
+  private val identify = Identify(method, pathParams)
 
   def apply(userSuppliedParams: (Parameter[_], String)*): Future[HttpResponse] = {
     val allSuppliedParams = Map(userSuppliedParams: _*) ++ systemSuppliedParams
@@ -30,7 +46,8 @@ class Client(method: HttpMethod,
     }
     val request = new DefaultHttpRequest(HttpVersion.HTTP_1_1, method, buildUrl(allSuppliedParams))
     headerParams.filter(allSuppliedParams.contains).foreach(p => p.into(request, allSuppliedParams(p)))
-    service(request)
+
+    identify.apply(request, service)
   }
 
   def buildUrl(allSuppliedParams: Map[Parameter[_], String]): String = {
