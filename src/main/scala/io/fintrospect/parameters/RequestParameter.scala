ec33144e7725877ee1262117f21d6571b6bca4ba
David Denton <denton.david@gmail.com>
2015-06-08 14:03:57 +0100

    dave - removed parse from RequestPArameter as not used

--- src/main/scala/io/fintrospect/parameters/RequestParameter.scala
+++ src/main/scala/io/fintrospect/parameters/RequestParameter.scala
@@ -2,9 +2,9 @@ package io.fintrospect.parameters
 
 import org.jboss.netty.handler.codec.http.HttpRequest
 
-import scala.util.{Success, Try}
+import scala.util.Try
 
-abstract class RequestParameter[T, X](parse: (String => Try[T])) extends Parameter[T] {
+abstract class RequestParameter[T, X] extends Parameter[T] {
   def from(request: HttpRequest): T
 
   def parseFrom(request: HttpRequest): Option[Try[X]]
@@ -15,7 +15,7 @@ class OptionalRequestParameter[T](val name: String,
                                   val description: Option[String],
                                   val paramType: ParamType,
                                   parse: (String => Try[T]))
-  extends RequestParameter[Option[T], T](s => Success(parse(s).toOption)) {
+  extends RequestParameter[Option[T], T] {
 
   override val required = false
 
@@ -28,7 +28,7 @@ class MandatoryRequestParameter[T](val name: String,
                                    val location: Location,
                                    val description: Option[String],
                                    val paramType: ParamType,
-                                   parse: (String => Try[T])) extends RequestParameter[T, T](parse) {
+                                   parse: (String => Try[T])) extends RequestParameter[T, T] {
   override val required = true
 
   override def from(request: HttpRequest): T = parseFrom(request).flatMap(_.toOption).get
