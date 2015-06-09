053e8df944d8c61bd07b0e28c986a3cf6cd49281
David Denton <denton.david@gmail.com>
2015-06-09 11:24:03 +0100

    dave - refactor of client

--- src/main/scala/io/fintrospect/clients/Client.scala
+++ src/main/scala/io/fintrospect/clients/Client.scala
@@ -28,7 +28,8 @@ class Client(method: HttpMethod,
              requestParams: List[RequestParameter[_]],
              pathParams: List[PathParameter[_]],
              service: Service[HttpRequest, HttpResponse]) {
-  private val systemSuppliedParams = pathParams.filter(_.isEmpty).map(p => p -> p.name)
+
+  private val systemSuppliedParams = pathParams.filter(_.isEmpty).map(parameter => ParamBinding(parameter, parameter.name))
   private val allPossibleParams = pathParams ++ requestParams
   private val requiredParams = allPossibleParams.filter(_.required)
   private val queryParams = requestParams.filter(_.where == "query")
@@ -36,15 +37,13 @@ class Client(method: HttpMethod,
   private val identify = Identify(method, pathParams)
 
   def apply(userSuppliedBindings: ParamBinding[_]*): Future[HttpResponse] = {
-    val userSuppliedParams = userSuppliedBindings.map(b => (b.parameter, b.value))
-
-    val allSuppliedParams: Map[Parameter[_], String] = Map(userSuppliedParams ++ systemSuppliedParams: _*)
+    val allSuppliedParams = Map((userSuppliedBindings ++ systemSuppliedParams).map(b => (b.parameter, b.value)): _*)
     val illegalParams = allSuppliedParams.keys.filterNot(param => allPossibleParams.contains(param))
     if (illegalParams.nonEmpty) {
       return Future.value(Error(BAD_REQUEST, "Client: Illegal params passed: " + illegalParams))
     }
 
-    val missingParams = requiredParams.filterNot(allSuppliedParams.contains)
+    val missingParams = requiredParams.filterNot(allSuppliedParams.keySet.contains)
     if (missingParams.nonEmpty) {
       return Future.value(Error(BAD_REQUEST, "Client: Missing required params passed: " + missingParams))
     }
