7ee0203f7ce63b0aeaa26b68d5cd8ac359c05c36
David Denton <denton.david@gmail.com>
2015-06-13 14:37:53 +0100

    dave - simplify

--- src/main/scala/io/fintrospect/parameters/BodyParameter.scala
+++ src/main/scala/io/fintrospect/parameters/BodyParameter.scala
@@ -5,20 +5,17 @@ import org.jboss.netty.handler.codec.http.HttpRequest
 
 import scala.util.Try
 
-abstract class BodyParameter[T](val name: String,
-                                val description: Option[String],
-                                val paramType: ParamType,
-                                val example: Option[JsonRootNode],
-                                location: Location,
-                                deserialize: String => T,
-                                serialize: T => String)
-  extends Parameter[T] {
+abstract class BodyParameter[T](spec: ParameterSpec[T], location: Location, val example: Option[JsonRootNode]) extends Parameter[T]  {
+
+  override val name = spec.name
+  override val description = spec.description
+  override val paramType = spec.paramType
 
   def into(request: HttpRequest, value: String): Unit = location.into(name, value, request)
 
-  override def ->(value: T): ParamBinding[T] = ParamBinding(this, serialize(value))
+  override def ->(value: T): ParamBinding[T] = ParamBinding(this, spec.serialize(value))
 
   val where = location.toString
 
-  def attemptToParseFrom(request: HttpRequest): Option[Try[T]] = location.from(name, request).map(s => Try(deserialize(s)))
+  def attemptToParseFrom(request: HttpRequest): Option[Try[T]] = location.from(name, request).map(s => Try(spec.deserialize(s)))
 }
