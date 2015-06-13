c7ace1b3572915f13eb32ba460026e277c287420
David Denton <denton.david@gmail.com>
2015-06-13 14:41:53 +0100

    dave - simplify

--- src/main/scala/io/fintrospect/parameters/PathParameter.scala
+++ src/main/scala/io/fintrospect/parameters/PathParameter.scala
@@ -1,6 +1,9 @@
 package io.fintrospect.parameters
 
-abstract class PathParameter[T](val name: String, val description: Option[String], val paramType: ParamType) extends Parameter[T] with Iterable[PathParameter[_]] {
+abstract class PathParameter[T](spec: ParameterSpec[T]) extends Parameter[T] with Iterable[PathParameter[_]] {
+  override val name = spec.name
+  override val description = spec.description
+  override val paramType = spec.paramType
   override val where = "path"
   def unapply(str: String): Option[T]
 }
