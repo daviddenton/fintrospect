3c3204c4469404d9f13a76471777125b13a292c8
David Denton <denton.david@gmail.com>
2015-06-15 11:09:41 +0100

    dave - move instances of ParameterSpec.scala

--- src/main/scala/io/fintrospect/parameters/Parameters.scala
+++ src/main/scala/io/fintrospect/parameters/Parameters.scala
@@ -1,10 +1,8 @@
 package io.fintrospect.parameters
 
-import java.time.format.DateTimeFormatter._
 import java.time.{LocalDate, LocalDateTime, ZonedDateTime}
 
 import argo.jdom.JsonRootNode
-import io.fintrospect.util.ArgoUtil._
 
 import scala.language.higherKinds
 
@@ -27,8 +25,7 @@ trait Parameters[P[_], R[_]] {
    * @param description optional description of the parameter (for use in description endpoints)
    * @return a parameter for retrieving a LocalDate value from the request
    */
-  def localDate(name: String, description: String = null): P[LocalDate] with R[LocalDate] =
-    apply(ParameterSpec(name, Option(description), StringParamType, LocalDate.parse(_), ISO_LOCAL_DATE.format(_)))
+  def localDate(name: String, description: String = null): P[LocalDate] with R[LocalDate] = apply(ParameterSpec.localDate(name, description))
 
   /**
    * Create a ZonedDateTime parameter which is constrained by the format  YYYY-MM-DDTHH:mm:SSZ (See DateTimeFormatter.ISO_OFFSET_DATE_TIME)
@@ -36,8 +33,7 @@ trait Parameters[P[_], R[_]] {
    * @param description optional description of the parameter (for use in description endpoints)
    * @return a parameter for retrieving a ZonedDateTime value from the request
    */
-  def zonedDateTime(name: String, description: String = null): P[ZonedDateTime] with R[ZonedDateTime] =
-    apply(ParameterSpec(name, Option(description), StringParamType, ZonedDateTime.parse(_), ISO_ZONED_DATE_TIME.format(_)))
+  def zonedDateTime(name: String, description: String = null): P[ZonedDateTime] with R[ZonedDateTime] = apply(ParameterSpec.zonedDateTime(name, description))
 
   /**
    * Create a LocalDateTime parameter which is constrained by the format YYYY-MM-DDTHH:mm:SS
@@ -45,8 +41,7 @@ trait Parameters[P[_], R[_]] {
    * @param description optional description of the parameter (for use in description endpoints)
    * @return a parameter for retrieving a LocalDateTime value from the request
    */
-  def dateTime(name: String, description: String = null): P[LocalDateTime] with R[LocalDateTime] =
-    apply(ParameterSpec(name, Option(description), StringParamType, LocalDateTime.parse(_), ISO_LOCAL_DATE_TIME.format(_)))
+  def dateTime(name: String, description: String = null): P[LocalDateTime] with R[LocalDateTime] = apply(ParameterSpec.dateTime(name, description))
 
   /**
    * Create a Boolean parameter which is constrained to boolean values
@@ -54,8 +49,7 @@ trait Parameters[P[_], R[_]] {
    * @param description optional description of the parameter (for use in description endpoints)
    * @return a parameter for retrieving a Boolean value from the request
    */
-  def boolean(name: String, description: String = null): P[Boolean] with R[Boolean] =
-    apply(ParameterSpec(name, Option(description), BooleanParamType, _.toBoolean, _.toString))
+  def boolean(name: String, description: String = null): P[Boolean] with R[Boolean] = apply(ParameterSpec.boolean(name, description))
 
   /**
    * Create a String parameter which is not constrained
@@ -63,8 +57,7 @@ trait Parameters[P[_], R[_]] {
    * @param description optional description of the parameter (for use in description endpoints)
    * @return a parameter for retrieving a String value from the request
    */
-  def string(name: String, description: String = null): P[String] with R[String] =
-    apply(ParameterSpec(name, Option(description), StringParamType, identity, _.toString))
+  def string(name: String, description: String = null): P[String] with R[String] = apply(ParameterSpec.string(name, description))
 
   /**
    * Create a BigDecimal parameter which is constrained to numeric values
@@ -72,8 +65,7 @@ trait Parameters[P[_], R[_]] {
    * @param description optional description of the parameter (for use in description endpoints)
    * @return a parameter for retrieving a BigDecimal value from the request
    */
-  def bigDecimal(name: String, description: String = null): P[BigDecimal] with R[BigDecimal] =
-    apply(ParameterSpec(name, Option(description), NumberParamType, BigDecimal(_), _.toString))
+  def bigDecimal(name: String, description: String = null): P[BigDecimal] with R[BigDecimal] = apply(ParameterSpec.bigDecimal(name, description))
 
   /**
    * Create a Long parameter which is constrained to numeric Long values
@@ -81,8 +73,7 @@ trait Parameters[P[_], R[_]] {
    * @param description optional description of the parameter (for use in description endpoints)
    * @return a parameter for retrieving a Long value from the request
    */
-  def long(name: String, description: String = null): P[Long] with R[Long] =
-    apply(ParameterSpec(name, Option(description), IntegerParamType, _.toLong, _.toString))
+  def long(name: String, description: String = null): P[Long] with R[Long] = apply(ParameterSpec.long(name, description))
 
   /**
    * Create a Scala Int parameter which is constrained to numeric Int values
@@ -90,8 +81,7 @@ trait Parameters[P[_], R[_]] {
    * @param description optional description of the parameter (for use in description endpoints)
    * @return a parameter for retrieving a Int value from the request
    */
-  def int(name: String, description: String = null): P[Int] with R[Int] =
-    apply(ParameterSpec(name, Option(description), IntegerParamType, _.toInt, _.toString))
+  def int(name: String, description: String = null): P[Int] with R[Int] = apply(ParameterSpec.int(name, description))
 
   /**
    * Create a Java Integer parameter which is constrained to numeric Integer values
@@ -99,8 +89,7 @@ trait Parameters[P[_], R[_]] {
    * @param description optional description of the parameter (for use in description endpoints)
    * @return a parameter for retrieving a Integer value from the request
    */
-  def integer(name: String, description: String = null): P[Integer] with R[Integer] =
-    apply(ParameterSpec(name, Option(description), IntegerParamType, new Integer(_), _.toString))
+  def integer(name: String, description: String = null): P[Integer] with R[Integer] = apply(ParameterSpec.integer(name, description))
 
   /**
    * Create a Argo-format JsonNode parameter which is constrained to values which parse to valid JSON objects
@@ -108,8 +97,7 @@ trait Parameters[P[_], R[_]] {
    * @param description optional description of the parameter (for use in description endpoints)
    * @return a parameter for retrieving a JsonNode value from the request
    */
-  def json(name: String, description: String = null): P[JsonRootNode] with R[JsonRootNode] =
-    apply(ParameterSpec(name, Option(description), ObjectParamType, parse, compact(_)))
+  def json(name: String, description: String = null): P[JsonRootNode] with R[JsonRootNode] = apply(ParameterSpec.json(name, description))
 }
 
 
