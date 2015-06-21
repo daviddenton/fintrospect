15e7b8795ab9f88cee21e66b3942a77d1a1ead1a
David Denton <denton.david@gmail.com>
2015-06-21 21:57:31 +0100

    dave - removed Location as is now dead dead

--- src/main/scala/io/fintrospect/parameters/HeaderParameter.scala
+++ src/main/scala/io/fintrospect/parameters/HeaderParameter.scala
@@ -5,27 +5,18 @@ import org.jboss.netty.handler.codec.http.HttpRequest
 import scala.util.{Failure, Success, Try}
 
 abstract class HeaderParameter[T](spec: ParameterSpec[T]) extends Parameter[T] with Validatable[T, HttpRequest] {
-
-  private val location = new Location {
-    override def toString = "header"
-
-    override def from(name: String, request: HttpRequest) = Option(request.headers().get(name))
-
-    override def into(name: String, value: String, request: HttpRequest) = request.headers().add(name, value)
-  }
-
   override val name = spec.name
   override val description = spec.description
   override val paramType = spec.paramType
 
-  def into(request: HttpRequest, value: String): Unit = location.into(name, value, request)
+  def into(request: HttpRequest, value: String): Unit = request.headers().add(name, value)
 
   override def ->(value: T): ParamBinding[T] = ParamBinding(this, spec.serialize(value))
 
-  val where = location.toString
+  val where = "header"
 
   def validate(request: HttpRequest) = {
-    location.from(name, request).map {
+    Option(request.headers().get(name)).map {
       v => Try(spec.deserialize(v)) match {
         case Success(d) => Right(Option(d))
         case Failure(_) => Left(this)
