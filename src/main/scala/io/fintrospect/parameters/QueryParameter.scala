f96d91ba4d3699506ed8a17af2f37bcad406200a
David Denton <denton.david@gmail.com>
2015-06-21 21:57:31 +0100

    dave - removed Location as is now dead dead

--- src/main/scala/io/fintrospect/parameters/QueryParameter.scala
+++ src/main/scala/io/fintrospect/parameters/QueryParameter.scala
@@ -5,33 +5,27 @@ import org.jboss.netty.handler.codec.http.{HttpRequest, QueryStringDecoder}
 import scala.util.{Failure, Success, Try}
 
 abstract class QueryParameter[T](spec: ParameterSpec[T]) extends Parameter[T] with Validatable[T, HttpRequest] {
-
-  private val location = new Location {
-    override def toString = "query"
-
-    override def from(name: String, request: HttpRequest) = {
-      Try(new QueryStringDecoder(request.getUri).getParameters.get(name)).map(_.get(0)).toOption
-    }
-
-    override def into(name: String, value: String, request: HttpRequest) = ???
-  }
-
   override val name = spec.name
   override val description = spec.description
   override val paramType = spec.paramType
 
-  def into(request: HttpRequest, value: String): Unit = location.into(name, value, request)
+  def into(request: HttpRequest, value: String): Unit = ???
+
+  private def from(name: String, request: HttpRequest) = {
+    Try(new QueryStringDecoder(request.getUri).getParameters.get(name)).map(_.get(0)).toOption
+  }
 
   override def ->(value: T): ParamBinding[T] = ParamBinding(this, spec.serialize(value))
 
-  val where = location.toString
+  val where = "query"
 
   def validate(request: HttpRequest) = {
-    location.from(name, request).map {
+    from(name, request).map {
       v => Try(spec.deserialize(v)) match {
         case Success(d) => Right(Option(d))
         case Failure(_) => Left(this)
       }
     }.getOrElse(if (required) Left(this) else Right(None))
   }
+
 }
