8f3af5dadf0398242b613725cfa63042bd5be869
David Denton <denton.david@gmail.com>
2015-06-24 07:46:52 +0100

    dave - refactor to remove duplication

--- src/main/scala/io/fintrospect/parameters/FormBody.scala
+++ src/main/scala/io/fintrospect/parameters/FormBody.scala
@@ -1,15 +1,11 @@
 package io.fintrospect.parameters
 
-import io.fintrospect.ContentType
-import io.fintrospect.ContentTypes._
 import io.fintrospect.util.HttpRequestResponseUtil._
 import org.jboss.netty.handler.codec.http.HttpRequest
 
 import scala.util.{Failure, Success, Try}
 
 class FormBody(fields: Seq[FormField[_] with Retrieval[_, Form]]) extends Body[Form](BodySpec.form()) {
-  override val contentType: ContentType = APPLICATION_FORM_URLENCODED
-
   override def ->(value: Form) = ???
 
   override def from(request: HttpRequest) = BodySpec.form().deserialize(contentFrom(request))
