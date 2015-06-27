2ccc6808b6c541094bad4498213d28d661a62829
David Denton <denton.david@gmail.com>
2015-06-27 20:23:16 +0100

    dave - moved form body spec to form

--- src/main/scala/io/fintrospect/parameters/BodySpec.scala
+++ src/main/scala/io/fintrospect/parameters/BodySpec.scala
@@ -1,9 +1,6 @@
 package io.fintrospect.parameters
 
-import io.fintrospect.{ContentType, ContentTypes}
-import org.jboss.netty.handler.codec.http.{QueryStringDecoder, QueryStringEncoder}
-
-import scala.collection.JavaConverters._
+import io.fintrospect.ContentType
 
 /**
  * Spec required to marshall a body of a custom type
@@ -15,15 +12,3 @@ import scala.collection.JavaConverters._
  * @tparam T the type of the parameter
  */
 case class BodySpec[T](description: Option[String], contentType: ContentType, deserialize: String => T, serialize: T => String)
-
-object BodySpec {
-  def form(description: String = null) = new BodySpec[Form](Option(description),
-    ContentTypes.APPLICATION_FORM_URLENCODED,
-    content => new Form(new QueryStringDecoder("?" + content).getParameters.asScala.mapValues(_.asScala.toSet)),
-    form => {
-      val encoder = new QueryStringEncoder("")
-      form.foreach(entry => encoder.addParam(entry._1, entry._2.mkString(",")))
-      encoder.toUri.getQuery
-    }
-  )
-}
