d640bc3ee1d55cc8c6601ce1f5677400d96414a8
David Denton <denton.david@gmail.com>
2015-06-27 20:33:03 +0100

    dave - replaced effective switch with polymorphism. removed case classes

--- src/main/scala/io/fintrospect/parameters/Binding.scala
+++ src/main/scala/io/fintrospect/parameters/Binding.scala
@@ -6,22 +6,22 @@ import scala.language.existentials
 
 sealed trait Binding {
   val parameter: Parameter[_]
-  def applyTo(requestBuild: RequestBuild): RequestBuild
+  def apply(requestBuild: RequestBuild): RequestBuild
 }
 
 class QueryBinding(val parameter: Parameter[_], key: String, value: String) extends Binding {
-  def applyTo(requestBuild: RequestBuild) = requestBuild.copy(queries = requestBuild.queries + (key -> value))
+  def apply(requestBuild: RequestBuild) = requestBuild.copy(queries = requestBuild.queries + (key -> value))
 }
 
 class PathBinding(val parameter: Parameter[_], value: String) extends Binding {
-  def applyTo(requestBuild: RequestBuild) = requestBuild.copy(uriParts = requestBuild.uriParts :+ value)
+  def apply(requestBuild: RequestBuild) = requestBuild.copy(uriParts = requestBuild.uriParts :+ value)
 }
 
 class RequestBinding(val parameter: Parameter[_], into: HttpRequest => HttpRequest) extends Binding {
-  def applyTo(requestBuild: RequestBuild) = requestBuild.copy(fn = requestBuild.fn.andThen(into))
+  def apply(requestBuild: RequestBuild) = requestBuild.copy(fn = requestBuild.fn.andThen(into))
 }
 
 class FormFieldBinding(val parameter: Parameter[_], val key: String, val value: String) extends Binding {
-  def applyTo(requestBuild: RequestBuild) = requestBuild
+  def apply(requestBuild: RequestBuild) = requestBuild
 }
 
