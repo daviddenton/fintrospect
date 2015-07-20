a85e1fe866eda39c28aa7e5a45664c7d9bfd4ce4
David Denton <denton.david@gmail.com>
2015-07-20 14:10:41 +0100

    dave - refactor

--- src/main/scala/io/fintrospect/parameters/QueryParameter.scala
+++ src/main/scala/io/fintrospect/parameters/QueryParameter.scala
@@ -5,40 +5,31 @@ import org.jboss.netty.handler.codec.http.{HttpRequest, QueryStringDecoder}
 import scala.collection.JavaConverters._
 import scala.util.{Failure, Success, Try}
 
-abstract class QueryParameter[T](val name: String, val description: Option[String], val paramType: ParamType)
-  extends Parameter
-  with Validatable[T, HttpRequest]
-  with Bindable[T, QueryBinding] {
+abstract class QueryParameter[T](spec: ParameterSpec[_], val deserialize: Seq[String] => T)
+  extends Parameter with Validatable[T, HttpRequest] with Bindable[T, QueryBinding] {
 
+  val name = spec.name
+  val description = spec.description
+  val paramType = spec.paramType
   val where = "query"
-}
-
-abstract class SingleQueryParameter[T](spec: ParameterSpec[T]) extends QueryParameter[T](spec.name, spec.description, spec.paramType) {
-
-  override def -->(value: T) = Seq(new QueryBinding(this, spec.serialize(value)))
-
-  def validate(request: HttpRequest) =
-    Option(new QueryStringDecoder(request.getUri).getParameters.get(name))
-      .map(_.asScala.toSeq)
-      .map(v =>
-      Try(spec.deserialize(v.head)) match {
-        case Success(d) => Right(Option(d))
-        case Failure(_) => Left(this)
-      }).getOrElse(if (required) Left(this) else Right(None))
-}
-
-abstract class MultiQueryParameter[T](spec: ParameterSpec[T])
-  extends QueryParameter[Seq[T]](spec.name, spec.description, spec.paramType) {
-
-  override def -->(value: Seq[T]) = value.map(v => new QueryBinding(this, spec.serialize(v)))
 
   def validate(request: HttpRequest) = {
     Option(new QueryStringDecoder(request.getUri).getParameters.get(name))
       .map(_.asScala.toSeq)
       .map(v =>
-      Try(v.map(s => spec.deserialize(s))) match {
+      Try(deserialize(v)) match {
         case Success(d) => Right(Option(d))
         case Failure(_) => Left(this)
       }).getOrElse(if (required) Left(this) else Right(None))
   }
+}
+
+abstract class SingleQueryParameter[T](spec: ParameterSpec[T])
+  extends QueryParameter[T](spec, xs => spec.deserialize(xs.head)) {
+  override def -->(value: T) = Seq(new QueryBinding(this, spec.serialize(value)))
+}
+
+abstract class MultiQueryParameter[T](spec: ParameterSpec[T])
+  extends QueryParameter[Seq[T]](spec, xs => xs.map(spec.deserialize)) {
+  override def -->(value: Seq[T]) = value.map(v => new QueryBinding(this, spec.serialize(v)))
 }
