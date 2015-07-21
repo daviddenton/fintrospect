1c6ad8ffbb0227c2353b532adb41e5cdc76a498c
David Denton <denton.david@gmail.com>
2015-07-21 15:17:25 +0100

    dave - added convienience methods for multi field retreival from a form

--- src/test/scala/io/fintrospect/parameters/FormTest.scala
+++ src/test/scala/io/fintrospect/parameters/FormTest.scala
@@ -4,10 +4,30 @@ import org.scalatest._
 
 class FormTest extends FunSpec with ShouldMatchers {
 
+  private val field1 = FormField.required.string("field1")
+  private val field2 = FormField.required.string("field2")
+  private val field3 = FormField.required.string("field3")
+  private val field4 = FormField.required.string("field4")
+  private val field5 = FormField.required.string("field5")
+  private val field6 = FormField.required.string("field6")
+
+  private val formSpec = Body.form(field1, field2, field3, field4, field5, field6)
+
   describe("construction") {
     it("from a set of bindings") {
-      val string = FormField.required.string("field")
-      Form(Seq(new FormFieldBinding(string, "value"))).iterator.toSeq shouldEqual Seq(("field", Set("value")))
+      Form(Seq(new FormFieldBinding(field1, "value"))).iterator.toSeq shouldEqual Seq(("field1", Set("value")))
+    }
+  }
+
+  describe("multiple parameter retrieval") {
+    val formInstance = Form(field1 --> "value1", field2 --> "value2", field3 --> "value3", field4 --> "value4", field5 --> "value5", field6 --> "value6")
+    it("1 binding") {
+      formInstance <-- field1 shouldEqual "value1"
+      formInstance <-- (field1, field2) shouldEqual ("value1", "value2")
+      formInstance <-- (field1, field2, field3) shouldEqual ("value1", "value2", "value3")
+      formInstance <-- (field1, field2, field3, field4) shouldEqual ("value1", "value2", "value3", "value4")
+      formInstance <-- (field1, field2, field3, field4, field5) shouldEqual ("value1", "value2", "value3", "value4", "value5")
+      formInstance <-- (field1, field2, field3, field4, field5, field6) shouldEqual ("value1", "value2", "value3", "value4", "value5", "value6")
     }
   }
 }
