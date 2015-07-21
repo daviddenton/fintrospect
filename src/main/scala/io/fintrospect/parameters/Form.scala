2e53a2ce2ca5f25798bf341fd60b0e22ce0225d5
David Denton <denton.david@gmail.com>
2015-07-21 15:17:25 +0100

    dave - added convienience methods for multi field retreival from a form

--- src/main/scala/io/fintrospect/parameters/Form.scala
+++ src/main/scala/io/fintrospect/parameters/Form.scala
@@ -1,6 +1,58 @@
 package io.fintrospect.parameters
 
 case class Form(private val fields: collection.Map[String, Set[String]]) extends Iterable[(String, Set[String])] {
+
+  /**
+   * Convenience method to retrieve multiple fields from form
+   */
+  def <--[A](fieldA: Retrieval[A, Form]):
+  A = fieldA <-- this
+
+  /**
+   * Convenience method to retrieve multiple fields from form
+   */
+  def <--[A, B](fieldA: Retrieval[A, Form],
+                fieldB: Retrieval[B, Form]):
+  (A, B) = (fieldA <-- this, fieldB <-- this)
+
+  /**
+   * Convenience method to retrieve multiple fields from form
+   */
+  def <--[A, B, C](fieldA: Retrieval[A, Form],
+                   fieldB: Retrieval[B, Form],
+                   fieldC: Retrieval[C, Form]):
+  (A, B, C) = (fieldA <-- this, fieldB <-- this, fieldC <-- this)
+
+  /**
+   * Convenience method to retrieve multiple fields from form
+   */
+  def <--[A, B, C, D](fieldA: Retrieval[A, Form],
+                      fieldB: Retrieval[B, Form],
+                      fieldC: Retrieval[C, Form],
+                      fieldD: Retrieval[D, Form]):
+  (A, B, C, D) = (fieldA <-- this, fieldB <-- this, fieldC <-- this, fieldD <-- this)
+
+  /**
+   * Convenience method to retrieve multiple fields from form
+   */
+  def <--[A, B, C, D, E](fieldA: Retrieval[A, Form],
+                         fieldB: Retrieval[B, Form],
+                         fieldC: Retrieval[C, Form],
+                         fieldD: Retrieval[D, Form],
+                         fieldE: Retrieval[E, Form]):
+  (A, B, C, D, E) = (fieldA <-- this, fieldB <-- this, fieldC <-- this, fieldD <-- this, fieldE <-- this)
+
+  /**
+   * Convenience method to retrieve multiple fields from form
+   */
+  def <--[A, B, C, D, E, F](fieldA: Retrieval[A, Form],
+                            fieldB: Retrieval[B, Form],
+                            fieldC: Retrieval[C, Form],
+                            fieldD: Retrieval[D, Form],
+                            fieldE: Retrieval[E, Form],
+                            fieldF: Retrieval[F, Form]):
+  (A, B, C, D, E, F) = (fieldA <-- this, fieldB <-- this, fieldC <-- this, fieldD <-- this, fieldE <-- this, fieldF <-- this)
+
   def +(key: String, value: String) = Form(fields + (key -> (fields.getOrElse(key, Set()) + value)))
 
   def get(name: String): Option[Seq[String]] = fields.get(name).map(_.toSeq)
