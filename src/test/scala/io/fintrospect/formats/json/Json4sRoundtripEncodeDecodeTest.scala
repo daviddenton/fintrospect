ff85fd4a317a297d378f504b7092c6c36fd268ad
David Denton <denton.david@gmail.com>
2015-09-28 16:55:33 +0100

    dave  - added decode/encode tests for Json4s

---
+++ src/test/scala/io/fintrospect/formats/json/Json4sRoundtripEncodeDecodeTest.scala
@@ -0,0 +1,27 @@
+package io.fintrospect.formats.json
+
+import io.fintrospect.util.json.Json4s
+import io.fintrospect.util.json.Json4s.Json4sFormat
+import org.scalatest.{FunSpec, ShouldMatchers}
+
+case class StreetAddress(address: String)
+
+case class Letter(to: StreetAddress, from: StreetAddress, message: String)
+
+class RoundtripEncodeDecodeSpec[T](format: Json4sFormat[T]) extends FunSpec with ShouldMatchers {
+
+  describe(format.getClass.getSimpleName) {
+    val aLetter = Letter(StreetAddress("my house"), StreetAddress("your house"), "hi there")
+    it("roundtrips to json and back") {
+      format.decode[Letter](format.encode(aLetter)) shouldEqual aLetter
+    }
+  }
+}
+
+class JsonNativeEncodeDecodeTest extends RoundtripEncodeDecodeSpec(Json4s.Native.JsonFormat)
+
+class Json4NativeDoubleEncodeDecodeTest extends RoundtripEncodeDecodeSpec(Json4s.NativeDoubleMode.JsonFormat)
+
+class Json4JacksonEncodeDecodeTest extends RoundtripEncodeDecodeSpec(Json4s.Jackson.JsonFormat)
+
+class Json4JacksonDoubleEncodeDecodeTest extends RoundtripEncodeDecodeSpec(Json4s.JacksonDoubleMode.JsonFormat)
