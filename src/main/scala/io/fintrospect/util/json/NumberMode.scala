d9d6b5b2eeb4f96cdafa0598f5ed7ad5b542d7f0
David Denton <denton.david@gmail.com>
2015-09-28 16:57:30 +0100

    dave  - removed numbermode as unused

--- src/main/scala/io/fintrospect/util/json/NumberMode.scala
+++
@@ -1,9 +0,0 @@
-package io.fintrospect.util.json
-
-/**
- * Defines how to parse JSON decimals. The default is UseBigDecimal for obvious reasons.
- */
-object NumberMode extends Enumeration {
-  type NumberMode = Value
-  val UseBigDecimal, DoubleMode = Value
-}
