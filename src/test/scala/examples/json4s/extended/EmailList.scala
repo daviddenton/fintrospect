91583b7f607c014b6a6530aafbe345396063271d
David Denton <denton.david@gmail.com>
2015-09-28 16:36:20 +0100

    dave  - tidied up Json4s example

--- src/test/scala/examples/json4s/extended/EmailList.scala
+++ src/test/scala/examples/json4s/extended/EmailList.scala
@@ -1,27 +1,25 @@
 package examples.json4s.extended
 
 import com.twitter.finagle.Service
-import com.twitter.util.Future
 import examples.json4s.extended.InboxApp.JsonLibrary.JsonFormat
-import examples.json4s.extended.InboxApp.JsonLibrary.ResponseBuilder.Ok
+import examples.json4s.extended.InboxApp.JsonLibrary.JsonFormat._
+import examples.json4s.extended.InboxApp.JsonLibrary.ResponseBuilder._
 import io.fintrospect._
+import io.fintrospect.formats.ResponseBuilder._
 import io.fintrospect.parameters.Path
 import org.jboss.netty.handler.codec.http.HttpMethod._
 import org.jboss.netty.handler.codec.http.HttpResponseStatus._
 import org.jboss.netty.handler.codec.http.{HttpRequest, HttpResponse}
-import org.json4s.JsonAST.{JArray, JValue}
 
 class EmailList(emails: Emails) {
   private val exampleEmail = Email(EmailAddress("you@github.com"), EmailAddress("wife@github.com"), "when are you going to be home for dinner", false)
 
   private def forUser(emailAddress: EmailAddress) = new Service[HttpRequest, HttpResponse] {
-    override def apply(request: HttpRequest): Future[HttpResponse] = Future.value(Ok(asJson(emails.forUser(emailAddress): _*)) )
+    override def apply(request: HttpRequest) = Ok(encode(emails.forUser(emailAddress)))
   }
 
-  private def asJson(emails: Email*): JValue = JArray(emails.map(Email.encodeJson).toList)
-
   val route = RouteSpec("list the inbox contents")
-    .returning(ResponseSpec.json(OK -> "list of emails for a user", asJson(exampleEmail), JsonFormat))
+    .returning(ResponseSpec.json(OK -> "list of emails for a user", encode(exampleEmail), JsonFormat))
     .at(GET) / "user" / Path(Email.Spec) bindTo forUser
 }
 
--- src/test/scala/examples/json4s/extended/UserList.scala
+++ src/test/scala/examples/json4s/extended/UserList.scala
@@ -3,22 +3,21 @@ package examples.json4s.extended
 import com.twitter.finagle.Service
 import com.twitter.util.Future
 import examples.json4s.extended.InboxApp.JsonLibrary.JsonFormat
+import examples.json4s.extended.InboxApp.JsonLibrary.JsonFormat._
 import examples.json4s.extended.InboxApp.JsonLibrary.ResponseBuilder.Ok
 import io.fintrospect._
+import io.fintrospect.formats.ResponseBuilder._
 import org.jboss.netty.handler.codec.http.HttpMethod._
 import org.jboss.netty.handler.codec.http.HttpResponseStatus._
 import org.jboss.netty.handler.codec.http.{HttpRequest, HttpResponse}
-import org.json4s.JsonAST.{JArray, JString, JValue}
 
 class UserList(emails: Emails) {
   private def list() = new Service[HttpRequest, HttpResponse] {
-    override def apply(request: HttpRequest): Future[HttpResponse] = Future.value(Ok(asJson(emails.users().toSeq: _*)))
+    override def apply(request: HttpRequest): Future[HttpResponse] = Ok(encode(emails.users()))
   }
 
-  private def asJson(emails: EmailAddress*): JValue = JArray(emails.map(a => JString(a.address)).toList)
-
   val route = RouteSpec("list the known users on this server")
-    .returning(ResponseSpec.json(OK -> "all users who have sent or received a mail", asJson(EmailAddress("you@github.com")), JsonFormat))
+    .returning(ResponseSpec.json(OK -> "all users who have sent or received a mail", encode(EmailAddress("you@github.com")), JsonFormat))
     .at(GET) / "user" bindTo list
 }
 
