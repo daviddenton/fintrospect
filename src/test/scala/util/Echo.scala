debd13bd704f1bc8500d68441021c4482922cbdf
David Denton <denton.david@gmail.com>
2015-09-28 10:08:46 +0100

    dave  - tweak

--- src/test/scala/util/Echo.scala
+++ src/test/scala/util/Echo.scala
@@ -2,10 +2,8 @@ package util
 
 import com.twitter.finagle.Service
 import com.twitter.util.Future
-import io.fintrospect.formats.ResponseBuilder
 import io.fintrospect.formats.ResponseBuilder._
 import io.fintrospect.util.HttpRequestResponseUtil.headersFrom
-import io.fintrospect.util.json.Argo.JsonFormat
 import io.fintrospect.util.json.Argo.JsonFormat._
 import io.fintrospect.util.json.Argo.ResponseBuilder._
 import org.jboss.netty.handler.codec.http.{HttpRequest, HttpResponse}
@@ -13,8 +11,8 @@ import org.jboss.netty.handler.codec.http.{HttpRequest, HttpResponse}
 case class Echo(parts: String*) extends Service[HttpRequest, HttpResponse] {
   def apply(request: HttpRequest): Future[HttpResponse] = {
     Ok(obj(
-      "headers" -> JsonFormat.string(headersFrom(request).toString()),
-      "params" -> JsonFormat.string(request.toString),
-      "message" -> JsonFormat.string(parts.mkString(" "))))
+      "headers" -> string(headersFrom(request).toString()),
+      "params" -> string(request.toString),
+      "message" -> string(parts.mkString(" "))))
   }
 }
