40053d0122735a40c7ff0b9fd554c2a767025d19
David Denton <denton.david@gmail.com>
2015-10-11 18:23:19 +0100

    dave - adding Argonaut support, rearranging tests

--- src/test/scala/io/fintrospect/formats/json/Json4sTest.scala
+++ src/test/scala/io/fintrospect/formats/json/Json4sTest.scala
@@ -1,5 +1,20 @@
 package io.fintrospect.formats.json
 
+import io.fintrospect.formats.json.Json4s.Json4sFormat
+
+case class StreetAddress(address: String)
+
+case class Letter(to: StreetAddress, from: StreetAddress, message: String)
+
+abstract class RoundtripEncodeDecodeSpec[T](format: Json4sFormat[T]) extends JsonFormatSpec(format) {
+  describe(format.getClass.getSimpleName) {
+    val aLetter = Letter(StreetAddress("my house"), StreetAddress("your house"), "hi there")
+    it("roundtrips to json and back") {
+      format.decode[Letter](format.encode(aLetter)) shouldEqual aLetter
+    }
+  }
+}
+
 class Json4sNativeEncodeDecodeTest extends RoundtripEncodeDecodeSpec(Json4s.Native.JsonFormat)
 class Json4sNativeDoubleEncodeDecodeTest extends RoundtripEncodeDecodeSpec(Json4s.NativeDoubleMode.JsonFormat)
 
