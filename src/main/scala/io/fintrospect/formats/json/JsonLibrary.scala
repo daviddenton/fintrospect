50ec2780c7d76f4286876389b2f51c119f80e304
David Denton <denton.david@gmail.com>
2015-10-11 18:34:37 +0100

    dave - inlined JsonResponseBuilder as this should only be used as a part of a JsonLibrary

--- src/main/scala/io/fintrospect/formats/json/JsonLibrary.scala
+++ src/main/scala/io/fintrospect/formats/json/JsonLibrary.scala
@@ -1,5 +1,8 @@
 package io.fintrospect.formats.json
 
+import io.fintrospect.ContentTypes
+import io.fintrospect.formats.{ResponseBuilder, ResponseBuilderMethods}
+
 /**
  * Defines a supported JSON library format (e.g. Argo or Json4s)
  * @tparam R - Root node type
@@ -15,5 +18,13 @@ trait JsonLibrary[R, N] {
   /**
    * Use this to create JSON-format Responses
    */
-  object ResponseBuilder extends JsonResponseBuilder[R, N](JsonFormat)
+  object ResponseBuilder extends ResponseBuilderMethods[R] {
+    private def formatJson(node: R): String = JsonFormat.pretty(node)
+
+    private def formatErrorMessage(errorMessage: String): R = JsonFormat.obj("message" -> JsonFormat.string(errorMessage))
+
+    private def formatError(throwable: Throwable): R = formatErrorMessage(Option(throwable.getMessage).getOrElse(throwable.getClass.getName))
+
+    override def HttpResponse() = new ResponseBuilder[R](formatJson, formatErrorMessage, formatError, ContentTypes.APPLICATION_JSON)
+  }
 }
--- src/main/scala/io/fintrospect/formats/xml/XmlResponseBuilder.scala
+++ src/main/scala/io/fintrospect/formats/xml/XmlResponseBuilder.scala
@@ -7,11 +7,11 @@ import scala.xml.Elem
 
 object XmlResponseBuilder extends ResponseBuilderMethods[Elem] {
 
-  private def formatJson(node: Elem): String = node.toString()
+  private def format(node: Elem): String = node.toString()
 
   private def formatErrorMessage(errorMessage: String): Elem = <message>{errorMessage}</message>
 
   private def formatError(throwable: Throwable): Elem = formatErrorMessage(Option(throwable.getMessage).getOrElse(throwable.getClass.getName))
 
-  override def HttpResponse() = new ResponseBuilder[Elem](formatJson, formatErrorMessage, formatError, ContentTypes.APPLICATION_XML)
+  override def HttpResponse() = new ResponseBuilder[Elem](format, formatErrorMessage, formatError, ContentTypes.APPLICATION_XML)
 }
