87d276d574a450cef20fe9dcc3b42267c7535ad4
David Denton <denton.david@gmail.com>
2015-10-11 19:41:57 +0100

    dave - removed duplication

--- src/test/scala/io/fintrospect/formats/json/ArgoTest.scala
+++ src/test/scala/io/fintrospect/formats/json/ArgoTest.scala
@@ -1,35 +1,6 @@
 package io.fintrospect.formats.json
 
-import java.math.BigInteger
-
-import org.scalatest.{FunSpec, ShouldMatchers}
-
-import scala.util.Try
-
 
 class ArgoJsonResponseBuilderTest extends JsonResponseBuilderSpec(Argo)
 
-class ArgoJsonFormatTest extends FunSpec with ShouldMatchers {
-  private val format = Argo.JsonFormat
-
-  describe(format.getClass.getSimpleName) {
-
-    it("creates JSON objects as expected") {
-      format.compact(format.obj(
-        "string" -> format.string("hello"),
-        "object" -> format.obj(Seq("field1" -> format.string("aString"))),
-        "int" -> format.number(1),
-        "long" -> format.number(2L),
-        "decimal" -> format.number(BigDecimal(1.2)),
-        "bigInt" -> format.number(new BigInteger("12344")),
-        "bool" -> format.boolean(true),
-        "null" -> format.nullNode(),
-        "array" -> format.array(format.string("world"), format.boolean(true))
-      )) shouldEqual """{"string":"hello","object":{"field1":"aString"},"int":1,"long":2,"decimal":1.2,"bigInt":12344,"bool":true,"null":null,"array":["world",true]}"""
-    }
-
-    it("parse blows up when invalid") {
-      Try(format.parse("not json")).isFailure shouldBe true
-    }
-  }
-}
+class ArgoJsonFormatTest extends JsonFormatSpec(Argo.JsonFormat)
