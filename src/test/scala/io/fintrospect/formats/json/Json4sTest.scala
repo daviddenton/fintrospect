c74d6433fddf3cfe11320bcf6a89dc0444e54328
David Denton <denton.david@gmail.com>
2015-10-11 18:26:02 +0100

    dave - adding Argonaut support, rearranging tests

--- src/test/scala/io/fintrospect/formats/json/Json4sTest.scala
+++ src/test/scala/io/fintrospect/formats/json/Json4sTest.scala
@@ -7,6 +7,7 @@ case class StreetAddress(address: String)
 case class Letter(to: StreetAddress, from: StreetAddress, message: String)
 
 abstract class RoundtripEncodeDecodeSpec[T](format: Json4sFormat[T]) extends JsonFormatSpec(format) {
+
   describe(format.getClass.getSimpleName) {
     val aLetter = Letter(StreetAddress("my house"), StreetAddress("your house"), "hi there")
     it("roundtrips to json and back") {
@@ -16,13 +17,14 @@ abstract class RoundtripEncodeDecodeSpec[T](format: Json4sFormat[T]) extends Jso
 }
 
 class Json4sNativeEncodeDecodeTest extends RoundtripEncodeDecodeSpec(Json4s.Native.JsonFormat)
+class Json4sNativeJsonResponseBuilderTest extends JsonResponseBuilderSpec(Json4s.Native)
+
 class Json4sNativeDoubleEncodeDecodeTest extends RoundtripEncodeDecodeSpec(Json4s.NativeDoubleMode.JsonFormat)
+class Json4sNativeDoubleJsonResponseBuilderTest extends JsonResponseBuilderSpec(Json4s.NativeDoubleMode)
 
 class Json4sJacksonEncodeDecodeTest extends RoundtripEncodeDecodeSpec(Json4s.Jackson.JsonFormat)
+class Json4sJacksonJsonResponseBuilderTest extends JsonResponseBuilderSpec(Json4s.Jackson)
+
 class Json4sJacksonDoubleEncodeDecodeTest extends RoundtripEncodeDecodeSpec(Json4s.JacksonDoubleMode.JsonFormat)
+class Json4sJacksonDoubleJsonResponseBuilderTest extends JsonResponseBuilderSpec(Json4s.JacksonDoubleMode)
 
-class Json4sNativeBuilderTest extends JsonResponseBuilderSpec(Json4s.Native)
-class Json4sNativeDoubleBuilderTest extends JsonResponseBuilderSpec(Json4s.NativeDoubleMode)
-
-class Json4sJacksonBuilderTest extends JsonResponseBuilderSpec(Json4s.Jackson)
-class Json4sJacksonDoubleBuilderTest extends JsonResponseBuilderSpec(Json4s.JacksonDoubleMode)
