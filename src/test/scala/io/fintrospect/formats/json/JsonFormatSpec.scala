1c2136f9154a2a65ac7bdb20beb242eb832bc78d
David Denton <denton.david@gmail.com>
2015-10-11 19:41:57 +0100

    dave - removed duplication

--- src/test/scala/io/fintrospect/formats/json/JsonFormatSpec.scala
+++ src/test/scala/io/fintrospect/formats/json/JsonFormatSpec.scala
@@ -6,7 +6,7 @@ import org.scalatest.{FunSpec, ShouldMatchers}
 
 import scala.util.Try
 
-abstract class JsonFormatSpec[X](val format: JsonFormat[X, X]) extends FunSpec with ShouldMatchers {
+abstract class JsonFormatSpec[X, Y](val format: JsonFormat[X, Y]) extends FunSpec with ShouldMatchers {
 
   val expectedJson = """{"string":"hello","object":{"field1":"aString"},"int":1,"long":2,"decimal":1.2,"bigInt":12344,"bool":true,"null":null,"array":["world",true]}"""
 
@@ -14,7 +14,7 @@ abstract class JsonFormatSpec[X](val format: JsonFormat[X, X]) extends FunSpec w
     it("creates JSON objects as expected") {
       format.compact(format.obj(
         "string" -> format.string("hello"),
-        "object" -> format.obj(Seq("field1" -> format.string("aString"))),
+        "object" -> format.obj(Seq("field1" -> format.string("aString"))).asInstanceOf[Y],
         "int" -> format.number(1),
         "long" -> format.number(2L),
         "decimal" -> format.number(BigDecimal(1.2)),
