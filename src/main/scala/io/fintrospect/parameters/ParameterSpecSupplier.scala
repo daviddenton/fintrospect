7863f268537531dcb41868947c1ae18325efe069
David Denton <denton.david@gmail.com>
2015-11-06 16:39:41 +0000

    dave - docs

--- src/main/scala/io/fintrospect/parameters/ParameterSpecSupplier.scala
+++ src/main/scala/io/fintrospect/parameters/ParameterSpecSupplier.scala
@@ -1,5 +1,9 @@
 package io.fintrospect.parameters
 
+
+/**
+ * By implementing ParameterSpecSupplier[T], we can declare parameters using a shorthand: eg. Path(HipsterBeardStyle)
+ */
 trait ParameterSpecSupplier[T] {
   def spec: ParameterSpec[T]
 }
