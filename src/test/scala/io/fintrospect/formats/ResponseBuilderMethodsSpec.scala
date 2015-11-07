81d6dd29c3069bf4bc844788fdf5c7b85a4771cc
David Denton <denton.david@gmail.com>
2015-11-07 11:12:02 +0000

    dave - added ability to set response content with Bufs and ChannelBuffers

--- src/test/scala/io/fintrospect/formats/ResponseBuilderMethodsSpec.scala
+++ src/test/scala/io/fintrospect/formats/ResponseBuilderMethodsSpec.scala
@@ -1,7 +1,11 @@
 package io.fintrospect.formats
 
+import java.nio.charset.Charset._
+
 import com.twitter.finagle.http.Status
+import com.twitter.io.Bufs
 import io.fintrospect.util.HttpRequestResponseUtil._
+import org.jboss.netty.buffer.ChannelBuffers._
 import org.scalatest.{FunSpec, ShouldMatchers}
 
 abstract class ResponseBuilderMethodsSpec[T](bldr: ResponseBuilderMethods[T]) extends FunSpec with ShouldMatchers {
@@ -16,22 +20,42 @@ abstract class ResponseBuilderMethodsSpec[T](bldr: ResponseBuilderMethods[T]) ex
   describe("Rendering") {
     it("ok") {
       statusAndContentFrom(bldr.OK) shouldEqual(Status.Ok, "")
-      statusAndContentFrom(bldr.OK) shouldEqual(Status.Ok, "")
     }
 
-    it("ok with message") {
-      statusAndContentFrom(bldr.OK(message)) shouldEqual(Status.Ok, expectedContent)
+    it("ok with message - String") {
       statusAndContentFrom(bldr.OK(message)) shouldEqual(Status.Ok, expectedContent)
     }
 
-    it("error with message") {
+    it("ok with message - Buf") {
+      statusAndContentFrom(bldr.OK(Bufs.utf8Buf(message))) shouldEqual(Status.Ok, expectedContent)
+    }
+
+    it("ok with message - ChannelBuffer") {
+      statusAndContentFrom(bldr.OK(copiedBuffer(message, defaultCharset()))) shouldEqual(Status.Ok, expectedContent)
+    }
+
+    it("error with message - String") {
       statusAndContentFrom(bldr.Error(Status.NotFound, message)) shouldEqual(Status.NotFound, expectedErrorContent)
+    }
+
+    it("error with message - Buf") {
       statusAndContentFrom(bldr.Error(Status.NotFound, customError)) shouldEqual(Status.NotFound, expectedErrorContent)
+    }
+
+    it("error with message - ChannelBuffer") {
       statusAndContentFrom(bldr.Error(Status.NotFound, new RuntimeException(message))) shouldEqual(Status.NotFound, expectedErrorContent)
     }
 
-    it("content") {
+    it("content - String") {
       statusAndContentFrom(bldr.HttpResponse().withContent(message).build) shouldEqual(Status.Ok, expectedContent)
+    }
+
+    it("content - Buf") {
+      statusAndContentFrom(bldr.HttpResponse().withContent(Bufs.utf8Buf(message)).build) shouldEqual(Status.Ok, expectedContent)
+    }
+
+    it("content - ChannelBuffer") {
+      statusAndContentFrom(bldr.HttpResponse().withContent(copiedBuffer(message, defaultCharset())).build) shouldEqual(Status.Ok, expectedContent)
     }
 
     it("errors - message") {
