b6551c9c3255d269b10edecf7ee965b7f6abc749
David Denton <denton.david@gmail.com>
2015-11-19 22:00:52 +0000

    dave - added EntryLogger to runnable env

--- src/test/scala/examples/full/test/env/RunnableEnvironment.scala
+++ src/test/scala/examples/full/test/env/RunnableEnvironment.scala
@@ -5,7 +5,10 @@ import io.fintrospect.testing.TestHttpServer
 object RunnableEnvironment extends App {
 
   private val userDirectoryPort = 10000
+  private val entryLoggerPort = 10001
 
   new TestHttpServer(userDirectoryPort, new FakeUserDirectoryState()).start()
+  new TestHttpServer(entryLoggerPort, new FakeEntryLoggerState()).start()
 
+  Thread.currentThread().join()
 }
