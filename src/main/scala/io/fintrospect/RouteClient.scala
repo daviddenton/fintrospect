7e552cc2fd5ec75742722c74202b975c7ea70e6a
David Denton <denton.david@gmail.com>
2015-11-21 18:00:51 +0000

    dave - removed return statements for niceness

--- src/main/scala/io/fintrospect/RouteClient.scala
+++ src/main/scala/io/fintrospect/RouteClient.scala
@@ -6,7 +6,6 @@ import com.twitter.finagle.{Service, SimpleFilter}
 import com.twitter.util.Future
 import io.fintrospect.Headers._
 import io.fintrospect.RouteClient.Identify
-import io.fintrospect.formats.ResponseBuilder._
 import io.fintrospect.formats.text.PlainTextResponseBuilder._
 import io.fintrospect.parameters._
 
@@ -25,16 +24,16 @@ object RouteClient {
 }
 
 /**
- * Representation of a pre-configured client HTTP call
- * @param routeSpec the route specification
- * @param method the HTTP method
- * @param underlyingService the underlying service to make the request from
- * @param pathParams the path parameters to use
- */
+  * Representation of a pre-configured client HTTP call
+  * @param routeSpec the route specification
+  * @param method the HTTP method
+  * @param underlyingService the underlying service to make the request from
+  * @param pathParams the path parameters to use
+  */
 class RouteClient(method: Method,
-             routeSpec: RouteSpec,
-             pathParams: Seq[PathParameter[_]],
-             underlyingService: Service[Request, Response]) {
+                  routeSpec: RouteSpec,
+                  pathParams: Seq[PathParameter[_]],
+                  underlyingService: Service[Request, Response]) {
 
   private val providedBindings = pathParams.filter(_.isFixed).map(p => new PathBinding(p, p.name))
   private val allPossibleParams = pathParams ++ routeSpec.headerParams ++ routeSpec.queryParams ++ routeSpec.body.toSeq.flatMap(_.iterator)
@@ -42,29 +41,28 @@ class RouteClient(method: Method,
   private val service = Identify(method, pathParams).andThen(underlyingService)
 
   /**
-   * Make a request to this client route using the passed bindings
-   * @param userBindings the bindings for this request
-   * @return the response Future
-   */
+    * Make a request to this client route using the passed bindings
+    * @param userBindings the bindings for this request
+    * @return the response Future
+    */
   def apply(userBindings: Iterable[Binding]*): Future[Response] = {
     val suppliedBindings = userBindings.flatten ++ providedBindings
 
     val userSuppliedParams = suppliedBindings.map(_.parameter).filter(_ != null)
 
     val missing = requiredParams.diff(userSuppliedParams)
-    if (missing.nonEmpty) {
-      return BadRequest("Client: Missing required params passed: " + missing.toSet)
-    }
-
     val invalid = userSuppliedParams.diff(allPossibleParams)
-    if (invalid.nonEmpty) {
-      return BadRequest("Client: Unknown params passed: " + invalid.toSet)
+
+    if (missing.nonEmpty) {
+      BadRequest("Client: Missing required params passed: " + missing.toSet)
+    } else if (invalid.nonEmpty) {
+      BadRequest("Client: Unknown params passed: " + invalid.toSet)
+    } else {
+      service(buildRequest(suppliedBindings))
     }
-
-    val req = suppliedBindings
-      .sortBy(p => pathParams.indexOf(p.parameter))
-      .foldLeft(RequestBuilder(method)) { (requestBuild, next) => next(requestBuild) }.build()
-
-    service(req)
   }
+
+  private def buildRequest(suppliedBindings: Seq[Binding]): Request = suppliedBindings
+    .sortBy(p => pathParams.indexOf(p.parameter))
+    .foldLeft(RequestBuilder(method)) { (requestBuild, next) => next(requestBuild) }.build()
 }
