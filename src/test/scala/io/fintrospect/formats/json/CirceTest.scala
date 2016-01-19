7adc892aa78aa521063b461bab60152a9d5f2774
David Denton <denton.david@gmail.com>
2016-01-19 22:04:32 +0000

    dave - Added decoding support to circe

--- src/test/scala/io/fintrospect/formats/json/CirceTest.scala
+++ src/test/scala/io/fintrospect/formats/json/CirceTest.scala
@@ -1,29 +1,31 @@
 package io.fintrospect.formats.json
 
+import io.fintrospect.formats.json.JsonFormat.InvalidJsonForDecoding
+
 case class CirceStreetAddress(address: String)
 
-object CirceStreetAddress {
-}
-
 case class CirceLetter(to: CirceStreetAddress, from: CirceStreetAddress, message: String)
-
-object CirceLetter {
-}
 
 class CirceJsonResponseBuilderTest extends JsonResponseBuilderSpec(Circe)
 
 class CirceJsonFormatTest extends JsonFormatSpec(Circe.JsonFormat) {
 
   describe("Circe.JsonFormat") {
-    //    val aLetter = CirceLetter(CirceStreetAddress("my house"), CirceStreetAddress("your house"), "hi there")
-    //    it("roundtrips to JSON and back") {
-    //      val encoded = Circe.JsonFormat.encode(aLetter)(CirceLetter.Codec)
-    //      Circe.JsonFormat.decode[CirceLetter](encoded)(CirceLetter.Codec) shouldEqual aLetter
-    //    }
-    //
-    //    it("invalid extracted JSON throws up") {
-    //      intercept[InvalidJsonForDecoding](Circe.JsonFormat.decode[CirceLetter](Circe.JsonFormat.obj()))
-    //    }
+    val aLetter = CirceLetter(CirceStreetAddress("my house"), CirceStreetAddress("your house"), "hi there")
+
+    import io.circe._
+    import io.circe.generic.auto._
+    import io.circe.parse._
+    import io.circe.syntax._
+
+    it("roundtrips to JSON and back") {
+      val encoded = Circe.JsonFormat.encode(aLetter)
+      Circe.JsonFormat.decode[CirceLetter](encoded) shouldEqual aLetter
+    }
+
+    it("invalid extracted JSON throws up") {
+      intercept[InvalidJsonForDecoding](Circe.JsonFormat.decode[CirceLetter](Circe.JsonFormat.obj()))
+    }
   }
   override val expectedJson: String = """{"string":"hello","object":{"field1":"aString"},"int":1.0,"long":2,"decimal":1.2,"bigInt":12344,"bool":true,"null":null,"array":["world",true]}"""
 }
