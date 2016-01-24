7d603fd8f5ed0bf3ac3c911a8b8a47d2c53b7a64
David Denton <denton.david@gmail.com>
2016-01-24 09:12:22 +0000

    dave - adding content to ful example, and auto conversion of Views to Future[View]

--- src/main/scala/io/fintrospect/templating/CachingClasspathViews.scala
+++ src/main/scala/io/fintrospect/templating/CachingClasspathViews.scala
@@ -9,8 +9,7 @@ class CachingClasspathViews[X](fn: String => X, suffix: String) {
   private val classToTemplate = new ConcurrentHashMap[Class[_], X]().asScala
 
   def loadView[T <: View](view: T): X = classToTemplate.getOrElseUpdate(view.getClass, {
-    val s = "/" + view.template + suffix
-    Option(getClass.getResourceAsStream(s))
+    Option(getClass.getResourceAsStream("/" + view.template + suffix))
       .map(fromInputStream)
       .map(t => fn(t.mkString))
       .getOrElse({throw new ViewNotFound(view)})
--- src/main/scala/io/fintrospect/templating/View.scala
+++ src/main/scala/io/fintrospect/templating/View.scala
@@ -1,8 +1,16 @@
 package io.fintrospect.templating
 
+import com.twitter.util.Future
+
+import scala.language.implicitConversions
+
 /**
   * View model for use with templating system.
   */
 trait View {
   val template: String = getClass.getName.replace('.', '/')
+}
+
+object View {
+  implicit def toFuture(view: View): Future[View] = Future.value(view)
 }
