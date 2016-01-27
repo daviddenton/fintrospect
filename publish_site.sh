1dce37c31e45fc3ee835dd0984a988a95d7c0f39
David Denton <denton.david@gmail.com>
2016-01-27 18:44:31 +0000

    dave - scripts to make site

--- publish_site.sh
+++ publish_site.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo Releasing and publishing v$1
 
-npm cleanSite
+npm run cleanSite
 cd target
 git clone github.com/daviddenton/fintrospect.git www
 cd www
 git checkout --orphan gh-pages
 git rm -rf .
 cd ../..
-npm makeSite
+npm run makeSite
 cd target/www
 git add *
 git commit -a -m "releasing $1 version of site"
