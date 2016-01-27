9f69f3ff55914693395d46ed481600d064647537
David Denton <denton.david@gmail.com>
2016-01-27 18:46:14 +0000

    dave - scripts to make site

--- publish_site.sh
+++ publish_site.sh
@@ -3,7 +3,7 @@ echo Releasing and publishing v$1
 
 npm run cleanSite
 cd target
-git clone github.com/daviddenton/fintrospect.git www
+git clone git@github.com:daviddenton/fintrospect.git www
 cd www
 git checkout --orphan gh-pages
 git rm -rf .
