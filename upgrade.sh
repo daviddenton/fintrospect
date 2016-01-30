7e145497342918795bed71b84854e6af4a7163ba
David Denton <denton.david@gmail.com>
2016-01-30 18:49:20 +0000

    dave - added script to upgrade version numbers of sbt files

--- upgrade.sh
+++ upgrade.sh
@@ -4,6 +4,7 @@ set -e
 function upgrade {
     echo Upgrade $1 to $2
     find . -type f -name '*.md' | grep -v "node_modules" | grep -v "bower_components" | xargs sed -i '' s/"$1"/"$2"/g
+    sed -i '' s/"$1"/"$2"/g build.sbt
 }
 
 upgrade `./tools/jq .globals.fintrospect.old harp.json` `./tools/jq .globals.fintrospect.new harp.json`
