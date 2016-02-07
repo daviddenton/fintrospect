592b3d3aebf549992a3ac27ddd159a78e11d88fd
David Denton <denton.david@gmail.com>
2016-02-07 18:58:40 +0000

    dave - test for TestHttpServer

---
+++ src/test/scala/io/fintrospect/TestHttpServerTest.scala
@@ -0,0 +1,24 @@
+package io.fintrospect
+
+import com.twitter.finagle.{Http, Service}
+import com.twitter.finagle.http.Method.Get
+import com.twitter.finagle.http.{Request, Response, Status}
+import com.twitter.util.{Await, Future}
+import io.fintrospect.testing.TestHttpServer
+import org.scalatest.{FunSpec, ShouldMatchers}
+
+class TestHttpServerTest extends FunSpec with ShouldMatchers {
+
+  it("will serve routes that are passed to it") {
+    val statusToReturn = Status.Conflict
+
+    val server = new TestHttpServer(9888, RouteSpec().at(Get) bindTo (
+      () => Service.mk { r => Future.value(Response(statusToReturn)) }))
+    Await.result(server.start())
+
+    Await.result(Http.newService("localhost:9888", "")(Request())).status shouldBe statusToReturn
+
+    Await.result(server.stop())
+
+  }
+}
