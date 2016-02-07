5eaa4cefd43690461f5e536a125df4421e1a951f
David Denton <denton.david@gmail.com>
2016-02-07 19:13:55 +0000

    dave - test coverage for JSON renderer

--- src/test/scala/io/fintrospect/testing/TestHttpServerTest.scala
+++ src/test/scala/io/fintrospect/testing/TestHttpServerTest.scala
@@ -1,24 +1,37 @@
 package io.fintrospect.testing
 
 import com.twitter.finagle.http.Method.Get
+import com.twitter.finagle.http.Status._
 import com.twitter.finagle.http.{Request, Response, Status}
 import com.twitter.finagle.{Http, Service}
 import com.twitter.util.{Await, Future}
 import io.fintrospect.RouteSpec
-import org.scalatest.{FunSpec, ShouldMatchers}
+import org.scalatest.{BeforeAndAfterEach, FunSpec, ShouldMatchers}
 
-class TestHttpServerTest extends FunSpec with ShouldMatchers {
+class TestHttpServerTest extends FunSpec with ShouldMatchers with BeforeAndAfterEach {
+  val originalStatus = Conflict
+
+  private val server = new TestHttpServer(9888, RouteSpec().at(Get) bindTo (
+    () => Service.mk { r => Future.value(Response(originalStatus)) }))
+
+  override def beforeEach() = {
+    Await.result(server.start())
+  }
+
+  override def afterEach() = {
+    Await.result(server.stop())
+  }
 
   it("will serve routes that are passed to it") {
-    val statusToReturn = Status.Conflict
+    statusFrom(originalStatus)
+  }
 
-    val server = new TestHttpServer(9888, RouteSpec().at(Get) bindTo (
-      () => Service.mk { r => Future.value(Response(statusToReturn)) }))
-    Await.result(server.start())
+  it("can override status") {
+    server.respondWith(Accepted)
+    statusFrom(Accepted)
+  }
 
-    Await.result(Http.newService("localhost:9888", "")(Request())).status shouldBe statusToReturn
-
-    Await.result(server.stop())
-
+  private def statusFrom(expected: Status): Unit = {
+    Await.result(Http.newService("localhost:9888", "")(Request())).status shouldBe expected
   }
 }
