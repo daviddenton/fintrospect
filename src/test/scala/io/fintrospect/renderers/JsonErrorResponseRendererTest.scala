2f9a3aaab01e4b7ee09dcec081e982f449e1af0e
David Denton <denton.david@gmail.com>
2016-02-07 19:05:01 +0000

    dave - test coverage for JSON renderer

---
+++ src/test/scala/io/fintrospect/renderers/JsonErrorResponseRendererTest.scala
@@ -0,0 +1,23 @@
+package io.fintrospect.renderers
+
+import com.twitter.finagle.http.Status
+import io.fintrospect.formats.json.Json4s.Native.JsonFormat._
+import io.fintrospect.parameters.Query
+import io.fintrospect.renderers.JsonErrorResponseRenderer._
+import io.fintrospect.util.HttpRequestResponseUtil._
+import org.scalatest.{FunSpec, ShouldMatchers}
+
+class JsonErrorResponseRendererTest extends FunSpec with ShouldMatchers {
+
+  it("can build 400") {
+    val response = statusAndContentFrom(badRequest(Seq(Query.required.string("bob"))))
+    response._1 shouldBe Status.BadRequest
+    parse(response._2) \ "message" shouldBe string("Missing/invalid parameters")
+  }
+
+  it("can build 404") {
+    val response = statusAndContentFrom(notFound())
+    response._1 shouldBe Status.NotFound
+    parse(response._2) \ "message" shouldBe string("No route found on this path. Have you used the correct HTTP verb?")
+  }
+}
