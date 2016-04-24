019e0abf1945b9c58cfcfc980a2cf87ebe4de586
David Denton <denton.david@gmail.com>
2016-04-24 19:36:14 +0100

    daveD - add support for wildcard content types

---
+++ src/test/scala/examples/multicontenttypes/MultiContentTypeRoutes.scala
@@ -0,0 +1,49 @@
+package examples.multicontenttypes
+
+import com.twitter.finagle.http.Method.Get
+import com.twitter.finagle.http.Request
+import com.twitter.finagle.http.Status.Ok
+import com.twitter.finagle.http.filter.Cors
+import com.twitter.finagle.http.filter.Cors.HttpFilter
+import com.twitter.finagle.http.path.Root
+import com.twitter.finagle.{Http, Service}
+import io.fintrospect.{ContentTypes, ModuleSpec, MultiContentType, RouteSpec}
+
+/**
+  * Shows how to add routes which can serve multiple content types. Basically, the Accept header is checked
+  * against the list of supplied services and a match found. If there is no Accept header set in the request,
+  * the first service in the list is used. This means that there is NO sophisticated content negotiation implemented,
+  * although Wildcard Accept headers will also match the first supplied service mapping.
+  */
+object MultiContentTypeRoutes extends App {
+
+  private val serveJson = Service.mk { (rq: Request) => import io.fintrospect.formats.json.Argo.JsonFormat._
+ import io.fintrospect.formats.json.Argo.ResponseBuilder.implicits._
+    Ok(obj("field" -> string("value")))
+  }
+
+  private val serveXml = Service.mk {
+    import io.fintrospect.formats.Xml.ResponseBuilder.implicits._
+    (rq: Request) =>
+      Ok(<root>
+        <field>value</field>
+      </root>)
+  }
+
+  private val serve = MultiContentType(
+    ContentTypes.APPLICATION_SVG_XML -> serveXml,
+    ContentTypes.APPLICATION_JSON -> serveJson
+  )
+
+  val route = RouteSpec()
+    .producing(ContentTypes.APPLICATION_XML, ContentTypes.APPLICATION_JSON)
+    .at(Get) / "multi" bindTo serve
+
+  Http.serve(":8080", new HttpFilter(Cors.UnsafePermissivePolicy)
+    .andThen(ModuleSpec(Root).withRoute(route).toService))
+
+  println("See the service description at: http://localhost:8080")
+
+  Thread.currentThread().join()
+
+}
---
+++ src/test/scala/io/fintrospect/MultiContentTypeTest.scala
@@ -0,0 +1,54 @@
+package io.fintrospect
+
+import com.twitter.finagle.Service
+import com.twitter.finagle.http.Status.{NotAcceptable, Ok}
+import com.twitter.finagle.http.{Request, Response}
+import com.twitter.util.Await.result
+import com.twitter.util.Future
+import io.fintrospect.ContentTypes.{TEXT_HTML, APPLICATION_ATOM_XML, APPLICATION_JSON}
+import io.fintrospect.formats.ResponseBuilder
+import org.scalatest.{FunSpec, ShouldMatchers}
+
+class MultiContentTypeTest extends FunSpec with ShouldMatchers {
+
+  describe("MultiContentType") {
+    it("on no match, return 406") {
+      val r = result(MultiContentType()(requestWithAcceptHeaders(APPLICATION_JSON.value)))
+      r.status shouldBe NotAcceptable
+    }
+    it("when there are no accept header set, just chooses the first type") {
+      val r = result(MultiContentType(serviceForType(APPLICATION_ATOM_XML), serviceForType(APPLICATION_JSON))(requestWithAcceptHeaders()))
+      r.status shouldBe Ok
+      r.headerMap("Content-Type") should startWith(APPLICATION_ATOM_XML.value)
+    }
+
+    it("when there is a wildcard set, just chooses the first type") {
+      val r = result(MultiContentType(serviceForType(APPLICATION_ATOM_XML), serviceForType(APPLICATION_JSON))(requestWithAcceptHeaders("*/*")))
+      r.status shouldBe Ok
+      r.headerMap("Content-Type") should startWith(APPLICATION_ATOM_XML.value)
+    }
+
+    it("when there is an exact (case insensitive) match on a content type, use that") {
+      val r = result(MultiContentType(serviceForType(APPLICATION_ATOM_XML), serviceForType(APPLICATION_JSON))(requestWithAcceptHeaders("apPliCation/jSoN")))
+      r.status shouldBe Ok
+      r.headerMap("Content-Type") should startWith(APPLICATION_JSON.value)
+    }
+
+    it("ignores q values and levels") {
+      val r = result(MultiContentType(serviceForType(TEXT_HTML), serviceForType(APPLICATION_JSON))
+      (requestWithAcceptHeaders("text/*;q=0.3, text/html;q=0.7, text/html;level=1,application/bob;level=2;q=0.4, */*;q=0.5")))
+      r.status shouldBe Ok
+      r.headerMap("Content-Type") should startWith(TEXT_HTML.value)
+    }
+  }
+
+  def serviceForType(contentType: ContentType): (ContentType, Service[Request, Response]) = {
+    contentType -> Service.mk[Request, Response] { r => Future.value(ResponseBuilder.HttpResponse(contentType).build()) }
+  }
+
+  def requestWithAcceptHeaders(headers: String*): Request = {
+    val request = Request()
+    headers.foreach(value => request.headerMap.add("Accept", value))
+    request
+  }
+}
