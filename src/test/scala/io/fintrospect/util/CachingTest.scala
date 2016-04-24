ffff0e6b09f730bca6782d6ba994709f35aa7c2c
David Denton <denton.david@gmail.com>
2016-04-24 13:02:02 +0100

    daveD - added If-Modified-Since header filter

--- src/test/scala/io/fintrospect/util/CachingTest.scala
+++ src/test/scala/io/fintrospect/util/CachingTest.scala
@@ -8,7 +8,9 @@ import com.twitter.finagle.http.{Method, Request, Response, Status}
 import com.twitter.finagle.{Filter, Service}
 import com.twitter.util.Await.result
 import com.twitter.util.{Await, Future}
+import io.fintrospect.util.Caching.Response.FallbackCacheControl
 import io.fintrospect.util.Caching.{DefaultCacheTimings, MaxAgeTtl, StaleIfErrorTtl, StaleWhenRevalidateTtl}
+import io.fintrospect.util.TestClocks.fixed
 import org.scalatest.{FunSpec, ShouldMatchers}
 
 class CachingTest extends FunSpec with ShouldMatchers {
@@ -16,63 +18,79 @@ class CachingTest extends FunSpec with ShouldMatchers {
   private val maxAge = ofSeconds(10)
   private val timings = DefaultCacheTimings(MaxAgeTtl(maxAge), StaleIfErrorTtl(ofSeconds(2000)), StaleWhenRevalidateTtl(ofSeconds(3000)))
 
-  describe("FallbackCacheControl") {
-    def getResponseWith(cacheTimings: DefaultCacheTimings, response: Response) = Await.result(Caching.FallbackCacheControl(TestClocks.fixed, cacheTimings)(Request(), Service.mk((r) => Future.value(response))))
+  describe("request") {
 
-    it("adds the headers if they are not set") {
-      val responseWithNoHeaders = Response(Status.Ok)
-      val response = getResponseWith(timings, responseWithNoHeaders)
-      response.headerMap("Cache-Control") should be("public, max-age=10, stale-while-revalidate=3000, stale-if-error=2000")
-      response.headerMap("Expires") should be(RFC_1123_DATE_TIME.format(ZonedDateTime.now(TestClocks.fixed).plus(maxAge)))
-      response.headerMap("Vary") should be("Accept-Encoding")
-    }
-
-    it("does not overwrite the headers if they are set") {
-      val responseWithHeaders = Response(Status.Ok)
-      responseWithHeaders.headerMap("Cache-Control") = "bob"
-      responseWithHeaders.headerMap("Expires") = "rita"
-      responseWithHeaders.headerMap("Vary") = "sue"
-      val response = getResponseWith(timings, responseWithHeaders)
-      response.headerMap("Cache-Control") should be("bob")
-      response.headerMap("Expires") should be("rita")
-      response.headerMap("Vary") should be("sue")
+    describe("Adds If-Modified-Since") {
+      it("works") {
+        val maxAge = Duration.ofSeconds(1)
+        result(Caching.Request.AddIfModifiedSince(fixed, maxAge)(Request(),
+          Service.mk { req => Future.value(req.headerMap("If-Modified-Since")) })
+        ) shouldBe RFC_1123_DATE_TIME.format(ZonedDateTime.now(fixed).minus(maxAge))
+      }
     }
   }
 
-  describe("NoCache") {
-    it("does not cache non-GET requests") {
-      val r = responseWith(Caching.NoCache(), Request(Method.Put, ""))
-      r.headerMap.isEmpty shouldBe true
+  describe("response") {
+
+    describe("FallbackCacheControl") {
+      def getResponseWith(cacheTimings: DefaultCacheTimings, response: Response) = Await.result(FallbackCacheControl(TestClocks.fixed, cacheTimings)(Request(), Service.mk((r) => Future.value(response))))
+
+      it("adds the headers if they are not set") {
+        val responseWithNoHeaders = Response(Status.Ok)
+        val response = getResponseWith(timings, responseWithNoHeaders)
+        response.headerMap("Cache-Control") should be("public, max-age=10, stale-while-revalidate=3000, stale-if-error=2000")
+        response.headerMap("Expires") should be(RFC_1123_DATE_TIME.format(ZonedDateTime.now(TestClocks.fixed).plus(maxAge)))
+        response.headerMap("Vary") should be("Accept-Encoding")
+      }
+
+      it("does not overwrite the headers if they are set") {
+        val responseWithHeaders = Response(Status.Ok)
+        responseWithHeaders.headerMap("Cache-Control") = "bob"
+        responseWithHeaders.headerMap("Expires") = "rita"
+        responseWithHeaders.headerMap("Vary") = "sue"
+        val response = getResponseWith(timings, responseWithHeaders)
+        response.headerMap("Cache-Control") should be("bob")
+        response.headerMap("Expires") should be("rita")
+        response.headerMap("Vary") should be("sue")
+      }
     }
 
-    it("adds correct headers to GET requests") {
-      val r = responseWith(Caching.NoCache(), Request(Method.Get, ""))
-      r.headerMap("Expires") shouldBe "0"
-      r.headerMap("Cache-Control") shouldBe "private, must-revalidate"
+    describe("NoCache") {
+      it("does not cache non-GET requests") {
+        val r = responseWith(Caching.Response.NoCache(), Request(Method.Put, ""))
+        r.headerMap.isEmpty shouldBe true
+      }
+
+      it("adds correct headers to GET requests") {
+        val r = responseWith(Caching.Response.NoCache(), Request(Method.Get, ""))
+        r.headerMap("Expires") shouldBe "0"
+        r.headerMap("Cache-Control") shouldBe "private, must-revalidate"
+      }
+    }
+
+    describe("MaxAge") {
+      it("does not cache non-GET requests") {
+        val r = responseWith(Caching.Response.MaxAge(Clock.fixed(Instant.now(), ZoneId.systemDefault()), Duration.ofHours(1)), Request(Method.Put, ""))
+        r.headerMap.isEmpty shouldBe true
+      }
+
+      it("adds correct headers to GET requests based on current time") {
+        val now = ZonedDateTime.now()
+        val r = responseWith(Caching.Response.MaxAge(Clock.fixed(now.toInstant, ZoneId.systemDefault()), Duration.ofHours(1)), Request(Method.Get, ""))
+        r.headerMap("Expires") shouldBe now.plusHours(1).format(RFC_1123_DATE_TIME)
+        r.headerMap("Cache-Control") shouldBe "public, max-age=3600"
+      }
+    }
+
+    describe("Add eTag") {
+      it("passes through when predicate succeeds") {
+        val response: Response = Response()
+        response.contentString = "bob"
+        result(Caching.Response.AddETag[Request]()(Request(), Service.mk { req => Future.value(response) })).headerMap("ETag") shouldBe "9f9d51bc70ef21ca5c14f307980a29d8"
+      }
     }
   }
 
-  describe("MaxAge") {
-    it("does not cache non-GET requests") {
-      val r = responseWith(Caching.MaxAge(Clock.fixed(Instant.now(), ZoneId.systemDefault()), Duration.ofHours(1)), Request(Method.Put, ""))
-      r.headerMap.isEmpty shouldBe true
-    }
-
-    it("adds correct headers to GET requests based on current time") {
-      val now = ZonedDateTime.now()
-      val r = responseWith(Caching.MaxAge(Clock.fixed(now.toInstant, ZoneId.systemDefault()), Duration.ofHours(1)), Request(Method.Get, ""))
-      r.headerMap("Expires") shouldBe now.plusHours(1).format(RFC_1123_DATE_TIME)
-      r.headerMap("Cache-Control") shouldBe "public, max-age=3600"
-    }
-  }
-
-  describe("Add eTag") {
-    it("passes through when predicate succeeds") {
-      val response: Response = Response()
-      response.contentString = "bob"
-      result(Caching.AddETag[Request]()(Request(), Service.mk { req => Future.value(response) })).headerMap("ETag") shouldBe "9f9d51bc70ef21ca5c14f307980a29d8"
-    }
-  }
 
   def responseWith(caching: Filter[Request, Response, Request, Response], request: Request) = result(caching(request, Service.mk((r) => Future.value(Response()))))
 }
