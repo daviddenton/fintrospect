7482a527b65eed7e33d9f2593773c6233c5f3076
David Denton <denton.david@gmail.com>
2016-04-24 17:39:37 +0100

    daveD - moved tests around

--- src/test/scala/io/fintrospect/parameters/HeaderTest.scala
+++ src/test/scala/io/fintrospect/parameters/HeaderTest.scala
@@ -11,60 +11,64 @@ class HeaderTest extends FunSpec with ShouldMatchers {
   private val paramName = "name"
 
   describe("required") {
-    val param = Header.required.localDate(paramName)
+    describe("singular") {
+      val param = Header.required.localDate(paramName)
 
-    it("validate value from field") {
-      param.validate(messageWithHeaderValueOf(Option("2015-02-04"))) shouldEqual Right(Option(LocalDate.of(2015, 2, 4)))
-      param <-- messageWithHeaderValueOf(Option("2015-02-04")) shouldEqual LocalDate.of(2015, 2, 4)
-    }
+      it("validate value from field") {
+        param.validate(messageWithHeaderValueOf(Option("2015-02-04"))) shouldEqual Right(Option(LocalDate.of(2015, 2, 4)))
+        param <-- messageWithHeaderValueOf(Option("2015-02-04")) shouldEqual LocalDate.of(2015, 2, 4)
+      }
 
-    it("fails to retrieve invalid value") {
-      param.validate(messageWithHeaderValueOf(Option("notValid"))) shouldEqual Left(param)
-    }
+      it("fails to retrieve invalid value") {
+        param.validate(messageWithHeaderValueOf(Option("notValid"))) shouldEqual Left(param)
+      }
 
-    it("does not retrieve non existent value") {
-      param.validate(messageWithHeaderValueOf(None)) shouldEqual Left(param)
-    }
+      it("does not retrieve non existent value") {
+        param.validate(messageWithHeaderValueOf(None)) shouldEqual Left(param)
+      }
 
-    it("can rebind valid value") {
-      val inRequest = Request()
-      inRequest.headerMap.add("field", "123")
-      val bindings = Header.required.int("field") <-> inRequest
-      val outRequest = bindings.foldLeft(RequestBuilder(Get)) { (requestBuild, next) => next(requestBuild) }.build()
-      outRequest.headerMap("field") shouldEqual "123"
+      it("can rebind valid value") {
+        val inRequest = Request()
+        inRequest.headerMap.add("field", "123")
+        val bindings = Header.required.int("field") <-> inRequest
+        val outRequest = bindings.foldLeft(RequestBuilder(Get)) { (requestBuild, next) => next(requestBuild) }.build()
+        outRequest.headerMap("field") shouldEqual "123"
+      }
     }
   }
 
   describe("optional") {
-    val param = Header.optional.localDate(paramName)
+    describe("singular") {
+      val param = Header.optional.localDate(paramName)
 
-    it("validate value from field") {
-      param.validate(messageWithHeaderValueOf(Option("2015-02-04"))) shouldEqual Right(Option(LocalDate.of(2015, 2, 4)))
-      param <-- messageWithHeaderValueOf(Option("2015-02-04")) shouldEqual Option(LocalDate.of(2015, 2, 4))
-    }
+      it("validate value from field") {
+        param.validate(messageWithHeaderValueOf(Option("2015-02-04"))) shouldEqual Right(Option(LocalDate.of(2015, 2, 4)))
+        param <-- messageWithHeaderValueOf(Option("2015-02-04")) shouldEqual Option(LocalDate.of(2015, 2, 4))
+      }
 
-    it("fails to retrieve invalid value") {
-      param.validate(messageWithHeaderValueOf(Option("notValid"))) shouldEqual Left(param)
-    }
+      it("fails to retrieve invalid value") {
+        param.validate(messageWithHeaderValueOf(Option("notValid"))) shouldEqual Left(param)
+      }
 
-    it("does not retrieve non existent value") {
-      param.validate(messageWithHeaderValueOf(None)) shouldEqual Right(None)
-      param <-- Request() shouldEqual None
-    }
+      it("does not retrieve non existent value") {
+        param.validate(messageWithHeaderValueOf(None)) shouldEqual Right(None)
+        param <-- Request() shouldEqual None
+      }
 
-    it("can rebind valid value") {
-      val inRequest = Request()
-      inRequest.headerMap.add("field", "123")
-      val bindings = Header.optional.int("field") <-> inRequest
-      val outRequest = bindings.foldLeft(RequestBuilder(Get)) { (requestBuild, next) => next(requestBuild) }.build()
-      outRequest.headerMap("field") shouldEqual "123"
-    }
+      it("can rebind valid value") {
+        val inRequest = Request()
+        inRequest.headerMap.add("field", "123")
+        val bindings = Header.optional.int("field") <-> inRequest
+        val outRequest = bindings.foldLeft(RequestBuilder(Get)) { (requestBuild, next) => next(requestBuild) }.build()
+        outRequest.headerMap("field") shouldEqual "123"
+      }
 
-    it("does not rebind missing value") {
-      val inRequest = Request()
-      val bindings = Header.optional.int("field") <-> inRequest
-      val outRequest = bindings.foldLeft(RequestBuilder(Get)) { (requestBuild, next) => next(requestBuild) }.build()
-      outRequest.headerMap.get("field") shouldEqual None
+      it("does not rebind missing value") {
+        val inRequest = Request()
+        val bindings = Header.optional.int("field") <-> inRequest
+        val outRequest = bindings.foldLeft(RequestBuilder(Get)) { (requestBuild, next) => next(requestBuild) }.build()
+        outRequest.headerMap.get("field") shouldEqual None
+      }
     }
   }
 
