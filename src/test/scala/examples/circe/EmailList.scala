994c707fabd3ec6b4087947ae0dd26fc78f21f1e
David Denton <denton.david@gmail.com>
2016-04-29 15:55:59 +0100

    daveD - fleshed out example

--- src/test/scala/examples/circe/EmailList.scala
+++ src/test/scala/examples/circe/EmailList.scala
@@ -15,15 +15,13 @@ import io.fintrospect.parameters.{ParameterSpec, Path, StringParamType}
   * This endpoint shows how to manually create an HTTP response with some Circe-encoded content.
   */
 class EmailList(emails: Emails) {
-  private val emailAddress = Path(ParameterSpec[EmailAddress]("address", Option("user email"), StringParamType, EmailAddress, e => e.address))
-
   private val exampleEmail = Email(EmailAddress("you@github.com"), EmailAddress("wife@github.com"), "when are you going to be home for dinner", 250)
 
   private def forUser(emailAddress: EmailAddress) = Service.mk[Request, Response] { _ => Ok(encode(emails.forUser(emailAddress))) }
 
   val route = RouteSpec("list the inbox contents")
     .returning(responseSpec(Ok -> "list of emails for a user", Seq(exampleEmail)))
-    .at(Get) / "user" / emailAddress bindTo forUser
+    .at(Get) / "emails" / Path(EmailAddress.spec) bindTo forUser
 }
 
 
