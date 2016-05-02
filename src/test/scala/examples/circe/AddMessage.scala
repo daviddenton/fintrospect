2da0b4cbc8f18a80116602db70c8d4fde21d19c4
David Denton <denton.david@gmail.com>
2016-05-02 12:01:28 +0100

    daveD - docs

--- src/test/scala/examples/circe/AddMessage.scala
+++ src/test/scala/examples/circe/AddMessage.scala
@@ -3,32 +3,37 @@ package examples.circe
 import com.twitter.finagle.Service
 import com.twitter.finagle.http.Method.Post
 import com.twitter.finagle.http.Status.Ok
+import com.twitter.finagle.http.{Request, Response}
 import com.twitter.util.Future
 import io.circe.generic.auto._
 import io.fintrospect.RouteSpec
 import io.fintrospect.formats.json.Circe
 import io.fintrospect.formats.json.Circe.JsonFormat.{bodySpec, responseSpec}
-import io.fintrospect.parameters.{Body, ObjectParamType}
+import io.fintrospect.parameters.{Body, ObjectParamType, Path}
 
 /**
   * This endpoint uses the "Circe.Filters.AutoInOut" Filter to automatically create a HTTP 200 response from some returned case class content.
- */
+  */
 class AddMessage(emails: Emails) {
   private val exampleEmail = Email(EmailAddress("you@github.com"), EmailAddress("wife@github.com"), "when are you going to be home for dinner", 250)
 
   private val email = Body(bodySpec[Email](Option("email")), exampleEmail, ObjectParamType)
 
-  private val addEmail: Service[Email, Seq[Email]] = Service.mk {
-    newEmail: Email => {
-      emails.add(newEmail)
-      Future.value(emails.forUser(newEmail.to))
-    }
-  }
+  private def addEmail(address: EmailAddress): Service[Request, Response] =
+    Circe.Filters.AutoInOut(
+      Service.mk {
+        newEmail: Email => {
+          // validate that the receiver is as passed as the one in the URL
+          if (address == newEmail.to) emails.add(newEmail)
+          Future.value(emails.forUser(newEmail.to))
+        }
+      }
+    )
 
   val route = RouteSpec("add an email and return the new inbox contents for the receiver")
     .body(email)
     .returning(responseSpec(Ok -> "new list of emails for the 'to' user", Seq(exampleEmail)))
-    .at(Post) / "email" bindTo Circe.Filters.AutoInOut(addEmail)
+    .at(Post) / "email" / Path(EmailAddress.spec) bindTo addEmail
 }
 
 
