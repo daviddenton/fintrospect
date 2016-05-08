b64fc98ce75189b998bbc0d2c9dd67447d803497
David Denton <denton.david@gmail.com>
2016-05-08 19:07:25 +0100

    daveD - refactor of validation and pulled <--?() out up to Validatable

--- src/main/scala/io/fintrospect/parameters/Validatable.scala
+++ src/main/scala/io/fintrospect/parameters/Validatable.scala
@@ -1,8 +1,17 @@
 package io.fintrospect.parameters
 
+import scala.util.{Failure, Success, Try}
+
 /**
- * Provides validation about the presence of a value parameter/entity value in a particular context
- */
+  * Provides validation about the presence of a value parameter/entity value in a particular context
+  */
 trait Validatable[T, -From] {
+
+  /**
+    * Attempt to deserialise from the message object. Only use this method instead of <--() if you want to not
+    * declare your parameters in the RouteSpec(). Returns Failure(InvalidParameters) on failure.
+    */
+  def <--?(from: From): Try[Option[T]] = validate(from).fold(s => Failure(new InvalidParameters(s)), o => Success(o))
+
   def validate(from: From): Either[Seq[Parameter], Option[T]]
 }
