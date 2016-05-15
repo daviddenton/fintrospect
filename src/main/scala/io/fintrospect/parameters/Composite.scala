9cfe722950fa4ae18061ec2dad7d4c9117be8c0f
David Denton <denton.david@gmail.com>
2016-05-15 21:56:51 +0100

    daveD - tidy composite

--- src/main/scala/io/fintrospect/parameters/Composite.scala
+++ src/main/scala/io/fintrospect/parameters/Composite.scala
@@ -1,8 +1,8 @@
 package io.fintrospect.parameters
 
 /**
-  * A composite is an object which can be deserialised from many parameters in a Request, for example a
-  * number of query parameters.
+  * A composite is an outbound object which can be deserialised from many parameters in an inbound object, for example a
+  * number of query parameters in a Request.
   */
 object Composite {
   def apply[From, O](fn: From => Either[Seq[InvalidParameter], O]) = new Mandatory[O, From] {
