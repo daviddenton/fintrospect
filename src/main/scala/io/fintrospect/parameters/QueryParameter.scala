6ecfe21f250d49ded14259fe2f7e727a6ee8d120
David Denton <denton.david@gmail.com>
2016-05-15 18:12:21 +0100

    daveD - move get back up chain

--- src/main/scala/io/fintrospect/parameters/QueryParameter.scala
+++ src/main/scala/io/fintrospect/parameters/QueryParameter.scala
@@ -1,5 +1,12 @@
 package io.fintrospect.parameters
 
+import com.twitter.finagle.http.Request
+import io.fintrospect.parameters.InvalidParameter.Invalid
+import org.jboss.netty.handler.codec.http.QueryStringDecoder
+
+import scala.collection.JavaConverters._
+import scala.util.{Failure, Success, Try}
+
 abstract class QueryParameter[T](spec: ParameterSpec[_], val deserialize: Seq[String] => T)
   extends Parameter with Bindable[T, QueryBinding] {
 
@@ -7,6 +14,15 @@ abstract class QueryParameter[T](spec: ParameterSpec[_], val deserialize: Seq[St
   override val description = spec.description
   override val paramType = spec.paramType
   override val where = "query"
+
+  protected def get[O](request: Request, fn: T => O, default: Extraction[O]): Extraction[O] =
+    Option(new QueryStringDecoder(request.uri).getParameters.get(name))
+      .map(_.asScala.toSeq)
+      .map(v =>
+        Try(deserialize(v)) match {
+          case Success(d) => Extracted(fn(d))
+          case Failure(_) => ExtractionFailed(Invalid(this))
+        }).getOrElse(default)
 }
 
 abstract class SingleQueryParameter[T](spec: ParameterSpec[T])
