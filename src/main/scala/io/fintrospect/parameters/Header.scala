8549858c29c9221aa214c9c119799428c1d20a47
David Denton <denton.david@gmail.com>
2016-05-15 18:14:11 +0100

    daveD - move get back up chain

--- src/main/scala/io/fintrospect/parameters/Header.scala
+++ src/main/scala/io/fintrospect/parameters/Header.scala
@@ -31,24 +31,15 @@ object Header {
     self: Bindable[Seq[T], RequestBinding] =>
   }
 
-  private def get[I, O](param: HeaderParameter[I], message: Message, fn: I => O, default: Extraction[O]): Extraction[O] = {
-    val headers = message.headerMap.getAll(param.name)
-    val opt = if (headers.isEmpty) None else Some(headers.toSeq)
-    opt.map(v => Try(param.deserialize(v)) match {
-      case Success(d) => Extracted(fn(d))
-      case Failure(_) => ExtractionFailed(Invalid(param))
-    }).getOrElse(default)
-  }
-
   val required = new Parameters[HeaderParameter, Mandatory] with MultiParameters[MultiHeaderParameter, MandatorySeq] {
     override def apply[T](spec: ParameterSpec[T]) = new SingleHeaderParameter[T](spec)
       with Mandatory[T] {
-      override def <--?(message: Message) = get[T, T](this, message, identity, ExtractionFailed(Missing(this)))
+      override def <--?(message: Message) = get[T](message, identity, ExtractionFailed(Missing(this)))
     }
 
     override val multi = new Parameters[MultiHeaderParameter, MandatorySeq] {
       override def apply[T](spec: ParameterSpec[T]) = new MultiHeaderParameter[T](spec) with MandatorySeq[T] {
-        override def <--?(message: Message) = get[Seq[T], Seq[T]](this, message, identity, ExtractionFailed(Missing(this)))
+        override def <--?(message: Message) = get[Seq[T]](message, identity, ExtractionFailed(Missing(this)))
       }
     }
   }
@@ -56,12 +47,12 @@ object Header {
   val optional = new Parameters[HeaderParameter, Optional] with MultiParameters[MultiHeaderParameter, OptionalSeq] {
     override def apply[T](spec: ParameterSpec[T]) = new SingleHeaderParameter[T](spec)
       with Optional[T] {
-      override def <--?(message: Message) = get[T, Option[T]](this, message, Some(_), NotProvided())
+      override def <--?(message: Message) = get[Option[T]](message, Some(_), NotProvided())
     }
 
     override val multi = new Parameters[MultiHeaderParameter, OptionalSeq] {
       override def apply[T](spec: ParameterSpec[T]) = new MultiHeaderParameter[T](spec) with OptionalSeq[T] {
-        override def <--?(message: Message) = get[Seq[T], Option[Seq[T]]](this, message, Some(_), NotProvided())
+        override def <--?(message: Message) = get[Option[Seq[T]]](message, Some(_), NotProvided())
       }
     }
 
