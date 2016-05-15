cf4d8487847864578eee1143dc62f5c46a209c3f
David Denton <denton.david@gmail.com>
2016-05-15 21:56:08 +0100

    daveD - tidy composite

--- src/main/scala/io/fintrospect/parameters/Composite.scala
+++ src/main/scala/io/fintrospect/parameters/Composite.scala
@@ -1,18 +1,12 @@
 package io.fintrospect.parameters
 
-import com.twitter.finagle.http.Request
-
 /**
   * A composite is an object which can be deserialised from many parameters in a Request, for example a
   * number of query parameters.
   */
-trait Composite[T] extends Mandatory[T, Request]
-
 object Composite {
-  def apply[T](fn: Request => Either[Seq[InvalidParameter], T]) = new Composite[T] {
-    override def extract(from: Request): Extraction[T] = {
-      fn(from).fold(ip => ExtractionFailed[T](ip), Extracted(_))
-    }
+  def apply[From, O](fn: From => Either[Seq[InvalidParameter], O]) = new Mandatory[O, From] {
+    override protected def extract(from: From): Extraction[O] = fn(from).fold(ip => ExtractionFailed[O](ip), Extracted(_))
   }
 }
 
