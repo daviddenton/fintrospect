53ea5aff6fee3a3d078942b2c4d7c7639043bd59
David Denton <denton.david@gmail.com>
2016-05-29 09:27:39 +0100

    daveD - DRYing up types

--- src/main/scala/io/fintrospect/parameters/Query.scala
+++ src/main/scala/io/fintrospect/parameters/Query.scala
@@ -1,40 +1,26 @@
 package io.fintrospect.parameters
 
 import com.twitter.finagle.http.Request
+import io.fintrospect.parameters.types._
 
 /**
   * Parameters which are bound to the query part of a URL
   */
 object Query {
 
-  type Param[F, T, B <: Binding] = Parameter with Extractable[F, T] with Bindable[T, B]
-
-  trait Opt[T] extends io.fintrospect.parameters.Optional[Request, T]
-  with ExtractableParameter[Request, T]
-  with OptionalRebind[Request, T, QueryBinding]
-  with OptionalBindable[T, QueryBinding] {
+  trait Mandatory[T] extends Mand[Request, T, QueryBinding] {
     self: Param[Request, T, QueryBinding] =>
   }
 
-  trait Mand[T] extends io.fintrospect.parameters.Mandatory[Request, T]
-  with ExtractableParameter[Request, T]
-  with MandatoryRebind[Request, T, QueryBinding] {
-    self: Param[Request, T, QueryBinding] =>
-  }
-
-  trait Mandatory[T] extends Mand[T] {
-    self: Param[Request, T, QueryBinding] =>
-  }
-
-  trait MandatorySeq[T] extends Mand[Seq[T]] {
+  trait MandatorySeq[T] extends Mand[Request, Seq[T], QueryBinding] {
     self: Param[Request, Seq[T], QueryBinding] =>
   }
 
-  trait Optional[T] extends Opt[T] {
+  trait Optional[T] extends Opt[Request, T, QueryBinding] {
     self: Param[Request, T, QueryBinding] =>
   }
 
-  trait OptionalSeq[T] extends Opt[Seq[T]] {
+  trait OptionalSeq[T] extends Opt[Request, Seq[T], QueryBinding] {
     self: Param[Request, Seq[T], QueryBinding] =>
   }
 
