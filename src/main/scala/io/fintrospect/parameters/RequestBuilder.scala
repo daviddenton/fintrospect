c57f2cb67cd6c9d9ce3a149ab9c8162f55137187
David Denton <denton.david@gmail.com>
2016-05-29 10:30:08 +0100

    daveD - move builder

--- src/main/scala/io/fintrospect/parameters/RequestBuilder.scala
+++ src/main/scala/io/fintrospect/parameters/RequestBuilder.scala
@@ -1,6 +1,7 @@
 package io.fintrospect.parameters
 
 import com.twitter.finagle.http.{Method, Request}
+import io.fintrospect.util.Builder
 import org.jboss.netty.handler.codec.http.QueryStringEncoder
 
 case class RequestBuilder(method: Method,
