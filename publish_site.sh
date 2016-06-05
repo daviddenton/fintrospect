96eae995ebe6ab2ce5146ffd7a683ac6fdbbe1dc
David Denton <denton.david@gmail.com>
2016-06-05 11:26:11 +0100

    daved - moving site to github org page

--- publish_site.sh
+++ publish_site.sh
@@ -4,17 +4,17 @@ set -e
 
 npm run cleanSite
 rm -rf /tmp/site
-git clone git@github.com:daviddenton/fintrospect.git /tmp/site
+git clone git@github.com:fintrospect/fintrospect.github.io.git /tmp/site
 cd /tmp/site
-git checkout gh-pages
 cd -
 npm run makeSite
 ./sbt doc
 rm -rf /tmp/site/*
 mv target/scala-2.11/api target/www/
 mv -f target/www/* /tmp/site
+cp -f src/main/site/static/* /tmp/co
 cd /tmp/site
 git add *
 git commit -m "releasing $1 version of site"
-git push origin gh-pages
+git push origin master
 cd -
