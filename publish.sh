4513b4a57c52476c4b412a1e37daeb135034daf4
David Denton <denton.david@gmail.com>
2016-06-22 16:48:46 +0100

    adding sync maven central

--- publish.sh
+++ publish.sh
@@ -5,6 +5,6 @@ git tag -a $1 -m "released version $1 to bintray"
 git tag
 git push origin $1
 
-./sbt "set version:=\"$1\"" clean compile test +package +publish +bintray::syncMavenCentral
+./sbt "set version:=\"$1\"" clean compile test +package +publish bintray::syncMavenCentral
 
 ./publish_site.sh $1
