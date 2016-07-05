93c716dd910cc7309a5fb010dadf791dd3a50758
David Denton <denton.david@gmail.com>
2016-07-05 20:36:54 +0100

    refactored webforms

--- core/src/main/scala/io/fintrospect/parameters/Validator.scala
+++ core/src/main/scala/io/fintrospect/parameters/Validator.scala
@@ -24,7 +24,7 @@ class Validator[In <: Product] private(extractors: Product, value: => In) {
 object Validator {
 
   type Ex[T] = Extraction[T]
-  type Op[T] = Option[T]
+  type Opt[T] = Option[T]
 
   private def <--[T](e: Extraction[T]): Option[T] = e match {
     case Extracted(v) => Some(v)
@@ -39,43 +39,43 @@ object Validator {
   def mk[In <: Product](validation: Validator[In]) = validation
 
   implicit def tuple2ToValidator[A, B](in: (Ex[A], Ex[B])):
-  Validator[(Op[A], Op[B])] =
+  Validator[(Opt[A], Opt[B])] =
     new Validator(in, (<--(in._1), <--(in._2)))
 
   implicit def tuple3ToValidator[A, B, C](in: (Ex[A], Ex[B], Ex[C])):
-  Validator[(Op[A], Op[B], Op[C])] =
+  Validator[(Opt[A], Opt[B], Opt[C])] =
     new Validator(in, (<--(in._1), <--(in._2), <--(in._3)))
 
   implicit def tuple4ToValidator[A, B, C, D](in: (Ex[A], Ex[B], Ex[C], Ex[D])):
-  Validator[(Op[A], Op[B], Op[C], Op[D])] =
+  Validator[(Opt[A], Opt[B], Opt[C], Opt[D])] =
     new Validator(in, (<--(in._1), <--(in._2), <--(in._3), <--(in._4)))
 
   implicit def tuple5ToValidator[A, B, C, D, E](in: (Ex[A], Ex[B], Ex[C], Ex[D], Ex[E])):
-  Validator[(Op[A], Op[B], Op[C], Op[D], Op[E])] =
+  Validator[(Opt[A], Opt[B], Opt[C], Opt[D], Opt[E])] =
     new Validator(in, (<--(in._1), <--(in._2), <--(in._3), <--(in._4), <--(in._5)))
 
   implicit def tuple6ToValidator[A, B, C, D, E, F](in: (Ex[A], Ex[B], Ex[C], Ex[D], Ex[E], Ex[F])):
-  Validator[(Op[A], Op[B], Op[C], Op[D], Op[E], Op[F])] =
+  Validator[(Opt[A], Opt[B], Opt[C], Opt[D], Opt[E], Opt[F])] =
     new Validator(in, (<--(in._1), <--(in._2), <--(in._3), <--(in._4), <--(in._5), <--(in._6)))
 
   implicit def tuple7ToValidator[A, B, C, D, E, F, G]
   (in: (Ex[A], Ex[B], Ex[C], Ex[D], Ex[E], Ex[F], Ex[G])):
-  Validator[(Op[A], Op[B], Op[C], Op[D], Op[E], Op[F], Op[G])] =
+  Validator[(Opt[A], Opt[B], Opt[C], Opt[D], Opt[E], Opt[F], Opt[G])] =
     new Validator(in, (<--(in._1), <--(in._2), <--(in._3), <--(in._4), <--(in._5), <--(in._6), <--(in._7)))
 
   implicit def tuple8ToValidator[A, B, C, D, E, F, G, H]
   (in: (Ex[A], Ex[B], Ex[C], Ex[D], Ex[E], Ex[F], Ex[G], Ex[H])):
-  Validator[(Op[A], Op[B], Op[C], Op[D], Op[E], Op[F], Op[G], Op[H])] =
+  Validator[(Opt[A], Opt[B], Opt[C], Opt[D], Opt[E], Opt[F], Opt[G], Opt[H])] =
     new Validator(in, (<--(in._1), <--(in._2), <--(in._3), <--(in._4), <--(in._5), <--(in._6), <--(in._7), <--(in._8)))
 
   implicit def tuple9ToValidator[A, B, C, D, E, F, G, H, I]
   (in: (Ex[A], Ex[B], Ex[C], Ex[D], Ex[E], Ex[F], Ex[G], Ex[H], Ex[I])):
-  Validator[(Op[A], Op[B], Op[C], Op[D], Op[E], Op[F], Op[G], Op[H], Op[I])] =
+  Validator[(Opt[A], Opt[B], Opt[C], Opt[D], Opt[E], Opt[F], Opt[G], Opt[H], Opt[I])] =
     new Validator(in, (<--(in._1), <--(in._2), <--(in._3), <--(in._4), <--(in._5), <--(in._6), <--(in._7), <--(in._8), <--(in._9)))
 
   implicit def tuple10ToValidator[A, B, C, D, E, F, G, H, I, J]
   (in: (Ex[A], Ex[B], Ex[C], Ex[D], Ex[E], Ex[F], Ex[G], Ex[H], Ex[I], Ex[J])):
-  Validator[(Op[A], Op[B], Op[C], Op[D], Op[E], Op[F], Op[G], Op[H], Op[I], Op[J])] =
+  Validator[(Opt[A], Opt[B], Opt[C], Opt[D], Opt[E], Opt[F], Opt[G], Opt[H], Opt[I], Opt[J])] =
     new Validator(in, (<--(in._1), <--(in._2), <--(in._3), <--(in._4), <--(in._5), <--(in._6), <--(in._7), <--(in._8), <--(in._9), <--(in._10)))
 }
 
--- src/main/scala/examples/formvalidation/ReportAge.scala
+++ src/main/scala/examples/formvalidation/ReportAge.scala
@@ -4,7 +4,8 @@ import com.twitter.finagle.Service
 import com.twitter.finagle.http.Method.{Get, Post}
 import com.twitter.finagle.http.Request
 import io.fintrospect.parameters.StringValidation.EmptyIsInvalid
-import io.fintrospect.parameters.{BodyParameter, Parameter, Body, Form, FormField, WebForm}
+import io.fintrospect.parameters.Validator.Opt
+import io.fintrospect.parameters.{Body, Form, FormField, Parameter, Validated, Validation, ValidationFailed, Validator, WebForm}
 import io.fintrospect.templating.View
 import io.fintrospect.templating.View.viewToFuture
 import io.fintrospect.{RouteSpec, ServerRoutes}
@@ -16,21 +17,28 @@ import io.fintrospect.{RouteSpec, ServerRoutes}
   */
 class ReportAge extends ServerRoutes[Request, View] {
 
+  private val NAMES = Seq("Bob", "Johnny", "Rita", "Sue")
+
   // displays the initial form to the user
-  add(RouteSpec().at(Get) bindTo Service.mk { rq: Request => NameAndAgeForm() })
+  add(RouteSpec().at(Get) bindTo Service.mk { rq: Request => NameAndAgeForm(NAMES) })
 
   private val submit = Service.mk {
     rq: Request => {
-      val submitted = NameAndAgeForm.form <-- rq
-      if (submitted.isValid) {
-        val name = NameAndAgeForm.fields.name <-- submitted.form
-        val age = NameAndAgeForm.fields.age <-- submitted.form
-        DisplayUserAge(name, age)
-      } else {
-        NameAndAgeForm(submitted)
-      }
+      val postedForm = NameAndAgeForm.form <-- rq
+      if (postedForm.isValid)
+        doFieldValidationOn(postedForm) match {
+          case Validated(nextView) => nextView
+          case ValidationFailed(errors) => NameAndAgeForm(NAMES, postedForm.copy(errors = errors))
+        }
+      else NameAndAgeForm(NAMES, postedForm)
     }
   }
+
+  // we can use a validator here to provide extra validation
+  private def doFieldValidationOn(submitted: WebForm): Validation[DisplayUserAge] =
+    NameAndAgeForm.validate(submitted) {
+      case (theAge, theName) => DisplayUserAge(theName.get, theAge.get)
+    }
 
   // provides form validation on POST to same route
   add(RouteSpec().body(NameAndAgeForm.form).at(Post) bindTo submit)
@@ -39,6 +47,7 @@ class ReportAge extends ServerRoutes[Request, View] {
 case class DisplayUserAge(name: String, age: Int) extends View
 
 object NameAndAgeForm {
+
   object fields {
     val name = FormField.required.string("name", validation = EmptyIsInvalid)
     val age = FormField.required.integer("age")
@@ -46,16 +55,21 @@ object NameAndAgeForm {
 
   val form = Body.webForm(fields.name, fields.age)
 
+  def validate(submitted: WebForm) = {
+    Validator.mk(
+      NameAndAgeForm.fields.age <--?(submitted.form, "Must be an adult", _ >= 18),
+      NameAndAgeForm.fields.name <--?(submitted.form, "Must start with Capital letter", _.charAt(0).isUpper)
+    )
+  }
+
   private val FIELD_MESSAGES: Map[Parameter, String] = Map(
     NameAndAgeForm.fields.name -> "select the user name",
     NameAndAgeForm.fields.age -> "this should be a number")
 
-  def apply(): NameAndAgeForm = apply(WebForm(Form(), Nil))
-
-  def apply(webForm: WebForm): NameAndAgeForm = new NameAndAgeForm(
+  def apply(names: Seq[String], webForm: WebForm = WebForm(Form(), Nil)): NameAndAgeForm = new NameAndAgeForm(names,
     webForm.form.fields.mapValues(_.mkString(",")),
     Map(webForm.errors.map(ip => ip.param.name -> FIELD_MESSAGES(ip.param)).toSeq: _*)
   )
 }
 
-case class NameAndAgeForm(values: Map[String, String], errors: Map[String, String]) extends View
+case class NameAndAgeForm(names: Seq[String], values: Map[String, String], errors: Map[String, String]) extends View
