e9ac983aa25cfd47808cf7a6d6d7e6585288c269
David Denton <denton.david@gmail.com>
2016-07-05 20:44:12 +0100

    invalid params as a case class

--- core/src/main/scala/io/fintrospect/parameters/InvalidParameter.scala
+++ core/src/main/scala/io/fintrospect/parameters/InvalidParameter.scala
@@ -1,11 +1,27 @@
 package io.fintrospect.parameters
 
-case class InvalidParameter(param: Parameter, reason: String) {
+sealed trait InvalidParameter {
+  val param: Parameter
+  val reason: String
+
   override def toString = s"$reason:$param"
 }
 
 object InvalidParameter {
-  def Unknown(parameter: Parameter) = InvalidParameter(parameter, "Unknown")
-  def Missing(parameter: Parameter) = InvalidParameter(parameter, "Missing")
-  def Invalid(parameter: Parameter) = InvalidParameter(parameter, "Invalid")
+  def apply(param: Parameter, reason: String) = Custom(param, reason)
+
+  case class Custom(param: Parameter, reason: String) extends InvalidParameter
+
+  case class Unknown(param: Parameter) extends InvalidParameter {
+    val reason = "Unknown"
+  }
+
+  case class Missing(param: Parameter) extends InvalidParameter {
+    val reason = "Missing"
+  }
+
+  case class Invalid(param: Parameter) extends InvalidParameter {
+    val reason = "Invalid"
+  }
+
 }
