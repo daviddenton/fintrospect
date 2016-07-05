ee10b9b3cc1c4de6f8112860e173cd4e948f85da
David Denton <denton.david@gmail.com>
2016-07-05 20:04:17 +0100

    refactored webforms

--- src/main/scala/examples/formvalidation/ReportAge.scala
+++ src/main/scala/examples/formvalidation/ReportAge.scala
@@ -4,7 +4,7 @@ import com.twitter.finagle.Service
 import com.twitter.finagle.http.Method.{Get, Post}
 import com.twitter.finagle.http.Request
 import io.fintrospect.parameters.StringValidation.EmptyIsInvalid
-import io.fintrospect.parameters.{Body, Form, FormField, WebForm}
+import io.fintrospect.parameters.{BodyParameter, Parameter, Body, Form, FormField, WebForm}
 import io.fintrospect.templating.View
 import io.fintrospect.templating.View.viewToFuture
 import io.fintrospect.{RouteSpec, ServerRoutes}
@@ -17,28 +17,28 @@ import io.fintrospect.{RouteSpec, ServerRoutes}
 class ReportAge extends ServerRoutes[Request, View] {
 
   // displays the initial form to the user
-  add(RouteSpec().at(Get) bindTo Service.mk { rq: Request => AgeEntry() })
+  add(RouteSpec().at(Get) bindTo Service.mk { rq: Request => NameAndAgeForm() })
 
   private val submit = Service.mk {
     rq: Request => {
-      val submitted = AgeEntry.form <-- rq
+      val submitted = NameAndAgeForm.form <-- rq
       if (submitted.isValid) {
-        val name = AgeEntry.fields.name <-- submitted.form
-        val age = AgeEntry.fields.age <-- submitted.form
-        AgeReport(name, age)
+        val name = NameAndAgeForm.fields.name <-- submitted.form
+        val age = NameAndAgeForm.fields.age <-- submitted.form
+        DisplayUserAge(name, age)
       } else {
-        AgeEntry(submitted)
+        NameAndAgeForm(submitted)
       }
     }
   }
 
   // provides form validation on POST to same route
-  add(RouteSpec().body(AgeEntry.form).at(Post) bindTo submit)
+  add(RouteSpec().body(NameAndAgeForm.form).at(Post) bindTo submit)
 }
 
-case class AgeReport(name: String, age: Int) extends View
+case class DisplayUserAge(name: String, age: Int) extends View
 
-object AgeEntry {
+object NameAndAgeForm {
   object fields {
     val name = FormField.required.string("name", validation = EmptyIsInvalid)
     val age = FormField.required.integer("age")
@@ -46,16 +46,16 @@ object AgeEntry {
 
   val form = Body.webForm(fields.name, fields.age)
 
-  val FIELD_MESSAGES = Map(
-    AgeEntry.fields.name -> "select the user name",
-    AgeEntry.fields.age -> "this should be a number")
+  private val FIELD_MESSAGES: Map[Parameter, String] = Map(
+    NameAndAgeForm.fields.name -> "select the user name",
+    NameAndAgeForm.fields.age -> "this should be a number")
 
-  def apply(): AgeEntry = apply(WebForm(Form(), Nil))
+  def apply(): NameAndAgeForm = apply(WebForm(Form(), Nil))
 
-  def apply(webForm: WebForm): AgeEntry = new AgeEntry(
+  def apply(webForm: WebForm): NameAndAgeForm = new NameAndAgeForm(
     webForm.form.fields.mapValues(_.mkString(",")),
-    Map(webForm.errors.map(ip => ip.param.name -> ip.reason).toSeq: _*)
+    Map(webForm.errors.map(ip => ip.param.name -> FIELD_MESSAGES(ip.param)).toSeq: _*)
   )
 }
 
-case class AgeEntry(values: Map[String, String], errors: Map[String, String]) extends View
+case class NameAndAgeForm(values: Map[String, String], errors: Map[String, String]) extends View
