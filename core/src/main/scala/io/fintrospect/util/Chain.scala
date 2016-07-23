c0d6b3d00c6641bf9e26b6984d291b1315c2a865
David Denton <denton.david@gmail.com>
2016-07-23 11:06:31 +0100

    more validation experiments

--- core/src/main/scala/io/fintrospect/util/Chain.scala
+++ core/src/main/scala/io/fintrospect/util/Chain.scala
@@ -2,37 +2,37 @@ package io.fintrospect.util
 
 import com.twitter.util.Future
 
-sealed trait EitherF[+R, +L] {
-  def map[O](f: R => O): EitherF[O, L]
+sealed trait EitherF[+L, +R] {
+  def map[Ra](f: R => Ra): EitherF[L, Ra]
 
-  def flatMap[O, EO >: L](f: R => EitherF[O, EO]): EitherF[O, EO]
+  def flatMap[La >: L, Ra](f: R => EitherF[La, Ra]): EitherF[La, Ra]
 }
 
-case class RightF[I](value: I) extends EitherF[I, Nothing] {
-  override def map[O](f: I => O): EitherF[O, Nothing] = RightF(f(value))
+case class RightF[R](value: R) extends EitherF[Nothing, R] {
+  override def map[Ra](f: R => Ra): EitherF[Nothing, Ra] = RightF(f(value))
 
-  override def flatMap[O, EO >: Nothing](f: I => EitherF[O, EO]): EitherF[O, EO] = f(value)
+  override def flatMap[La >: Nothing, Ra](f: R => EitherF[La, Ra]): EitherF[La, Ra] = f(value)
 }
 
-case class LeftF[E](error: E) extends EitherF[Nothing, E] {
-  override def map[O](f: Nothing => O): EitherF[Nothing, E] = this
+case class LeftF[L](error: L) extends EitherF[L, Nothing] {
+  override def map[Ra](f: Nothing => Ra): EitherF[L, Nothing] = this
 
-  override def flatMap[O, EO >: E](f: Nothing => EitherF[O, EO]): EitherF[O, EO] = LeftF(error)
+  override def flatMap[La >: L, O](f: Nothing => EitherF[La, O]): EitherF[La, O] = LeftF(error)
 }
 
-class Chain[A, E] private(f: Future[EitherF[A, E]]) {
-  def map[B](next: A => EitherF[B, E]): Chain[B, E] = new Chain(f.map(check => check.flatMap(next)))
+class Chain[L, R] private(f: Future[EitherF[L, R]]) {
+  def map[Ra](next: R => EitherF[L, Ra]): Chain[L, Ra] = new Chain(f.map(check => check.flatMap(next)))
 
-  def flatMap[B](next: A => Future[EitherF[B, E]]): Chain[B, E] =
-    new Chain[B, E](f.flatMap {
+  def flatMap[Ra](next: R => Future[EitherF[L, Ra]]): Chain[L, Ra] =
+    new Chain[L, Ra](f.flatMap {
       case RightF(v) => next(v)
       case LeftF(e) => Future.value(LeftF(e))
     })
 
-  def end[B](fn: EitherF[A, E] => B) = f.map(fn)
+  def end[Ra](fn: EitherF[L, R] => Ra) = f.map(fn)
 }
 
 object Chain {
-  def apply[A, E](a: A): Chain[A, E] = new Chain[A, E](Future.value(RightF(a)))
+  def apply[L, R](a: R): Chain[L, R] = new Chain[L, R](Future.value(RightF(a)))
 }
 
