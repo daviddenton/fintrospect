faf808615ada440f7b7be8a305b6d3aa65c37040
David Denton <denton.david@gmail.com>
2016-07-23 11:14:39 +0100

    more validation experiments

---
+++ core/src/main/scala/io/fintrospect/util/EitherF.scala
@@ -0,0 +1,28 @@
+package io.fintrospect.util
+
+import com.twitter.util.Future
+
+import scala.util.Left
+
+/**
+  * Wrapper for Either which supports Futures as the wrapping type
+  */
+class EitherF[L, R] private(f: Future[Either[L, R]]) {
+  def map[Ra](next: R => Either[L, Ra]): EitherF[L, Ra] = new EitherF(f.map {
+    case Right(v) => next(v)
+    case Left(e) => Left(e)
+  })
+
+  def flatMap[Ra](next: R => Future[Either[L, Ra]]): EitherF[L, Ra] =
+    new EitherF[L, Ra](f.flatMap {
+      case Right(v) => next(v)
+      case Left(e) => Future.value(Left(e))
+    })
+
+  def end[Ra](fn: Either[L, R] => Ra) = f.map(fn)
+}
+
+object EitherF {
+  def eitherF[L, R](a: R): EitherF[L, R] = new EitherF[L, R](Future.value(Right(a)))
+}
+
