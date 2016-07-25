0d09cd570ee8bf87f698cd6ad23be764e01b1f5f
David Denton <denton.david@gmail.com>
2016-07-25 12:54:08 +0100

    removing self types

--- core/src/main/scala/io/fintrospect/parameters/types.scala
+++ core/src/main/scala/io/fintrospect/parameters/types.scala
@@ -7,14 +7,10 @@ object types {
   trait OptionalParameter[From, T, Bnd <: Binding] extends Optional[From, T]
   with ExtractableParameter[From, T]
   with OptionalRebind[From, T, Bnd] {
-    self: Parameter =>
     def -->(value: Option[T]): Iterable[Bnd] = value.map(-->).getOrElse(Nil)
-
   }
 
   trait MandatoryParameter[From, T, Bnd <: Binding] extends Mandatory[From, T]
   with ExtractableParameter[From, T]
-  with MandatoryRebind[From, T, Bnd] {
-    self: Parameter =>
-  }
+  with MandatoryRebind[From, T, Bnd]
 }
