3e154b70e2f9a153c9b5af61152da1f5879afa16
David Denton <denton.david@gmail.com>
2016-07-25 13:00:12 +0100

    remove OptionalBindable

--- core/src/main/scala/io/fintrospect/parameters/OptionalBindable.scala
+++
@@ -1,6 +0,0 @@
-package io.fintrospect.parameters
-
-/**
-  * For optional Parameters, adds the ability to bind an Optional value as well as a concrete value
-  */
-trait OptionalBindable[T, B <: Binding] extends Bindable[T, B]
