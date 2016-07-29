28b6235a7c687fd3a1ca35c9ecc0f762fcaad74f
David Denton <denton.david@gmail.com>
2016-07-29 17:02:27 +0100

    adding ProxyModule detail

--- core/src/main/scala/io/fintrospect/ProxyModule.scala
+++ core/src/main/scala/io/fintrospect/ProxyModule.scala
@@ -7,8 +7,10 @@ import io.fintrospect.Module.ServiceBinding
 import io.fintrospect.renderers.swagger2dot0.{ApiInfo, Swagger2dot0Json}
 
 object ProxyModule {
-  def apply(name: String, service: Service[Request, Response]): ProxyModule =
-    ProxyModule(name, service, ModuleSpec(Root, Swagger2dot0Json(ApiInfo(name, name, Option(s"Proxy services for $name")))))
+  def apply(name: String, service: Service[Request, Response], description: String = null): ProxyModule = {
+    val descriptionOption = if(description == null) Option(s"Proxy services for $name API") else Option(description)
+    ProxyModule(name, service, ModuleSpec(Root, Swagger2dot0Json(ApiInfo(name, name, descriptionOption))))
+  }
 }
 
 case class ProxyModule private(name: String, service: Service[Request, Response], spec: ModuleSpec[Request, Response]) extends Module {
