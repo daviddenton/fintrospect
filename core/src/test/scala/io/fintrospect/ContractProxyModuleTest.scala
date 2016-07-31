c697f3e9f264c17cf03f8b119cb13c4a62e5e7d2
David Denton <denton.david@gmail.com>
2016-07-31 21:49:51 +0100

    tests for ContractProxyModule

---
+++ core/src/test/scala/io/fintrospect/ContractProxyModuleTest.scala
@@ -0,0 +1,27 @@
+package io.fintrospect
+
+import com.twitter.finagle.Service
+import com.twitter.finagle.http.Method.Get
+import com.twitter.finagle.http.{Request, Status}
+import com.twitter.util.Await
+import io.fintrospect.formats.PlainText.ResponseBuilder.implicits._
+import io.fintrospect.parameters.Query
+import org.scalatest.{FunSpec, ShouldMatchers}
+
+object TestContract extends Contract {
+
+  object Endpoint extends ContractEndpoint {
+    val query = Query.required.string("query")
+    override val route = RouteSpec().taking(query).at(Get) / "hello"
+  }
+}
+
+class ContractProxyModuleTest extends FunSpec with ShouldMatchers {
+
+  describe("ContractProxyModule") {
+    it("Proxies to the underlying service") {
+      val svc = Service.mk { req: Request => Status.Ok(TestContract.Endpoint.query <-- req) }
+      Await.result(ContractProxyModule("remote", svc, TestContract).toService(Request("/hello?query=value"))).contentString shouldBe "value"
+    }
+  }
+}
