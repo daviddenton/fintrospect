07439ec875c7a5dc9b8038f18f610b04d5d38d0e
David Denton <denton.david@gmail.com>
2016-08-02 13:30:34 +0100

    put bintray sync at end of publish step

--- publish.sh
+++ publish.sh
@@ -7,6 +7,8 @@ git tag -a $1 -m "released version $1 to bintray"
 git tag
 git push origin $1
 
-./sbt clean compile test package publish bintraySyncMavenCentral
+./sbt clean compile test package publish
 
 ./publish_site.sh $1
+
+./sbt bintraySyncMavenCentral
