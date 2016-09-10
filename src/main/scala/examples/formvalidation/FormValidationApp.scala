a490a3a54042a57d6cb65b1e4763cdadf350a73b
David Denton <denton.david@gmail.com>
2016-09-10 11:17:40 +0100

    remove unused generic

--- src/main/scala/examples/formvalidation/FormValidationApp.scala
+++ src/main/scala/examples/formvalidation/FormValidationApp.scala
@@ -3,14 +3,13 @@ package examples.formvalidation
 import java.net.URL
 
 import com.twitter.finagle.Http
-import com.twitter.finagle.http.Request
 import com.twitter.finagle.http.filter.Cors
 import com.twitter.finagle.http.filter.Cors.HttpFilter
 import com.twitter.finagle.http.path.Root
 import io.fintrospect.ModuleSpec
 import io.fintrospect.formats.Html
 import io.fintrospect.renderers.SiteMapModuleRenderer
-import io.fintrospect.templating.{MustacheTemplates, RenderView, View}
+import io.fintrospect.templating.{MustacheTemplates, RenderView}
 
 /**
   * This example shows how to use Body.webform() and a templating engine to construct a validating form, with custom messages
@@ -20,7 +19,7 @@ object FormValidationApp extends App {
 
   val renderView = new RenderView(Html.ResponseBuilder, MustacheTemplates.HotReload("src/main/resources"))
 
-  val module = ModuleSpec[Request, View](Root, new SiteMapModuleRenderer(new URL("http://my.cool.app")), renderView)
+  val module = ModuleSpec(Root, new SiteMapModuleRenderer(new URL("http://my.cool.app")), renderView)
     .withDescriptionPath(_ / "sitemap.xml")
     .withRoutes(new ReportAge(new GreetingDatabase))
 
