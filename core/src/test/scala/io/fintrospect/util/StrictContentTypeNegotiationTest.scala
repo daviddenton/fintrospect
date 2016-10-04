c94febea2dfd47a1fa2a758d9b072bd59d5a36c4
David Denton <denton.david@gmail.com>
2016-10-04 13:14:34 +0100

    replace of() with apply(), so mucho nicer strictContentTypeNegotiation

--- core/src/test/scala/io/fintrospect/util/StrictContentTypeNegotiationTest.scala
+++ core/src/test/scala/io/fintrospect/util/StrictContentTypeNegotiationTest.scala
@@ -14,30 +14,30 @@ class StrictContentTypeNegotiationTest extends FunSpec with Matchers {
 
   describe("StrictContentTypeNegotiation") {
     it("on no match, return 406") {
-      val r = result(StrictContentTypeNegotiation.of(serviceForType(APPLICATION_ATOM_XML))(requestWithAcceptHeaders(APPLICATION_JSON.value)))
+      val r = result(StrictContentTypeNegotiation(serviceForType(APPLICATION_ATOM_XML))(requestWithAcceptHeaders(APPLICATION_JSON.value)))
       r.status shouldBe NotAcceptable
     }
 
     it("when there are no accept header set, just chooses the first type") {
-      val r = result(StrictContentTypeNegotiation.of(serviceForType(APPLICATION_ATOM_XML), serviceForType(APPLICATION_JSON))(requestWithAcceptHeaders()))
+      val r = result(StrictContentTypeNegotiation(serviceForType(APPLICATION_ATOM_XML), serviceForType(APPLICATION_JSON))(requestWithAcceptHeaders()))
       r.status shouldBe Ok
       r.headerMap("Content-Type") should startWith(APPLICATION_ATOM_XML.value)
     }
 
     it("when there is a wildcard set, just chooses the first type") {
-      val r = result(StrictContentTypeNegotiation.of(serviceForType(APPLICATION_ATOM_XML), serviceForType(APPLICATION_JSON))(requestWithAcceptHeaders("*/*")))
+      val r = result(StrictContentTypeNegotiation(serviceForType(APPLICATION_ATOM_XML), serviceForType(APPLICATION_JSON))(requestWithAcceptHeaders("*/*")))
       r.status shouldBe Ok
       r.headerMap("Content-Type") should startWith(APPLICATION_ATOM_XML.value)
     }
 
     it("when there is an exact (case insensitive) match on a content type, use that") {
-      val r = result(StrictContentTypeNegotiation.of(serviceForType(APPLICATION_ATOM_XML), serviceForType(APPLICATION_JSON))(requestWithAcceptHeaders("apPliCation/jSoN")))
+      val r = result(StrictContentTypeNegotiation(serviceForType(APPLICATION_ATOM_XML), serviceForType(APPLICATION_JSON))(requestWithAcceptHeaders("apPliCation/jSoN")))
       r.status shouldBe Ok
       r.headerMap("Content-Type") should startWith(APPLICATION_JSON.value)
     }
 
     it("ignores q values and levels") {
-      val r = result(StrictContentTypeNegotiation.of(serviceForType(TEXT_HTML), serviceForType(APPLICATION_JSON))
+      val r = result(StrictContentTypeNegotiation(serviceForType(TEXT_HTML), serviceForType(APPLICATION_JSON))
       (requestWithAcceptHeaders("text/*;q=0.3, text/html;q=0.7, text/html;level=1,application/bob;level=2;q=0.4, */*;q=0.5")))
       r.status shouldBe Ok
       r.headerMap("Content-Type") should startWith(TEXT_HTML.value)
