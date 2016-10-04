21b75a707adce68e11d16c14852dc0d0533f027c
David Denton <denton.david@gmail.com>
2016-10-04 16:39:39 +0100

    stage

--- src/test/scala/experiments/Exp.scala
+++ src/test/scala/experiments/Exp.scala
@@ -1,7 +1,8 @@
 package experiments
 
 import com.twitter.finagle.Service
-import com.twitter.finagle.http.{Request, Response}
+import com.twitter.finagle.http.path.{->, /, Path}
+import com.twitter.finagle.http.{Method, Request, Response}
 
 trait Builder[T] {
 }
@@ -14,7 +15,17 @@ class Builder1[Base, A](base: Base, a: Gen[A]) extends Builder[Base] {
   def /[B](b: Gen[B]) = new Builder2(base, a, b)
 }
 
-class Builder2[Base, A, B](base: Base, a: Gen[A], b: Gen[B]) extends Builder[Base] {
+trait SR {
+  def toPf(basePath: Path): PartialFunction[(Method, Path), Service[Request, Response]]
+}
+
+
+class Builder2[Base, A, B](base: Base, val ga: Gen[A], val gb: Gen[B]) extends Builder[Base] {
+  def bindTo(fn: (A, B) => Service[Request, Response]) = new SR {
+    override def toPf(basePath: Path) = {
+      case actualMethod -> path / ga(s1) / gb(s2) => fn(s1, s2)
+    }
+  }
 }
 
 trait Contract {
@@ -23,11 +34,11 @@ trait Contract {
 
   def gens: Seq[Gen[_]]
 
-  def at(): Builder0[self.type] = new Builder0(this)
 }
 
 case class Gen[T](t: T) {
   def get: T = t
+  def unapply(str: String): Option[T] = ???
 }
 
 class Contract0() extends Contract {
@@ -36,6 +47,8 @@ class Contract0() extends Contract {
   val gens = Nil
 
   def taking[A](a: Gen[A]): Contract1[A] = new Contract1(a)
+
+  //  def at(): Builder0[T] = new Builder0(this)
 }
 
 class Contract1[A](a: Gen[A]) extends Contract {
@@ -44,15 +57,22 @@ class Contract1[A](a: Gen[A]) extends Contract {
   val gens = Seq(a)
 
   def taking[B](b: Gen[B]) = new Contract2(a, b)
+
+  //  def at(): Builder0[T] = new Builder0(this)
 }
 
 class Contract2[A, B](a: Gen[A], b: Gen[B]) extends Contract {
   type T = (A, B) => Service[Request, Response]
 
   val gens = Seq(a, b)
+
+  def aaa(req: Request) = (a: A, b: B) => Service.mk[Request, Response] { req => ??? }
+
+  def at(): Builder0[Request => T] = new Builder0(aaa)
 }
 
 
 object Test extends App {
   private val builder = new Contract0().taking(Gen("string")).taking(Gen(123)).at() / Gen('a') / Gen(true)
+
 }
