27739b579fb4af251bd6e21b44a90235cb6a028a
David Denton <denton.david@gmail.com>
2016-10-05 17:08:03 +0100

    exp

--- src/test/scala/experiments/Exp.scala
+++ src/test/scala/experiments/Exp.scala
@@ -5,10 +5,10 @@ import com.twitter.finagle.http.path.{->, /, Path}
 import com.twitter.finagle.http.{Method, Request, Response}
 import com.twitter.util.Future
 
-trait BuilderWithParams[RqParams <: Product, T <: Request => RqParams] {
+trait BuilderWithParams[RqParams, T <: Request => RqParams] {
 }
 
-class BuilderWithParams0[Base <: Product](extract: Request => Base) extends BuilderWithParams[Base, Request => Base] {
+class BuilderWithParams0[Base](extract: Request => Base) extends BuilderWithParams[Base, Request => Base] {
   def /[A](a: Gen[A]): BuilderWithParams1[Base, A] = new BuilderWithParams1(extract, a)
 
   def bindTo(fn: Base => Future[Response]) = new SR {
@@ -20,7 +20,7 @@ class BuilderWithParams0[Base <: Product](extract: Request => Base) extends Buil
   }
 }
 
-class BuilderWithParams1[Base <: Product, A](extract: Request => Base, ga: Gen[A]) extends BuilderWithParams[Base, Request => Base] {
+class BuilderWithParams1[Base, A](extract: Request => Base, ga: Gen[A]) extends BuilderWithParams[Base, Request => Base] {
   def /[B](b: Gen[B]) = new BuilderWithParams2(extract, ga, b)
 
   def bindTo(fn: (A, Base) => Future[Response]) = new SR {
@@ -36,7 +36,7 @@ trait SR {
   def toPf(basePath: Path): PartialFunction[(Method, Path), Service[Request, Response]]
 }
 
-class BuilderWithParams2[Base <: Product, A, B](extract: Request => Base, val ga: Gen[A], val gb: Gen[B]) extends BuilderWithParams[Base, Request => Base] {
+class BuilderWithParams2[Base, A, B](extract: Request => Base, val ga: Gen[A], val gb: Gen[B]) extends BuilderWithParams[Base, Request => Base] {
   def bindTo(fn: (A, B, Base) => Future[Response]) = new SR {
     override def toPf(basePath: Path) = {
       case actualMethod -> path / ga(s1) / gb(s2) => Service.mk[Request, Response] {
@@ -70,7 +70,7 @@ class Contract0() extends Contract {
 
   def taking[A](a: Gen[A]): Contract1[A] = new Contract1(a)
 
-  //  def at(): Builder0[T] = new Builder0(this)
+  def at() = new BuilderWithParams0(identity)
 }
 
 class Contract1[A](a: Gen[A]) extends Contract {
@@ -80,7 +80,9 @@ class Contract1[A](a: Gen[A]) extends Contract {
 
   def taking[B](b: Gen[B]) = new Contract2(a, b)
 
-  //  def at(): Builder0[T] = new Builder0(this)
+  private def aaa(req: Request) = (a.from(req), req)
+
+  def at() = new BuilderWithParams0(aaa)
 }
 
 class Contract2[A, B](a: Gen[A], b: Gen[B]) extends Contract {
@@ -88,20 +90,53 @@ class Contract2[A, B](a: Gen[A], b: Gen[B]) extends Contract {
 
   val gens = Seq(a, b)
 
-  private def aaa(req: Request): (A, B) = (a.from(req), b.from(req))
+  private def aaa(req: Request): (A, B, Request) = (a.from(req), b.from(req), req)
 
-  def at(): BuilderWithParams0[(A, B)] = new BuilderWithParams0(aaa)
+  def at() = new BuilderWithParams0(aaa)
 }
 
 
 object Test extends App {
-  private val builder = new Contract0().taking(Gen("string")).taking(Gen(123)).at() / Gen('a') / Gen(true)
+  private val onePathOneParam = new Contract0().taking(Gen("string")).at() / Gen('a')
 
-  def svc(c: Char, b: Boolean, params: (String, Int)) = Future[Response] {
-    val (str, int) = params
+  def svc0(c: Char, params: (String, Request)) = Future[Response] {
     ???
   }
 
-  builder.bindTo(svc)
+  onePathOneParam.bindTo(svc0)
+
+  private val pathAndParams = new Contract0().taking(Gen("string")).taking(Gen(123)).at() / Gen('a') / Gen(true)
+
+  def svc(c: Char, b: Boolean, params: (String, Int, Request)) = Future[Response] {
+    val (str, int, req) = params
+    ???
+  }
+
+  pathAndParams.bindTo(svc)
+
+  private val pathOnly = new Contract0().at() / Gen('a') / Gen(true)
+
+  def svc2(c: Char, b: Boolean, req: Request) = Future[Response] {
+    ???
+  }
+
+  pathOnly.bindTo(svc2)
+
+  private val paramsOnly = new Contract0().taking(Gen("string")).taking(Gen(123)).at()
+
+  def svc3(params: (String, Int, Request)) = Future[Response] {
+    val (str, int, req) = params
+    ???
+  }
+
+  paramsOnly.bindTo(svc3)
+
+  private val nothing = new Contract0().at()
+
+  def svc4(req: Request) = Future[Response] {
+    ???
+  }
+
+  nothing.bindTo(svc4)
 
 }
