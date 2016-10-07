b1f2c6b2291180c8849a023791617aa118f7abd8
David Denton <denton.david@gmail.com>
2016-10-07 20:39:49 +0100

    exp

--- src/test/scala/experiments/NuServerRoute.scala
+++ src/test/scala/experiments/NuServerRoute.scala
@@ -3,9 +3,13 @@ package experiments
 import com.twitter.finagle.Service
 import com.twitter.finagle.http.path.Path
 import com.twitter.finagle.http.{Method, Request, Response}
+import io.fintrospect.parameters.PathParameter
 
-abstract class NuServerRoute(method: Method, pathFn: Path => Path) {
+abstract class NuServerRoute(method: Method, pathFn: Path => Path,
+                             pathParams: PathParameter[_]*) {
   def toPf(basePath: Path): PartialFunction[(Method, Path), Service[Request, Response]]
 
   def matches(actualMethod: Method, basePath: Path, actualPath: Path) = actualMethod == method && actualPath == pathFn(basePath)
+
+  def describeFor(basePath: Path): String = (pathFn(basePath).toString +: pathParams.map(_.toString())).mkString("/")
 }
