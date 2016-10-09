54d9b9711424778a73c5e0c06a133d8764be2aec
David Denton <denton.david@gmail.com>
2016-10-09 18:08:57 +0100

    exp

--- src/test/scala/experiments/Exp.scala
+++ src/test/scala/experiments/Exp.scala
@@ -22,10 +22,10 @@ case class PathBuilder0[In, Out](method: Method, terms: Terms, extract: Request 
   def bindToClient(svc: Service[Request, Response]): Out => Future[Response] =
     in => svc(buildRequest(method, bind(in)))
 
-  def bindTo(fn: In => Future[Response]) = new NuServerRoute(method, identity) {
+  def bindTo(fn: (In, Request) => Future[Response]) = new NuServerRoute(method, identity) {
     override def toPf(inPath: Path) = {
       case actualMethod -> path if matches(actualMethod, inPath, path) => terms.useFilter.andThen(Service.mk[Request, Response] {
-        (req: Request) => fn(extract(req))
+        (req: Request) => fn(extract(req), req)
       })
     }
   }
@@ -38,10 +38,10 @@ case class PathBuilder1[In, PP1](method: Method, terms: Terms, extract: Request 
 
   def /[NEXT](next: PathParam[NEXT]) = PathBuilder2(method, terms, extract, pp1, next)
 
-  def bindTo(fn: (PP1, In) => Future[Response]) = new NuServerRoute(method, identity, pp1) {
+  def bindTo(fn: (PP1, In, Request) => Future[Response]) = new NuServerRoute(method, identity, pp1) {
     override def toPf(inPath: Path) = {
       case actualMethod -> path / pp1(s1) if matches(actualMethod, inPath, path) => Service.mk[Request, Response] {
-        (req: Request) => fn(s1, extract(req))
+        (req: Request) => fn(s1, extract(req), req)
       }
     }
   }
@@ -56,10 +56,10 @@ case class PathBuilder2[In, PP1, PP2](method: Method, terms: Terms,
   def /[NEXT](next: PathParam[NEXT]) = PathBuilder3(method, terms, extract, pp1, pp2, next)
 
 
-  def bindTo(fn: (PP1, PP2, In) => Future[Response]) = new NuServerRoute(method, identity, pp1, pp2) {
+  def bindTo(fn: (PP1, PP2, In, Request) => Future[Response]) = new NuServerRoute(method, identity, pp1, pp2) {
     override def toPf(inPath: Path) = {
       case actualMethod -> path / pp1(s1) / pp2(s2) if matches(actualMethod, inPath, path) => Service.mk[Request, Response] {
-        (req: Request) => fn(s1, s2, extract(req))
+        (req: Request) => fn(s1, s2, extract(req), req)
       }
     }
   }
@@ -72,10 +72,10 @@ case class PathBuilder3[In, PP1, PP2, PP3](method: Method, terms: Terms,
 
   def /[NEXT](next: PathParam[NEXT]) = PathBuilder4(method, terms, extract, pp1, pp2, pp3, next)
 
-  def bindTo(fn: (PP1, PP2, PP3, In) => Future[Response]) = new NuServerRoute(method, identity, pp1, pp2, pp3) {
+  def bindTo(fn: (PP1, PP2, PP3, In, Request) => Future[Response]) = new NuServerRoute(method, identity, pp1, pp2, pp3) {
     override def toPf(inPath: Path) = {
       case actualMethod -> path / pp1(s1) / pp2(s2) / pp3(s3) if matches(actualMethod, inPath, path) => Service.mk[Request, Response] {
-        (req: Request) => fn(s1, s2, s3, extract(req))
+        (req: Request) => fn(s1, s2, s3, extract(req), req)
       }
     }
   }
@@ -88,10 +88,10 @@ case class PathBuilder4[In, PP1, PP2, PP3, PP4](method: Method, terms: Terms,
 
   def /[NEXT](next: PathParam[NEXT]) = PathBuilder5(method, terms, extract, pp1, pp2, pp3, pp4, next)
 
-  def bindTo(fn: (PP1, PP2, PP3, PP4, In) => Future[Response]) = new NuServerRoute(method, identity, pp1, pp2, pp3, pp4) {
+  def bindTo(fn: (PP1, PP2, PP3, PP4, In, Request) => Future[Response]) = new NuServerRoute(method, identity, pp1, pp2, pp3, pp4) {
     override def toPf(inPath: Path) = {
       case actualMethod -> path / pp1(s1) / pp2(s2) / pp3(s3) / pp4(s4) if matches(actualMethod, inPath, path) => Service.mk[Request, Response] {
-        (req: Request) => fn(s1, s2, s3, s4, extract(req))
+        (req: Request) => fn(s1, s2, s3, s4, extract(req), req)
       }
     }
   }
@@ -105,10 +105,10 @@ case class PathBuilder5[In, PP1, PP2, PP3, PP4, PP5](method: Method, terms: Term
 
   def /[NEXT](next: PathParam[NEXT]) = PathBuilder6(method, terms, extract, pp1, pp2, pp3, pp4, pp5, next)
 
-  def bindTo(fn: (PP1, PP2, PP3, PP4, PP5, In) => Future[Response]) = new NuServerRoute(method, identity, pp1, pp2, pp3, pp4, pp5) {
+  def bindTo(fn: (PP1, PP2, PP3, PP4, PP5, In, Request) => Future[Response]) = new NuServerRoute(method, identity, pp1, pp2, pp3, pp4, pp5) {
     override def toPf(inPath: Path) = {
       case actualMethod -> path / pp1(s1) / pp2(s2) / pp3(s3) / pp4(s4) / pp5(s5) if matches(actualMethod, inPath, path) => Service.mk[Request, Response] {
-        (req: Request) => fn(s1, s2, s3, s4, s5, extract(req))
+        (req: Request) => fn(s1, s2, s3, s4, s5, extract(req), req)
       }
     }
   }
@@ -117,10 +117,10 @@ case class PathBuilder5[In, PP1, PP2, PP3, PP4, PP5](method: Method, terms: Term
 case class PathBuilder6[In, PP1, PP2, PP3, PP4, PP5, PP6](method: Method, terms: Terms,
                                                             extract: Request => In,
                                                             pp1: PathParam[PP1], pp2: PathParam[PP2], pp3: PathParam[PP3], pp4: PathParam[PP4], pp5: PathParam[PP5], pp6: PathParam[PP6]) extends PathBuilder[In, Request => In] {
-  def bindTo(fn: (PP1, PP2, PP3, PP4, PP5, PP6, In) => Future[Response]) = new NuServerRoute(method, identity, pp1, pp2, pp3, pp4, pp5, pp6) {
+  def bindTo(fn: (PP1, PP2, PP3, PP4, PP5, PP6, In, Request) => Future[Response]) = new NuServerRoute(method, identity, pp1, pp2, pp3, pp4, pp5, pp6) {
     override def toPf(inPath: Path) = {
       case actualMethod -> path / pp1(s1) / pp2(s2) / pp3(s3) / pp4(s4) / pp5(s5) / pp6(s6) if matches(actualMethod, inPath, path) => Service.mk[Request, Response] {
-        (req: Request) => fn(s1, s2, s3, s4, s5, s6, extract(req))
+        (req: Request) => fn(s1, s2, s3, s4, s5, s6, extract(req), req)
       }
     }
   }
