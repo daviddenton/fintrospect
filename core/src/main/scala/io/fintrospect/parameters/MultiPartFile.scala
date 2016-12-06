5bb06e693ba5732a98cd6afe74b26cd67c3cad6d
David Denton <denton.david@gmail.com>
2016-12-06 14:38:06 +0000

    supporting multipart ondisk and in-memory files

--- core/src/main/scala/io/fintrospect/parameters/MultiPartFile.scala
+++ core/src/main/scala/io/fintrospect/parameters/MultiPartFile.scala
@@ -1,5 +1,39 @@
 package io.fintrospect.parameters
 
-import com.twitter.io.Buf
+import java.io.{File, RandomAccessFile}
+import java.nio.channels.FileChannel
 
-case class MultiPartFile(content: Buf, contentType: Option[String] = None, filename: Option[String] = None)
+import com.twitter.finagle.http.exp.Multipart.{FileUpload, InMemoryFileUpload, OnDiskFileUpload}
+import com.twitter.finagle.http.{FileElement, FormElement}
+import com.twitter.io.{Buf, Bufs}
+
+sealed trait MultiPartFile {
+  def toFormElement(name: String): FormElement
+}
+
+object MultiPartFile {
+  def apply(fileUpload: FileUpload): MultiPartFile = fileUpload match {
+    case InMemoryFileUpload(content, fileType, name, _) => InMemoryMultiPartFile(content, Option(fileType), Option(name))
+    case OnDiskFileUpload(file, fileType, name, _) => OnDiskMultiPartFile(file, Option(fileType), Option(name))
+  }
+}
+
+/**
+  * This is a multipart form file element that is under the max memory limit, and thus has been kept
+  */
+case class InMemoryMultiPartFile(content: Buf, contentType: Option[String] = None, filename: Option[String] = None) extends MultiPartFile {
+  def toFormElement(name: String): FormElement = FileElement(name, content, contentType, filename)
+}
+
+/**
+  * This is a multipart form file element that is over the max memory limit, and thus has been stored on disk temporarily
+  */
+case class OnDiskMultiPartFile(content: File, contentType: Option[String] = None, filename: Option[String] = None) extends MultiPartFile {
+
+  def toFormElement(name: String): FormElement = FileElement(name, toBuffer(), contentType, filename)
+
+  private def toBuffer() = {
+    val channel = new RandomAccessFile(content, "r").getChannel
+    Bufs.ownedBuf(channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size()))
+  }
+}
--- core/src/main/scala/io/fintrospect/parameters/MultiPartFormBody.scala
+++ core/src/main/scala/io/fintrospect/parameters/MultiPartFormBody.scala
@@ -1,8 +1,6 @@
 package io.fintrospect.parameters
 
 import com.twitter.finagle.http._
-import com.twitter.finagle.http.exp.Multipart.{FileUpload, InMemoryFileUpload, OnDiskFileUpload}
-import com.twitter.io.Buf
 import io.fintrospect.ContentTypes.MULTIPART_FORM
 import io.fintrospect.util.{Extraction, ExtractionError, ExtractionFailed, Extractor}
 
@@ -19,7 +17,7 @@ class MultiPartFormBody(formContents: Seq[FormField[_] with Extractor[Form, _]],
   def -->(value: Form): Seq[RequestBinding] =
     Seq(new RequestBinding(null, req => {
       val fields = value.fields.flatMap(f => f._2.map(g => SimpleElement(f._1, g))).toSeq
-      val files = value.files.flatMap(f => f._2.map(g => FileElement(f._1, g.content, g.contentType, g.filename))).toSeq
+      val files = value.files.flatMap(f => f._2.map(_.toFormElement(f._1))).toSeq
 
       val next = RequestBuilder()
         .url("http://notreallyaserver")
@@ -33,15 +31,9 @@ class MultiPartFormBody(formContents: Seq[FormField[_] with Extractor[Form, _]],
   override def <--?(message: Message): Extraction[Form] = message.asInstanceOf[Request].multipart
     .map(m => {
       val multipart = message.asInstanceOf[Request].multipart.get
-      Try(validator(formContents, Form(multipart.attributes.mapValues(_.toSet), multipart.files.mapValues(_.map(toMultipartFile).toSet)))) match {
+      Try(validator(formContents, Form(multipart.attributes.mapValues(_.toSet), multipart.files.mapValues(_.map(MultiPartFile(_)).toSet)))) match {
         case Success(form) => extractor(formContents, form)
         case Failure(e) => ExtractionFailed(formContents.filter(_.required).map(param => ExtractionError(param, "Could not parse")))
       }
     }).getOrElse(ExtractionFailed(formContents.filter(_.required).map(param => ExtractionError(param, "Could not parse"))))
-
-  private def toMultipartFile(fileUpload: FileUpload) = fileUpload match {
-    case InMemoryFileUpload(content, fileType, name, _) => MultiPartFile(content, Option(fileType), Option(name))
-    // FIXME - OnDiskUploads!
-    case OnDiskFileUpload(_, fileType, name, _) => MultiPartFile(Buf.Empty, Option(fileType), Option(name))
-  }
 }
