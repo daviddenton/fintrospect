0d505049dcdbdb980548be32d1fb16b4f5d97f33
David Denton <denton.david@gmail.com>
2016-12-09 07:53:13 +0000

    rename

--- core/src/main/scala/io/fintrospect/parameters/LengthValidations.scala
+++ core/src/main/scala/io/fintrospect/parameters/LengthValidations.scala
@@ -5,12 +5,12 @@ import scala.language.reflectiveCalls
 /**
   * Reusable validation functions for parameter and body types
   */
-trait Validations[T <: {def isEmpty() : Boolean}] {
+trait LengthValidations[T <: {def isEmpty() : Boolean}] {
   type Rule = (T => T)
   val EmptyIsValid: Rule = in => if (in == null) throw new IllegalArgumentException("Cannot be null") else in
   val EmptyIsInvalid: Rule = in => if (in.isEmpty()) throw new IllegalArgumentException("Cannot be empty") else in
 }
 
-object StringValidations extends Validations[String]
+object StringValidations extends LengthValidations[String]
 
-object FileValidations extends Validations[MultiPartFile]
+object FileValidations extends LengthValidations[MultiPartFile]
