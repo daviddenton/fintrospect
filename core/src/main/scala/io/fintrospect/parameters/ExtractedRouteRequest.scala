e222dc80361011a92b87980a9de0e0c8277a5f29
David Denton <denton.david@gmail.com>
2016-12-15 17:33:14 +0000

    tidy

--- core/src/main/scala/io/fintrospect/parameters/ExtractedRouteRequest.scala
+++ core/src/main/scala/io/fintrospect/parameters/ExtractedRouteRequest.scala
@@ -6,6 +6,4 @@ import io.fintrospect.util.{Extraction, ExtractionError, ExtractionFailed}
 case class ExtractedRouteRequest(override val request: Request, contents: Map[Any, Extraction[Any]]) extends RequestProxy {
   def get[T](p: Parameter): Extraction[T] = contents.getOrElse(p, ExtractionFailed(ExtractionError.Missing(p))).asInstanceOf[Extraction[T]]
   def get[T](p: Body[T]): Extraction[T] = contents.getOrElse(p, p.map(ExtractionError.Missing)).asInstanceOf[Extraction[T]]
-
-  override def toString(): String = super.toString() + ":"  + contents
 }
