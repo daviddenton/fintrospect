75916b0592f373669b6820d29b841bfad098959a
David Denton <denton.david@gmail.com>
2016-12-15 20:50:53 +0000

    tweak

--- core/src/main/scala/io/fintrospect/util/PathSegmentEncoderDecoder.scala
+++ core/src/main/scala/io/fintrospect/util/PathSegmentEncoderDecoder.scala
@@ -3,9 +3,7 @@ package io.fintrospect.util
 import java.net.URI
 
 object PathSegmentEncoderDecoder {
-  def encode(in: String): String = {
-    in.split("/").map(new URI("http", "localhost", _).getRawFragment).mkString("%2F")
-  }
+  def encode(in: String): String = in.split("/").map(new URI("http", "localhost", _).getRawFragment).mkString("%2F")
 
   def decode(in: String): String = new URI("http://localhost/" + in).getPath.substring(1)
 }
