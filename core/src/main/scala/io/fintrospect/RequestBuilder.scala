2067d125c0293b3aca8972dc95ea2ca1423b0db0
David Denton <denton.david@gmail.com>
2016-12-29 08:50:07 +0000

    removed unused trait

--- core/src/main/scala/io/fintrospect/RequestBuilder.scala
+++ core/src/main/scala/io/fintrospect/RequestBuilder.scala
@@ -1,13 +1,12 @@
 package io.fintrospect
 
 import com.twitter.finagle.http.{Method, Request}
-import io.fintrospect.util.Builder
 import org.jboss.netty.handler.codec.http.QueryStringEncoder
 
 case class RequestBuilder(method: Method,
                           uriParts: Seq[String] = Nil,
                           queries: Map[String, Seq[String]] = Map(),
-                          fn: Request => Request = identity) extends Builder[Request] {
+                          fn: Request => Request = identity) {
   def build(): Request = {
     val baseUri = uriParts.mkString("/")
     val uri = queries.foldLeft(new QueryStringEncoder(if (baseUri.isEmpty) "/" else baseUri)) {
