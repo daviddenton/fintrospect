d4b3407a3b38064a56686f50aa90c97a83410334
David Denton <denton.david@gmail.com>
2016-12-30 17:30:54 +0000

    Argonaut conversion for filters

--- argonaut/src/test/scala/io/fintrospect/formats/ArgonautTest.scala
+++ argonaut/src/test/scala/io/fintrospect/formats/ArgonautTest.scala
@@ -1,103 +1,31 @@
 package io.fintrospect.formats
 
 import argonaut.Argonaut.{casecodec1, casecodec3}
-import argonaut._
-import com.twitter.finagle.Service
+import argonaut.CodecJson
 import com.twitter.finagle.http.{Request, Status}
-import com.twitter.util.Await.result
-import com.twitter.util.{Await, Future}
-import io.fintrospect.formats.Argonaut.Filters.{AutoOptionalOut, AutoOut}
-import io.fintrospect.formats.Argonaut.JsonFormat._
-import io.fintrospect.formats.Argonaut.ResponseBuilder._
+import io.fintrospect.formats.Argonaut.JsonFormat.{compact, decode, encode, parse, _}
 import io.fintrospect.formats.Argonaut._
 import io.fintrospect.formats.JsonFormat.InvalidJsonForDecoding
-import io.fintrospect.parameters.{Body, Query}
-import org.scalatest.{FunSpec, Matchers}
+import io.fintrospect.parameters.{Body, BodySpec, Query}
+import helpers._
 
 import scala.language.reflectiveCalls
 
-case class ArgonautStreetAddress(address: String)
+object helpers {
+  implicit def StreetAddressCodec: CodecJson[StreetAddress] = casecodec1(StreetAddress.apply, StreetAddress.unapply)("address")
 
-object ArgonautStreetAddress {
-  implicit def Codec: CodecJson[ArgonautStreetAddress] = casecodec1(ArgonautStreetAddress.apply, ArgonautStreetAddress.unapply)("address")
+  implicit def LetterCodec: CodecJson[Letter] = casecodec3(Letter.apply, Letter.unapply)("to", "from", "message")
 }
 
-case class ArgonautLetter(to: ArgonautStreetAddress, from: ArgonautStreetAddress, message: String)
+class ArgonautFiltersTest extends AutoFiltersSpec(Argonaut.Filters) {
 
-object ArgonautLetter {
-  implicit def Codec: CodecJson[ArgonautLetter] = casecodec3(ArgonautLetter.apply, ArgonautLetter.unapply)("to", "from", "message")
-}
+  override def toString(l: Letter): String = compact(encode(l))
 
-class ArgonautFiltersTest extends FunSpec with Matchers {
+  override def fromString(s: String): Letter = decode[Letter](parse(s))
 
-  describe("Argonaut.Filters") {
-    val aLetter = ArgonautLetter(ArgonautStreetAddress("my house"), ArgonautStreetAddress("your house"), "hi there")
+  override def bodySpec: BodySpec[Letter] = Argonaut.bodySpec[Letter]()
 
-    val request = Request()
-    request.contentString = Argonaut.JsonFormat.compact(encode(aLetter))
-
-    describe("AutoInOut") {
-      it("returns Ok") {
-        val svc = Filters.AutoInOut(Service.mk { in: ArgonautLetter => Future.value(in) }, Status.Created)
-
-        val response = result(svc(request))
-        response.status shouldBe Status.Created
-        decode[ArgonautLetter](parse(response.contentString)) shouldBe aLetter
-      }
-    }
-
-    describe("AutoInOptionalOut") {
-      it("returns Ok when present") {
-        val svc = Filters.AutoInOptionalOut(Service.mk[ArgonautLetter, Option[ArgonautLetter]] { in => Future.value(Option(in)) })
-
-        val response = result(svc(request))
-        response.status shouldBe Status.Ok
-        decode[ArgonautLetter](parse(response.contentString)) shouldBe aLetter
-      }
-
-      it("returns NotFound when missing present") {
-        val svc = Filters.AutoInOptionalOut(Service.mk[ArgonautLetter, Option[ArgonautLetter]] { in => Future.value(None) })
-        result(svc(request)).status shouldBe Status.NotFound
-      }
-    }
-
-    describe("AutoIn") {
-      val svc = Filters.AutoIn(Body(bodySpec[ArgonautLetter]())).andThen(Service.mk { in: ArgonautLetter => Ok(encode(in)) })
-      it("takes the object from the request") {
-        decode[ArgonautLetter](parse(result(svc(request)).contentString)) shouldBe aLetter
-      }
-
-      it("rejects illegal content with a BadRequest") {
-        val request = Request()
-        request.contentString = "not xml"
-        Await.result(svc(request)).status shouldBe Status.BadRequest
-      }
-    }
-
-    describe("AutoOut") {
-      it("takes the object from the request") {
-        val svc = AutoOut[ArgonautLetter, ArgonautLetter](Status.Created).andThen(Service.mk { in: ArgonautLetter => Future.value(in) })
-        val response = result(svc(aLetter))
-        response.status shouldBe Status.Created
-        decode[ArgonautLetter](parse(response.contentString)) shouldBe aLetter
-      }
-    }
-
-    describe("AutoOptionalOut") {
-      it("returns Ok when present") {
-        val svc = AutoOptionalOut[ArgonautLetter, ArgonautLetter](Status.Created).andThen(Service.mk[ArgonautLetter, Option[ArgonautLetter]] { in => Future.value(Option(in)) })
-
-        val response = result(svc(aLetter))
-        response.status shouldBe Status.Created
-        decode[ArgonautLetter](parse(response.contentString)) shouldBe aLetter
-      }
-
-      it("returns NotFound when missing present") {
-        val svc = AutoOptionalOut[ArgonautLetter, ArgonautLetter](Status.Created).andThen(Service.mk[ArgonautLetter, Option[ArgonautLetter]] { in => Future.value(None) })
-        result(svc(aLetter)).status shouldBe Status.NotFound
-      }
-    }
-  }
+  override def toOut() = Argonaut.Filters.tToToOut[Letter]
 }
 
 class ArgonautJsonResponseBuilderTest extends JsonResponseBuilderSpec(Argonaut)
@@ -105,27 +33,27 @@ class ArgonautJsonResponseBuilderTest extends JsonResponseBuilderSpec(Argonaut)
 class ArgonautJsonFormatTest extends JsonFormatSpec(Argonaut) {
 
   describe("Argonaut.JsonFormat") {
-    val aLetter = ArgonautLetter(ArgonautStreetAddress("my house"), ArgonautStreetAddress("your house"), "hi there")
+    val aLetter = Letter(StreetAddress("my house"), StreetAddress("your house"), "hi there")
     it("roundtrips to JSON and back") {
-      val encoded = encode(aLetter)(ArgonautLetter.Codec)
-      decode[ArgonautLetter](encoded)(ArgonautLetter.Codec) shouldBe aLetter
+      val encoded = encode(aLetter)
+      decode[Letter](encoded) shouldBe aLetter
     }
 
     it("invalid extracted JSON throws up") {
-      intercept[InvalidJsonForDecoding](decode[ArgonautLetter](obj()))
+      intercept[InvalidJsonForDecoding](decode[Letter](obj()))
     }
 
     it("body spec decodes content") {
-      Body(bodySpec[ArgonautLetter]()) <-- Argonaut.ResponseBuilder.Ok(encode(aLetter)).build() shouldBe aLetter
+      Body(bodySpec[Letter]()) <-- Argonaut.ResponseBuilder.Ok(encode(aLetter)).build() shouldBe aLetter
     }
 
     it("param spec decodes content") {
-      val param = Query.required(parameterSpec[ArgonautLetter]("name"))
+      val param = Query.required(parameterSpec[Letter]("name"))
       (param <-- Request("?name=" + encode(aLetter))) shouldBe aLetter
     }
 
     it("response spec has correct code") {
-      responseSpec[ArgonautLetter](Status.Ok -> "ok", aLetter).status shouldBe Status.Ok
+      responseSpec[Letter](Status.Ok -> "ok", aLetter).status shouldBe Status.Ok
     }
 
   }
