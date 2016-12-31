9b90db459eada8c8fef11394760f4b47eadfe4f0
David Denton <denton.david@gmail.com>
2016-12-31 14:20:35 +0000

    Xml filters

--- core/src/main/scala/io/fintrospect/formats/Xml.scala
+++ core/src/main/scala/io/fintrospect/formats/Xml.scala
@@ -4,7 +4,9 @@ import com.twitter.finagle.http.{Request, Response, Status}
 import com.twitter.finagle.{Filter, Service}
 import com.twitter.io.Buf
 import io.fintrospect.ContentTypes
-import io.fintrospect.parameters.Body
+import io.fintrospect.formats.Xml.ResponseBuilder.HttpResponse
+import io.fintrospect.parameters.{Body, Mandatory}
+import io.fintrospect.util.{Extracted, ExtractionFailed}
 
 import scala.xml.Elem
 
@@ -17,52 +19,45 @@ object Xml {
     * Auto-marshalling filters which can be used to create Services which take and return Elem objects
     * instead of HTTP responses
     */
-  object Filters extends AutoFilters[Elem] {
+  object Filters {
 
-    override protected val responseBuilder = Xml.ResponseBuilder
+    import ResponseBuilder._
 
-    import Xml.ResponseBuilder._
+    type ToResponse[L] = (L) => ResponseBuilder[_]
+    type ToBody[BODY] = () => Body[BODY]
 
-    private val body = Body.xml(None)
+    def AutoIn[IN](body: Body[IN] with Mandatory[Request, IN]): Filter[Request, Response, IN, Response]
+    = Filter.mk[Request, Response, IN, Response] {
+      (req, svc) =>
+        body <--? req match {
+          case Extracted(in) => svc(in.get)
+          case ExtractionFailed(_) => HttpResponse(Status.BadRequest)
+        }
+    }
+
+    def AutoInOut(svc: Service[Elem, Elem], successStatus: Status = Status.Ok): Service[Request, Response] =
+      AutoIn(Body.xml(None)).andThen(AutoOut[Elem](successStatus)).andThen(svc)
 
     private def toResponse(successStatus: Status = Status.Ok) = (out: Elem) => HttpResponse(successStatus).withContent(out)
 
-    /**
-      * Wrap the enclosed service with auto-marshalling of input and output Elem instances for HTTP POST scenarios
-      * which return an Elem.
-      * HTTP OK is returned by default in the auto-marshalled response (overridable).
-      */
-    def AutoInOut(svc: Service[Elem, Elem], successStatus: Status = Status.Ok): Service[Request, Response] =
-      AutoIn(body).andThen(AutoOut[Elem](successStatus)).andThen(svc)
-
-    /**
-      * Wrap the enclosed service with auto-marshalling of input and output Elem instances for HTTP POST scenarios
-      * which may return an Elem.
-      * HTTP OK is returned by default in the auto-marshalled response (overridable), otherwise a 404 is returned
-      */
     def AutoInOptionalOut(svc: Service[Elem, Option[Elem]], successStatus: Status = Status.Ok)
-    : Service[Request, Response] = _AutoInOptionalOut[Elem, Elem](svc, body, toResponse(successStatus))
+    : Service[Request, Response] = AutoIn[Elem](Body.xml(None)).andThen(AutoOptionalOut[Elem](successStatus)).andThen(svc)
 
-    /**
-      * Filter to provide auto-marshalling of output Elem instances for HTTP scenarios where an object is returned.
-      * HTTP OK is returned by default in the auto-marshalled response (overridable).
-      */
-    def AutoOut[IN](successStatus: Status = Status.Ok): Filter[IN, Response, IN, Elem] = _AutoOut(toResponse(successStatus))
+    def AutoOut[IN](successStatus: Status = Status.Ok): Filter[IN, Response, IN, Elem] = Filter.mk[IN, Response, IN, Elem] { (req, svc) => svc(req).map(t => toResponse(successStatus)(t).build()) }
 
-    /**
-      * Filter to provide auto-marshalling of Elem instances for HTTP scenarios where an object may not be returned
-      * HTTP OK is returned by default in the auto-marshalled response (overridable), otherwise a 404 is returned
-      */
     def AutoOptionalOut[IN](successStatus: Status = Status.Ok): Filter[IN, Response, IN, Option[Elem]]
-    = _AutoOptionalOut(toResponse(successStatus))
-
+    = Filter.mk[IN, Response, IN, Option[Elem]] {
+      (req, svc) => svc(req).map(_.map(toResponse(successStatus)).getOrElse(HttpResponse(Status.NotFound)).build())
+    }
   }
 
   object ResponseBuilder extends AbstractResponseBuilder[Elem] {
 
     private def format(node: Elem): String = node.toString()
 
-    private def formatErrorMessage(errorMessage: String): Elem = <message>{errorMessage}</message>
+    private def formatErrorMessage(errorMessage: String): Elem = <message>
+      {errorMessage}
+    </message>
 
     private def formatError(throwable: Throwable): Elem = formatErrorMessage(Option(throwable.getMessage).getOrElse(throwable.getClass.getName))
 
