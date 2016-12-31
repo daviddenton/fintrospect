08f463704eed316d6b88f49620d39cce8922420b
David Denton <denton.david@gmail.com>
2016-12-31 13:17:08 +0000

    tidy test classes

--- circe/src/test/scala/io/fintrospect/formats/CirceTest.scala
+++ circe/src/test/scala/io/fintrospect/formats/CirceTest.scala
@@ -9,15 +9,7 @@ import io.fintrospect.parameters.{Body, BodySpec, Query}
 
 import scala.language.reflectiveCalls
 
-
-case class CirceStreetAddress(address: String)
-
-case class CirceLetter(to: CirceStreetAddress, from: CirceStreetAddress, message: String)
-
-case class CirceLetterOpt(to: CirceStreetAddress, from: CirceStreetAddress, message: Option[String])
-
 class CirceJsonResponseBuilderTest extends JsonResponseBuilderSpec(Circe)
-
 
 class CirceFiltersTest extends AutoFiltersSpec(Circe.Filters) {
 
@@ -32,52 +24,52 @@ class CirceJsonFormatTest extends JsonFormatSpec(Circe) {
   import io.circe.generic.auto._
 
   describe("Circe.JsonFormat") {
-    val aLetter = CirceLetter(CirceStreetAddress("my house"), CirceStreetAddress("your house"), "hi there")
+    val aLetter = Letter(StreetAddress("my house"), StreetAddress("your house"), "hi there")
 
     it("roundtrips to JSON and back") {
       val encoded = encode(aLetter)
-      decode[CirceLetter](encoded) shouldBe aLetter
+      decode[Letter](encoded) shouldBe aLetter
     }
 
     it("patchbody modifies original object with a non-null value") {
-      val original = CirceLetterOpt(CirceStreetAddress("my house"), CirceStreetAddress("your house"), None)
+      val original = LetterOpt(StreetAddress("my house"), StreetAddress("your house"), None)
       val modifier = encode(obj("message" -> string("hi there")))
-      val modifyLetter = patcher[CirceLetterOpt](modifier)
-      modifyLetter(original) shouldBe CirceLetterOpt(CirceStreetAddress("my house"), CirceStreetAddress("your house"), Option("hi there"))
+      val modifyLetter = patcher[LetterOpt](modifier)
+      modifyLetter(original) shouldBe LetterOpt(StreetAddress("my house"), StreetAddress("your house"), Option("hi there"))
     }
 
     // wait for circe 0.6.X, where this bug will be fixed - https://github.com/travisbrown/circe/issues/304
     ignore("patcher modifies original object with a null value") {
-      val original = CirceLetterOpt(CirceStreetAddress("my house"), CirceStreetAddress("your house"), Option("hi there"))
+      val original = LetterOpt(StreetAddress("my house"), StreetAddress("your house"), Option("hi there"))
       val modifier = encode(obj())
-      val modifyLetter = patcher[CirceLetterOpt](modifier)
-      modifyLetter(original) shouldBe CirceLetterOpt(CirceStreetAddress("my house"), CirceStreetAddress("your house"), None)
+      val modifyLetter = patcher[LetterOpt](modifier)
+      modifyLetter(original) shouldBe LetterOpt(StreetAddress("my house"), StreetAddress("your house"), None)
     }
 
     it("invalid extracted JSON throws up") {
-      intercept[InvalidJsonForDecoding](decode[CirceLetter](Circe.JsonFormat.obj()))
+      intercept[InvalidJsonForDecoding](decode[Letter](Circe.JsonFormat.obj()))
     }
 
     it("body spec decodes content") {
-      (Body(bodySpec[CirceLetter]()) <-- Circe.ResponseBuilder.Ok(encode(aLetter)).build()) shouldBe aLetter
+      (Body(bodySpec[Letter]()) <-- Circe.ResponseBuilder.Ok(encode(aLetter)).build()) shouldBe aLetter
     }
 
     it("patch body can be used to modify an existing case class object") {
-      val letterWithNoMessage = CirceLetterOpt(CirceStreetAddress("my house"), CirceStreetAddress("your house"), None)
+      val letterWithNoMessage = LetterOpt(StreetAddress("my house"), StreetAddress("your house"), None)
       val modifiedMessage = encode(obj("message" -> string("hi there")))
-      val modifiedLetterWithMessage = CirceLetterOpt(CirceStreetAddress("my house"), CirceStreetAddress("your house"), Some("hi there"))
+      val modifiedLetterWithMessage = LetterOpt(StreetAddress("my house"), StreetAddress("your house"), Some("hi there"))
 
-      val patch = patchBody[CirceLetterOpt](None, modifiedLetterWithMessage) <-- Circe.ResponseBuilder.Ok(modifiedMessage).build()
+      val patch = patchBody[LetterOpt](None, modifiedLetterWithMessage) <-- Circe.ResponseBuilder.Ok(modifiedMessage).build()
 
       patch(letterWithNoMessage) shouldBe modifiedLetterWithMessage
     }
 
     it("response spec has correct code") {
-      Circe.responseSpec[CirceLetter](Status.Ok -> "ok", aLetter).status shouldBe Status.Ok
+      Circe.responseSpec[Letter](Status.Ok -> "ok", aLetter).status shouldBe Status.Ok
     }
 
     it("param spec decodes content") {
-      val param = Query.required(parameterSpec[CirceLetter]("name"))
+      val param = Query.required(parameterSpec[Letter]("name"))
       (param <-- Request("?name=" + encode(aLetter))) shouldBe aLetter
     }
   }
