1eb1504a7eb1389b20a859b06996c448dc714236
David Denton <denton.david@gmail.com>
2017-01-08 19:40:12 +0000

    changed ParameterSpec description to not be a pointless Option[String] (default to null instead). Nulls are generally bad, but in this case, we're mostly interested in API niceness.

--- argonaut/src/main/scala/io/fintrospect/formats/Argonaut.scala
+++ argonaut/src/main/scala/io/fintrospect/formats/Argonaut.scala
@@ -75,6 +75,6 @@ object Argonaut extends JsonLibrary[Json, Json] {
   /**
     * Convenience method for creating ParameterSpecs that just use straight JSON encoding/decoding logic
     */
-  def parameterSpec[R](name: String, description: Option[String] = None)(implicit encodec: EncodeJson[R], decodec: DecodeJson[R]) =
-    ParameterSpec.json(name, description.orNull, this).map(j => JsonFormat.decode[R](j)(decodec), (u: R) => JsonFormat.encode(u)(encodec))
+  def parameterSpec[R](name: String, description: String = null)(implicit encodec: EncodeJson[R], decodec: DecodeJson[R]) =
+    ParameterSpec.json(name, description, this).map(j => JsonFormat.decode[R](j)(decodec), (u: R) => JsonFormat.encode(u)(encodec))
 }
