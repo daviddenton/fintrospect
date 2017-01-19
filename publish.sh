24e80572e4c98713cdb6b929bf5afa93a131f534
David Denton <denton.david@gmail.com>
2017-01-19 15:17:37 +0000

    publish all versions

--- publish.sh
+++ publish.sh
@@ -7,7 +7,7 @@ git tag -a $1 -m "released version $1 to bintray"
 git tag
 git push origin $1
 
-./sbt clean compile test package publish
+./sbt clean compile test +package +publish
 
 ./publish_site.sh $1
 
