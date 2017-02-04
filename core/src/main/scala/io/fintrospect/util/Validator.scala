5af248fc94a20a320d509685b0e055c1e652afd4
David Denton <denton.david@gmail.com>
2017-02-04 16:04:32 +0000

    validator

--- core/src/main/scala/io/fintrospect/util/Validator.scala
+++ core/src/main/scala/io/fintrospect/util/Validator.scala
@@ -25,7 +25,7 @@ object Validator {
   type Opt[T] = Option[T]
 
   private def <--[T](e: Extraction[T]): Option[T] = e match {
-    case Extracted(v) => ???
+    case Extracted(v) => Some(v)
     case ExtractionFailed(_) => None
   }
 
