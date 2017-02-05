c120957bf12f64447c50926c09d28af69270df35
David Denton <denton.david@gmail.com>
2017-02-05 11:44:05 +0000

    cleaning up parameter hierarchy

--- core/src/main/scala/io/fintrospect/parameters/Parameter.scala
+++ core/src/main/scala/io/fintrospect/parameters/Parameter.scala
@@ -76,47 +76,38 @@ trait MandatoryParameter[From, T, Bnd <: Binding] extends Mandatory[From, T]
 
 }
 
-abstract class ExtractableParameter[T, From, B <: Binding, Bt, O] (spec: ParameterSpec[T], eab: ParameterExtractAndBind[From, String, B],
-                                                                   bindFn: Bt => Seq[T])
-  extends Parameter with Bindable[Bt, B] {
+abstract class ExtractableParameter[Raw, Wrapper, Bndg <: Binding, Bind, Out] (spec: ParameterSpec[Raw],
+                                                                   eab: ParameterExtractAndBind[Wrapper, String, Bndg],
+                                                                   bindFn: Bind => Seq[Raw],
+                                                                   tToOut: Seq[Raw] => Out,
+                                                                   onMissing: (Parameter => Extraction[Out]))
+  extends Parameter with Bindable[Bind, Bndg] {
 
   override def iterator: Iterator[Parameter] = Seq(this).iterator
 
   override val paramType: ParamType = spec.paramType
 
-  protected def extractOrHandle(from: From, tToOut: Seq[T] => O, onMissing: Extraction[O]): Extraction[O] = from match {
+  def <--?(from: Wrapper): Extraction[Out] = from match {
     case req: ExtractedRouteRequest => req.get(this)
     case _ => eab.valuesFrom(this, from)
       .map(xs => Try(xs.map(spec.deserialize)) match {
         case Success(x) => Extracted(tToOut(x))
         case Failure(_) => ExtractionFailed(Invalid(this))
-      }).getOrElse(onMissing)
+      }).getOrElse(onMissing(this))
   }
 
-  override def -->(value: Bt): Seq[B] = bindFn(value).map(spec.serialize).map(v => eab.newBinding(this, v))
+  override def -->(value: Bind): Seq[Bndg] = bindFn(value).map(spec.serialize).map(v => eab.newBinding(this, v))
 
 }
 
 abstract class SingleMandatoryParameter[T, From, B <: Binding](val name: String, val description: String, spec: ParameterSpec[T], eab: ParameterExtractAndBind[From, String, B])
-  extends ExtractableParameter[T, From, B, T, T](spec, eab, (t: T) => Seq(t)) {
-
-  def <--?(from: From): Extraction[T] = extractOrHandle(from, _.head, ExtractionFailed(Missing(this)))
-}
+  extends ExtractableParameter[T, From, B, T, T](spec, eab, Seq(_), _.head, p => ExtractionFailed(Missing(p)))
 
 abstract class SingleOptionalParameter[T, From, B <: Binding](val name: String, val description: String, spec: ParameterSpec[T], eab: ParameterExtractAndBind[From, String, B])
-  extends ExtractableParameter[T, From, B, T, Option[T]](spec, eab, (t: T) => Seq(t)) {
-
-  def <--?(from: From): Extraction[Option[T]] = extractOrHandle(from, _.headOption, Extracted(None))
-}
+  extends ExtractableParameter[T, From, B, T, Option[T]](spec, eab, Seq(_), _.headOption, _ => Extracted(None))
 
 abstract class MultiMandatoryParameter[T, From, B <: Binding](val name: String, val description: String, spec: ParameterSpec[T], eab: ParameterExtractAndBind[From, String, B])
-  extends ExtractableParameter[T, From, B, Seq[T], Seq[T]](spec, eab, identity[Seq[T]]) {
-
-  def <--?(from: From): Extraction[Seq[T]] = extractOrHandle(from, identity, ExtractionFailed(Missing(this)))
-}
+  extends ExtractableParameter[T, From, B, Seq[T], Seq[T]](spec, eab, identity, identity, p => ExtractionFailed(Missing(p)))
 
 abstract class MultiOptionalParameter[T, From, B <: Binding](val name: String, val description: String, spec: ParameterSpec[T], eab: ParameterExtractAndBind[From, String, B])
-  extends ExtractableParameter[T, From, B, Seq[T], Option[Seq[T]]](spec, eab, identity[Seq[T]]) {
-
-  def <--?(from: From): Extraction[Option[Seq[T]]] = extractOrHandle(from, Some(_), Extracted(None))
-}
+  extends ExtractableParameter[T, From, B, Seq[T], Option[Seq[T]]](spec, eab, identity, Some(_), _ => Extracted(None))
