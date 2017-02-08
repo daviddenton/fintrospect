fa617cbe97623ce7289f6f0a966c638aa2b7a633
David Denton <denton.david@gmail.com>
2017-02-08 13:32:22 +0000

    removed heinous cast.

--- core/src/main/scala/io/fintrospect/parameters/ExtractedRouteRequest.scala
+++ core/src/main/scala/io/fintrospect/parameters/ExtractedRouteRequest.scala
@@ -2,11 +2,11 @@ package io.fintrospect.parameters
 
 import com.twitter.finagle.http.{Request, RequestProxy}
 import io.fintrospect.util.ExtractionError.Missing
-import io.fintrospect.util.{Extraction, ExtractionError, ExtractionFailed}
+import io.fintrospect.util.{Extraction, ExtractionFailed}
 
 case class ExtractedRouteRequest(override val request: Request, contents: Map[Any, Extraction[Any]]) extends RequestProxy {
   def get[T](p: Parameter): Extraction[T] = contents.getOrElse(p, ExtractionFailed(Missing(p))).asInstanceOf[Extraction[T]]
 
-  def get[T](b: Body[T]): Extraction[T] = contents.getOrElse(b, ExtractionFailed(b.map(Missing).asInstanceOf[Seq[ExtractionError]])).asInstanceOf[Extraction[T]]
+  def get[T](b: Body[T]): Extraction[T] = contents.getOrElse(b, ExtractionFailed(b.toSeq.map(Missing))).asInstanceOf[Extraction[T]]
 
 }
