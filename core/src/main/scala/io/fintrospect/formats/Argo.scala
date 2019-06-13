6e9057e752e1f7c7d341f9e20454b4bdedd996fe
Nick <gurinderu@gmail.com>
2019-06-13 13:26:12 +0300

    Update deps (#51)

--- core/src/main/scala/io/fintrospect/formats/Argo.scala
+++ core/src/main/scala/io/fintrospect/formats/Argo.scala
@@ -4,26 +4,26 @@ import java.math.BigInteger
 
 import argo.format.{CompactJsonFormatter, PrettyJsonFormatter}
 import argo.jdom.JsonNodeFactories.booleanNode
-import argo.jdom.{JdomParser, JsonNode, JsonNodeFactories, JsonRootNode}
+import argo.jdom.{JdomParser, JsonNode, JsonNodeFactories}
 
 /**
   * Argo JSON support (application/json content type)
   */
-object Argo extends JsonLibrary[JsonRootNode, JsonNode] {
+object Argo extends JsonLibrary[JsonNode, JsonNode] {
 
-  object JsonFormat extends JsonFormat[JsonRootNode, JsonNode] {
+  object JsonFormat extends JsonFormat[JsonNode, JsonNode] {
     private val pretty = new PrettyJsonFormatter()
     private val compact = new CompactJsonFormatter()
 
-    override def parse(in: String): JsonRootNode = new JdomParser().parse(in)
+    override def parse(in: String): JsonNode = new JdomParser().parse(in)
 
-    override def pretty(node: JsonRootNode): String = pretty.format(node)
+    override def pretty(node: JsonNode): String = pretty.format(node)
 
-    override def compact(node: JsonRootNode): String = compact.format(node)
+    override def compact(node: JsonNode): String = compact.format(node)
 
-    override def obj(fields: Iterable[Field]): JsonRootNode = JsonNodeFactories.`object`(fields.map(f => field(f._1, f._2)).toSeq: _*)
+    override def obj(fields: Iterable[Field]): JsonNode = JsonNodeFactories.`object`(fields.map(f => field(f._1, f._2)).toSeq: _*)
 
-    override def array(elements: Iterable[JsonNode]): JsonRootNode = JsonNodeFactories.array(elements.toSeq: _*)
+    override def array(elements: Iterable[JsonNode]): JsonNode = JsonNodeFactories.array(elements.toSeq: _*)
 
     override def string(value: String) = JsonNodeFactories.string(value)
 
